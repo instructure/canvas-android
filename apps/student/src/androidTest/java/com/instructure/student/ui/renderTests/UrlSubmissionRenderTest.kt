1414abf9f9555853e832bbb6da45d3ca459983f9
Jordan Marshall <jmarshall@instructure.com>
2019-04-12 09:22:56 -0600

    [Student][MBL-11328] View URL submissions

---
+++ apps/student/src/androidTest/java/com/instructure/student/ui/renderTests/UrlSubmissionRenderTest.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package com.instructure.student.ui.renderTests
+
+import com.instructure.espresso.assertCompletelyDisplayed
+import com.instructure.espresso.assertHasText
+import com.instructure.student.espresso.StudentRenderTest
+import com.instructure.student.mobius.assignmentDetails.submissionDetails.content.UrlSubmissionFragment
+import org.junit.Test
+
+class UrlSubmissionRenderTest : StudentRenderTest() {
+
+    private val testUrl = "https://www.instructure.com"
+
+    @Test
+    fun displaysDisclaimer() {
+        loadPageWithData(testUrl, "")
+        urlSubmissionRenderPage.disclaimer.assertCompletelyDisplayed()
+    }
+
+    @Test
+    fun displaysUrl() {
+        loadPageWithData(testUrl, "")
+        urlSubmissionRenderPage.url.assertHasText(testUrl)
+    }
+
+    private fun loadPageWithData(url: String, previewUrl: String) {
+        val fragment = UrlSubmissionFragment.newInstance(url, previewUrl)
+        activityRule.activity.loadFragment(fragment)
+    }
+
+}
