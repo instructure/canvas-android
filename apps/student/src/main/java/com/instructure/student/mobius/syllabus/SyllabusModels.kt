b0b2d416c6d652ed8d330a548ff436e72bfa4b48
Calvin Kern <calvinmkern@gmail.com>
2019-04-22 13:03:34 -0600

    [Student][MBL-11023] Syllabus moflow

---
+++ apps/student/src/main/java/com/instructure/student/mobius/syllabus/SyllabusModels.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.instructure.student.mobius.syllabus
+
+import com.instructure.canvasapi2.models.Assignment
+import com.instructure.canvasapi2.models.CanvasContext
+import com.instructure.canvasapi2.models.ScheduleItem
+import com.instructure.canvasapi2.utils.DataResult
+
+sealed class SyllabusEvent {
+    data class DataLoaded(val events: DataResult<List<ScheduleItem>>) : SyllabusEvent()
+    data class AssignmentClicked(val assignmentId: Long) : SyllabusEvent()
+}
+
+sealed class SyllabusEffect {
+    data class LoadData(val contextId: Long, val forceNetwork: Boolean) : SyllabusEffect()
+    data class ShowAssignmentView(val assignment: Assignment, val canvasContext: CanvasContext) : SyllabusEffect()
+}
+
+data class SyllabusModel(
+    val isLoading: Boolean = false,
+    val canvasContext: CanvasContext,
+    val syllabus: ScheduleItem,
+    val events: DataResult<List<ScheduleItem>>? = null
+)
