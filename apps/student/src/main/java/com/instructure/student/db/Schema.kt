6f75067aeb2b830d68e024464fbb68ab26776beb
Stephen Brough <sbrough@instructure.com>
2019-05-14 13:59:24 -0600

    Add in licenses; remove test data insert code

--- apps/student/src/main/java/com/instructure/student/db/Schema.kt
+++ apps/student/src/main/java/com/instructure/student/db/Schema.kt
@@ -1,6 +1,21 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
 package com.instructure.student.db
 
-import com.instructure.canvasapi2.models.CanvasContext
 import com.instructure.student.Submission
 import com.instructure.student.db.sqlColAdapters.CanvasContextAdapter
 import com.instructure.student.db.sqlColAdapters.DateAdapter
@@ -19,21 +34,5 @@ fun createQueryWrapper(driver: SqlDriver): StudentDb {
 object Schema : SqlDriver.Schema by StudentDb.Schema {
     override fun create(driver: SqlDriver) {
         StudentDb.Schema.create(driver)
-
-        // Add some test data
-        createQueryWrapper(driver).apply {
-            submissionQueries.insertOnlineTextSubmission(
-                "test",
-                "some name",
-                123L,
-                CanvasContext.getGenericContext(CanvasContext.Type.COURSE, 124L)
-            )
-            submissionQueries.insertOnlineTextSubmission(
-                "test2",
-                "some name2",
-                1234L,
-                CanvasContext.getGenericContext(CanvasContext.Type.COURSE, 124L)
-            )
-        }
     }
 }
