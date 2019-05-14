0baab933f10656a9b06629976e6f1bc9c06df699
Stephen Brough <sbrough@instructure.com>
2019-05-14 13:39:27 -0600

    Add unit tests for the Date adapter

--- apps/student/src/main/java/com/instructure/student/db/sqlColAdapters/DateAdapter.kt
+++ apps/student/src/main/java/com/instructure/student/db/sqlColAdapters/DateAdapter.kt
@@ -2,22 +2,29 @@ package com.instructure.student.db.sqlColAdapters
 
 import com.squareup.sqldelight.ColumnAdapter
 import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.ZoneOffset
+import org.threeten.bp.format.DateTimeFormatter
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 typealias Date = OffsetDateTime
 
+/**
+ * Expectation is that the database will hold whatever datetime the server gives back to us.
+ * When we pull it out, it will be converted to a local [OffsetDateTime].
+ * When we put something in, it will be converted to a UTC-based ISO-8601 datetime (what the server currently uses).
+ */
 class DateAdapter : ColumnAdapter<Date, String> {
-    override fun decode(databaseValue: String) = Date.parse(databaseValue) as Date
+    override fun decode(databaseValue: String) = Date.parse(databaseValue).withOffsetSameInstant(OffsetDateTime.now().offset) as Date
     override fun encode(value: Date): String = value.toApiString()
 }
 
-fun OffsetDateTime?.toApiString(timeZone: TimeZone? = null): String {
+/**
+ * Turns a local OffsetDateTime to a UTC-based ISO-8601 Datetime with timezone string
+ */
+fun OffsetDateTime?.toApiString(): String {
     this ?: return ""
-
-    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)
-    timeZone?.let { format.timeZone = it }
-    val formatted = format.format(this)
-    return formatted.substring(0, 22) + ":" + formatted.substring(22)
+    val utcTime = this.withOffsetSameInstant(ZoneOffset.UTC)
+    return utcTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
 }
