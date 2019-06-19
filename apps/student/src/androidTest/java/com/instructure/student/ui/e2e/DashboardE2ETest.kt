57c59181cf80828e6f575f78583b7c25d7e42329
Joe Hoag <jhoag@instructure.com>
2019-06-19 10:55:36 -0600

    MBL-12551: Stubbed out remaining E2E UI tests
    Also added a "Stub" annotation, and tweaked our flank configuration files appropriately.

---
+++ apps/student/src/androidTest/java/com/instructure/student/ui/e2e/DashboardE2ETest.kt
@@ -0,0 +1,19 @@
+package com.instructure.student.ui.e2e
+
+import com.instructure.canvas.espresso.E2E
+import com.instructure.canvas.espresso.Stub
+import com.instructure.student.ui.utils.StudentTest
+import org.junit.Test
+
+class DashboardE2ETest : StudentTest() {
+    override fun displaysPageObjects() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    @Stub
+    @E2E
+    @Test
+    fun testDashboardE2E() {
+
+    }
+}
