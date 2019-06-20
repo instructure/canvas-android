1dd0e0e271c04855ae4c790ad271fbfba86e3806
Joe Hoag <jhoag@instructure.com>
2019-06-20 10:22:05 -0600

    MBL-12551: Stubbed out remaining E2E UI tests (#106)
    Also added a "Stub" annotation, and tweaked our flank configuration files appropriately.

--- automation/canvas_espresso/src/main/kotlin/com/instructure/canvas/espresso/E2EAnnotation.kt
+++ automation/canvas_espresso/src/main/kotlin/com/instructure/canvas/espresso/E2EAnnotation.kt
@@ -1,5 +1,6 @@
 package com.instructure.canvas.espresso
 
+// When applied to a test method, denotes that the test will run "end-to-end", generating real network requests
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class E2E
