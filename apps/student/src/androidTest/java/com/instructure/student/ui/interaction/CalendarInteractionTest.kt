441438c5489b0197f287f05ce46080a07a48f367
Jordan Marshall <jmarshall@instructure.com>
2019-06-26 13:25:07 -0600

    [Student][MBL-12553] Interaction test stubs

---
+++ apps/student/src/androidTest/java/com/instructure/student/ui/interaction/CalendarInteractionTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package com.instructure.student.ui.interaction
+
+import com.instructure.canvas.espresso.Stub
+import com.instructure.student.ui.utils.StudentTest
+import org.junit.Test
+
+class CalendarInteractionTest : StudentTest() {
+
+    override fun displaysPageObjects() = Unit // Not used for interaction tests
+
+    @Stub
+    @Test
+    fun testMonthView_tappingADayDisplaysAllItemsForThatDay() {
+        // Tapping a day in the calendar view should display all items for that day in the list view
+    }
+
+    @Stub
+    @Test
+    fun testMonthView_itemListIsScrollable() {
+        // List of calendar items should be scrollable
+    }
+
+    @Stub
+    @Test
+    fun testMonthView_todayButtonGoesToCurrentDate() {
+        // The 'today' button in the toolbar should select the current date in the calendar
+    }
+
+}
---
+++ apps/student/src/androidTest/java/com/instructure/student/ui/interaction/SettingsInteractionTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package com.instructure.student.ui.interaction
+
+import com.instructure.canvas.espresso.Stub
+import com.instructure.student.ui.utils.StudentTest
+import org.junit.Test
+
+class SettingsInteractionTest : StudentTest() {
+
+    override fun displaysPageObjects() = Unit // Not used for interaction tests
+
+    @Stub
+    @Test
+    fun testHelp_askQuestion() {
+        // Should open a dialog and send a question for the selected course
+    }
+
+    @Stub
+    @Test
+    fun testHelp_searchCanvasGuides() {
+        // Should open the Canvas guides in a WebView
+    }
+
+    @Stub
+    @Test
+    fun testHelp_reportAProblem() {
+        // Should send an error report
+    }
+
+    @Stub
+    @Test
+    fun testHelp_submitFeatureIdea() {
+        // Should send a pre-filled email intent. Should be addressed to mobilesupport@instructure.com.
+    }
+
+    @Stub
+    @Test
+    fun testHelp_shareYourLove() {
+        // Should send an intent to open the listing for Student App in the Play Store
+    }
+
+    @Stub
+    @Test
+    fun testLegal_showOpenSourceLicenses() {
+        // Should display a list of open source dependencies used in the app, along with their licenses
+    }
+
+    @Stub
+    @Test
+    fun testLegal_showTermsOfUse() {
+        // Should display terms of use in a WebView
+    }
+
+    @Stub
+    @Test
+    fun testLegal_showPrivacyPolicy() {
+        // Should display the privacy policy in a WebView
+    }
+
+}
