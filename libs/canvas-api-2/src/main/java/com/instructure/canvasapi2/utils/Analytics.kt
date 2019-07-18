36f4048857d9aa25d883dcfa93dcdf30d25541b3
Trevor Needham <tneedham@instructure.com>
2019-07-18 10:34:59 -0600

    fix license

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/utils/Analytics.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/utils/Analytics.kt
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (C) 2017 - present Instructure, Inc.
+ * Copyright (C) 2019g - present Instructure, Inc.
  *
  *     Licensed under the Apache License, Version 2.0 (the "License");
  *     you may not use this file except in compliance with the License.
