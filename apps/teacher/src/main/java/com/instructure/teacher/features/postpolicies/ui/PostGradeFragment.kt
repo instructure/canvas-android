401ea8d9ffbd91c0b6f41ed18894f5c1f9a5e92e
CalvinKern <calvinmkern@gmail.com>
2019-08-26 08:52:02 -0600

    [Teacher][MBL-13105] Post Policy skeleton (#257)
    Added the feature flag, necessary strings, and the view pager layout for the base screen. Now just the two individual screens need their loops/views done.

---
+++ apps/teacher/src/main/java/com/instructure/teacher/features/postpolicies/ui/PostGradeFragment.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.instructure.teacher.features.postpolicies.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.instructure.canvasapi2.models.Assignment
+import com.instructure.canvasapi2.models.Course
+import com.instructure.pandautils.utils.Const
+import com.instructure.pandautils.utils.ParcelableArg
+import com.instructure.pandautils.utils.makeBundle
+import com.instructure.pandautils.utils.withArgs
+import com.instructure.teacher.R
+
+class PostGradeFragment : Fragment() {
+    private var assignment: Assignment by ParcelableArg(Assignment(), Const.ASSIGNMENT)
+    private var course: Course by ParcelableArg(Course(), Const.CANVAS_CONTEXT)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return TextView(requireContext()).apply { setText(R.string.postGradesTab) }
+    }
+
+    companion object {
+        fun newInstance(course: Course, assignment: Assignment) = PostGradeFragment().withArgs(course.makeBundle {
+            putParcelable(Const.ASSIGNMENT, assignment)
+        })
+    }
+}
