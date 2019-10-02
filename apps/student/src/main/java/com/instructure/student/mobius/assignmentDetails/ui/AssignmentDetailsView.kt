f48e459e63e603c384c39c4c093bd549872c40e5
CalvinKern <calvinmkern@gmail.com>
2019-10-02 08:46:00 -0600

    [Student][MBL-13260] Fix non native quizzes from assignments (#327)

--- apps/student/src/main/java/com/instructure/student/mobius/assignmentDetails/ui/AssignmentDetailsView.kt
+++ apps/student/src/main/java/com/instructure/student/mobius/assignmentDetails/ui/AssignmentDetailsView.kt
@@ -265,7 +265,11 @@ class AssignmentDetailsView(
 
     fun showQuizStartView(canvasContext: CanvasContext, quiz: Quiz) {
         logEvent(AnalyticsEventConstants.ASSIGNMENT_DETAIL_QUIZLAUNCH)
-        RouteMatcher.route(context, QuizStartFragment.makeRoute(canvasContext, quiz))
+        if (QuizListFragment.isNativeQuiz(canvasContext, quiz)) {
+            RouteMatcher.route(context, QuizStartFragment.makeRoute(canvasContext, quiz))
+        } else {
+            RouteMatcher.route(context, BasicQuizViewFragment.makeRoute(canvasContext, quiz, quiz.url!!))
+        }
     }
 
     fun showDiscussionDetailView(canvasContext: CanvasContext, discussionTopicHeaderId: Long) {
