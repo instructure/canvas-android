30223d57f3a950b9b179d4ad52bd940c67489de0
Joe Hoag <jhoag@instructure.com>
2019-10-29 09:06:20 -0600

    MBL-13476: Fixed the run_tests_with_coverage script
    For some reason, it didn't want to succeed if it wasn't run from the root of the repo.  I commented that out.

--- apps/flutter_parent/run_tests_with_coverage.sh
+++ apps/flutter_parent/run_tests_with_coverage.sh
@@ -77,7 +77,7 @@ runReport() {
     fi
 }
 
-if ! [ -d .git ]; then printf "\nError: not in root of repo"; show_help; fi
+#if ! [ -d .git ]; then printf "\nError: not in root of repo"; show_help; fi
 
 case $1 in
     --help)
