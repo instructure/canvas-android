0e0198ac2c350945affd1ca5bac04f85dca930d3
Stephen Brough <sbrough@instructure.com>
2019-11-06 10:42:21 -0700

    Remove unused import

--- apps/flutter_parent/lib/models/enrollment.dart
+++ apps/flutter_parent/lib/models/enrollment.dart
@@ -15,7 +15,6 @@ library enrollment;
 
 import 'dart:convert';
 
-import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 
