5c432c2ce8a3fe7acfcb96fc5a454411dcf3e628
Jordan Marshall <jmarshall@instructure.com>
2019-12-09 07:14:03 -0700

    'child' -> 'student'

--- apps/flutter_parent/lib/l10n/app_localizations.dart
+++ apps/flutter_parent/lib/l10n/app_localizations.dart
@@ -320,7 +320,7 @@ class AppLocalizations {
   String get noCoursesTitle => Intl.message('No Courses', desc: 'Title for having no courses');
 
   String get noCoursesMessage =>
-      Intl.message("Your child’s courses might not be published yet.", desc: 'Message for having no courses');
+      Intl.message("Your students’s courses might not be published yet.", desc: 'Message for having no courses');
 
   String get noGrade => Intl.message(
         'No Grade',
