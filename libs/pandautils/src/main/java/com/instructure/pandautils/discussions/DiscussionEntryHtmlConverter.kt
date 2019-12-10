32e3931cceadf6be127a3bb01c8fda3c6d30c8a9
CalvinKern <calvinmkern@gmail.com>
2019-12-10 12:41:41 -0700

    [Student][Teacher][MBL-13634] Add a11y label for discussion liking (#419)
    * [Student][Teacher][MBL-13634] Add a11y label for discussion liking
    
    * PR feedback - fix updating aria labels in place

--- libs/pandautils/src/main/java/com/instructure/pandautils/discussions/DiscussionEntryHtmlConverter.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/discussions/DiscussionEntryHtmlConverter.kt
@@ -50,6 +50,8 @@ class DiscussionEntryHtmlConverter {
             deletedText: String): String {
 
         val repliesButtonText = context.resources.getQuantityString(R.plurals.utils_discussionsReplies, discussionEntry.totalChildren, discussionEntry.totalChildren.localized)
+        val likeCountLabel = getLikeCountAllyText(context, discussionEntry)
+        val likeEntryLabel = context.resources.getString(R.string.likeEntryLabel)
 
         val htmlListener = getItemClickListener(discussionEntry.id.toString())
         val replyListener = getReplyListener(discussionEntry.id.toString())
@@ -179,6 +181,9 @@ class DiscussionEntryHtmlConverter {
                 .replace("__GROUP__", "display: block;")
                 .replace("__LIKE_ICON__", likingIcon)
                 .replace("__LIKE_COUNT__", likingSum)
+                .replace("__LIKE_ARIA__", likeEntryLabel)
+                .replace("__LIKE_CHECKED_ARIA__", discussionEntry._hasRated.toString())
+                .replace("__LIKE_COUNT_ARIA__", likeCountLabel)
                 .replace("__LIKE_ALLOWED__", liking)
                 .replace("__LIKE_COLOR__", colorToHex(likingColor))
                 .replace("__BRAND_COLOR__", colorToHex(brandColor))
@@ -286,5 +291,15 @@ class DiscussionEntryHtmlConverter {
             return String.format("<div class=\"delete_vertical_divider\">%s</div>", "|")
         }
         return ""
+    }
+
+    companion object {
+        fun getLikeCountAllyText(context: Context, discussionEntry: DiscussionEntry): String {
+            return if (discussionEntry.ratingSum == 0) "" else context.resources.getQuantityString(
+                R.plurals.likeCountLabel,
+                discussionEntry.ratingSum,
+                discussionEntry.ratingSum.localized
+            )
+        }
     }
 }
