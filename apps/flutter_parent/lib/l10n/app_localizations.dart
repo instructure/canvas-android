86c07a73b11dfe2c2439b606f80ebb9db103c569
Trevor Needham <tneedham@instructure.com>
2020-01-21 08:01:50 -0700

    fixed typo in localizations

--- apps/flutter_parent/lib/l10n/app_localizations.dart
+++ apps/flutter_parent/lib/l10n/app_localizations.dart
@@ -686,7 +686,7 @@ class AppLocalizations {
       desc: 'Message shown when an announcement detail screen fails to load');
 
   String get institutionAnnouncementTitle => Intl.message('Institution Announcement',
-      desc: 'Title text show for institution level announcements');
+      desc: 'Title text shown for institution level announcements');
 
   String get genericNetworkError => Intl.message('Network error');
 }
