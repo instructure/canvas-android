38ca209cf60d6786134cc8f6ff3f940716ab7b6d
Joe Hoag <jhoag@instructure.com>
2020-01-27 10:56:21 -0700

    MBL-13852: Cleaned up new ally-check.

--- apps/flutter_parent/test/utils/accessibility_utils.dart
+++ apps/flutter_parent/test/utils/accessibility_utils.dart
@@ -51,7 +51,7 @@ Future<void> runAccessibilityTests(WidgetTester tester) async {
   await expectLater(tester, meetsGuideline(textContrastGuideline));
   await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
   await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
-  await expectLater(tester, meetsGuideline(SensibleDpadNavigationGuideline())); // Needs to be last, because it fiddles with UI
+  await expectLater(tester, meetsGuideline(TextFieldNavigationGuideline())); // Needs to be last, because it fiddles with UI
 }
 
 // Here's an example of a custom guideline.  We can conceivably write
@@ -87,96 +87,86 @@ class NoHintsGuideline extends AccessibilityGuideline {
   }
 }
 
-class SensibleDpadNavigationGuideline extends AccessibilityGuideline {
+/*
+ * An a11y checker that insures that we can navigate out of TextFields using dpad arrows.
+ */
+class TextFieldNavigationGuideline extends AccessibilityGuideline {
   @override
-  String get description => 'Dpad navigation should be sensible';
+  String get description => 'You should be able to direction-arrow out of TextFields';
 
-  SemanticsNode focusedNode = null;
-
-  void traverseFocusNodes(List<FocusNode> nodeList, FocusNode root) {
+  // Grab the FocusNodes associated with this screen
+  List<FocusNode> _getFocusNodes(FocusNode root) {
+    List<FocusNode> result = List<FocusNode>();
     Queue<FocusNode> nodeQueue = Queue<FocusNode>();
     nodeQueue.addLast(root);
     while(nodeQueue.isNotEmpty) {
       FocusNode node = nodeQueue.removeFirst();
-      nodeList.add(node);
+      result.add(node);
       node.children.forEach( (fn) {
         nodeQueue.addLast(fn);
-//        bool isEditableText = fn.context.widget is EditableText;
-//        print("FocusNode: $fn, Widget: ${fn.context.widget}, isEditableText: $isEditableText");
-//
-//        if(isEditableText) {
-//          Widget w = fn.context.widget;
-//          fn.requestFocus();
-//          SemanticsNode sn = findFocusedNode(tester.binding.pipelineOwner.semanticsOwner.rootSemanticsNode);
-//        }
       });
     }
+
+    return result;
   }
 
-  void gatherFocusableNodes(Set<SemanticsNode> result, SemanticsNode root) {
+  // Grab the focusable SemanticsNodes associated with this screen.
+  List<SemanticsNode> _getFocusableSemanticsNodes(SemanticsNode root) {
+    List<SemanticsNode> result = List<SemanticsNode>();
     if(root.hasFlag(SemanticsFlag.isFocusable) && !root.isMergedIntoParent & !root.isInvisible && !root.hasFlag(SemanticsFlag.isHidden)) {
       result.add(root);
     }
     
     root.visitChildren((SemanticsNode child) {
-      gatherFocusableNodes(result, child);
+      result.addAll(_getFocusableSemanticsNodes(child));
       return true;
     });
+
+    return result;
   }
 
-  void showAllSemanticsNodes(SemanticsNode root) {
-    print("SemanticsNode: $root");
+  // Attempt to find the SemanticsNode that is currently focused.
+  SemanticsNode _findFocusedNode(SemanticsNode root) {
+    //print("findFocusedNode: root = $root");
 
-    root.visitChildren( (child) {
-      showAllSemanticsNodes(child);
-      return true;
-    });
-  }
-  
-  void showFocusNodes(FocusNode root) {
-    print("focusNode: $root");
-    print("ancestors: ${root.ancestors.map( (fn) => fn.toString()).join(",")}");
-    print("descendants: ${root.descendants.map( (fn) => fn.toString()).join(",")}");
-    
-    root.traversalChildren.forEach( (node) {showFocusNodes(node);});
-  }
-
-  SemanticsNode findFocusedNode(SemanticsNode root) {
-    print("findFocusedNode: root = $root");
     if(root.hasFlag(SemanticsFlag.isFocused) ) {
-      focusedNode = root;
       //print("Found focused node! $focusedNode");
       return root;
     }
 
-    focusedNode = null;
+    SemanticsNode result = null;
     root.visitChildren((SemanticsNode child) {
-      if(focusedNode == null) {
-        findFocusedNode(child);
+      if(result == null) {
+        result = _findFocusedNode(child);
       }
       return true;
     });
 
-    return focusedNode;
+    return result;
   }
 
-  Future<FocusNode> moveDown(WidgetTester tester) async {
-    return move(tester, LogicalKeyboardKey.arrowDown);
+  // Cause an arrow-down to be sent to the screen
+  Future<FocusNode> _moveDown(WidgetTester tester) async {
+    return _move(tester, LogicalKeyboardKey.arrowDown);
   }
 
-  Future<FocusNode> moveUp(WidgetTester tester) async {
-    return move(tester, LogicalKeyboardKey.arrowUp);
+  // Cause an arrow-up to be sent to the screen
+  Future<FocusNode> _moveUp(WidgetTester tester) async {
+    return _move(tester, LogicalKeyboardKey.arrowUp);
   }
 
-  Future<FocusNode> moveLeft(WidgetTester tester) async {
-    return move(tester, LogicalKeyboardKey.arrowLeft);
+  // Cause an arrow-left to be sent to the screen
+  Future<FocusNode> _moveLeft(WidgetTester tester) async {
+    return _move(tester, LogicalKeyboardKey.arrowLeft);
   }
 
-  Future<FocusNode> moveRight(WidgetTester tester) async {
-    return move(tester, LogicalKeyboardKey.arrowRight);
+  // Cause an arrow-right to be sent to the screen
+  Future<FocusNode> _moveRight(WidgetTester tester) async {
+    return _move(tester, LogicalKeyboardKey.arrowRight);
   }
 
-  Future<FocusNode> move(WidgetTester tester, LogicalKeyboardKey key) async {
+  // Common logic for arrow-moving
+  Future<FocusNode> _move(WidgetTester tester, LogicalKeyboardKey key) async {
     await tester.sendKeyEvent(key);
     await tester.pumpAndSettle();
     FocusNode  newFocus = tester.binding.focusManager.primaryFocus;
@@ -185,95 +175,41 @@ class SensibleDpadNavigationGuideline extends AccessibilityGuideline {
 
   @override
   FutureOr<Evaluation> evaluate(WidgetTester tester) async {
-    final SemanticsNode root = tester.binding.pipelineOwner.semanticsOwner
-        .rootSemanticsNode;
+    final SemanticsNode root = tester.binding.pipelineOwner.semanticsOwner.rootSemanticsNode;
 
+    // Default result
     Evaluation result = Evaluation.pass();
 
-    //List<FocusScope> allFocusScopeWidgets = tester.allWidgets.where( (w) => w.runtimeType == FocusScope);
+    // Gather all focusable SemanticsNodes for our screen
+    List<SemanticsNode> focusableSemanticsNodes = _getFocusableSemanticsNodes(root);
 
-//    tester.allWidgets.forEach((widget) {
-//      if(widget.runtimeType == TextField) {
-//        widget.
-//        if(allFocusScopeWidgets.where( (fs) => fs.))
-//        DiagnosticsNode dn = widget.toDiagnosticsNode();
-//        tester.allWidgets.where( (w) => w.debugDescribeChildren().where( (d) => d.));
-//      }
-//
-//    });
-    String focusScopeWidgets = tester.allWidgets.where( (w) => w.runtimeType == FocusScope).map( (w) => w.toString()).join("\n");
-    print("FocusScopes: $focusScopeWidgets");
-
-    showAllSemanticsNodes(root);
-
-    // Traversal logic that recurses to children
-    Evaluation traverse(SemanticsNode node) {
-      Evaluation result = const Evaluation.pass();
-
-      node.visitChildren((node) {
-        result += traverse(node);
-        return true;
-      });
-
-      print("node = ${node}");
-      if (node.hasFlag(SemanticsFlag.isTextField)) {
-        node.sendEvent(TapSemanticEvent());
-        SemanticsNode focusedNode = findFocusedNode(root);
-        print("TextField $node, focused node = $focusedNode");
-        SemanticsNode parent = node.parent;
-        bool focusScopeFound = false;
-        while (parent != null) {
-          if (parent.runtimeType == FocusScope) {
-            print("FOUND FocusScope $parent");
-            focusScopeFound = true;
-            break;
-          }
-          parent = parent.parent;
-        }
-
-        if (!focusScopeFound) {
-          result += Evaluation.fail(
-              "TextField $node does not have a wrapping FocusScope");
-        }
-      }
-
-      return result;
-    }
-
-    Set<SemanticsNode> focusableNodes = Set<SemanticsNode>();
-    gatherFocusableNodes(focusableNodes, root);
-
-    //showFocusNodes(tester.binding.focusManager.primaryFocus);
-    focusableNodes.forEach((SemanticsNode node) {
-      print("Focusable: $node, id: ${node.id}");
-    });
-
-    List<FocusNode> focusNodeTraversal = List<FocusNode>();
-    traverseFocusNodes(focusNodeTraversal, tester.binding.focusManager.rootScope);
+    // Compute all eligible FocusNodes on our screen.
+    List<FocusNode> focusNodes = _getFocusNodes(tester.binding.focusManager.rootScope);
 
 
-    if(focusableNodes.length > 1) {
-      for(FocusNode fn in focusNodeTraversal) {
+    if(focusableSemanticsNodes.length > 1) { // Only test navigability if there is something else to which to navigate.
+      for(FocusNode fn in focusNodes) {
         if(fn.context != null && fn.context.widget is EditableText) {
+          // For each EditableText that we encounter, tap on it to focus it...
           fn.requestFocus();
           await tester.pumpAndSettle();
-          FocusNode currFocus = tester.binding.focusManager.primaryFocus;
-          print("EditableText Focusable: $fn, currFocus: $currFocus");
-          FocusNode newFocus = null;
 
-          newFocus = await moveUp(tester);
+          // and then try to navigate out of it.
+          FocusNode currFocus = tester.binding.focusManager.primaryFocus;
+          FocusNode newFocus = await _moveUp(tester);
           if(newFocus == currFocus) {
-            newFocus = await moveDown(tester);
+            newFocus = await _moveDown(tester);
           }
           if(newFocus == currFocus) {
-            newFocus = await moveRight(tester);
+            newFocus = await _moveRight(tester);
           }
           if(newFocus == currFocus) {
-            newFocus = await moveLeft(tester);
+            newFocus = await _moveLeft(tester);
           }
           if(newFocus == currFocus) {
-            SemanticsNode focusedSemanticsNode = findFocusedNode(root);
-            result += Evaluation.fail("Directional nav stuck in $currFocus, Semantics: $focusedSemanticsNode"); // TODO: Left/Right
+            // Attempt to correlate a SemanticsNode with our failed FocusNode
+            SemanticsNode focusedSemanticsNode = _findFocusedNode(root);
+            result += Evaluation.fail("Directional nav stuck in $currFocus, Semantics: $focusedSemanticsNode\n");
           }
         }
       }
@@ -281,135 +217,4 @@ class SensibleDpadNavigationGuideline extends AccessibilityGuideline {
 
     return result;
   }
-//      SemanticsNode upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//      upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//      upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//      upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//      upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//      upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//      upNode = await moveUp(tester);
-//      print("up-node: $upNode");
-//
-//
-//      //for(int i=0; i<10; i++) await moveUp(tester);
-//
-//      SemanticsNode leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      leftNode = await moveLeft(tester);
-//      print("left-node: $leftNode");
-//      //for(int i=0; i<10; i++) await moveLeft(tester);
-//
-//
-//
-////      print("initial focus: ${findFocusedNode(root).toString()}");
-////      await moveDown(tester);
-////      print("focus after movedown: ${findFocusedNode(root).toString()}");
-////
-////      FocusNode focus = tester.binding.focusManager.primaryFocus;
-////      print("initial focusNode:  $focus");
-////      // Make sure we're the top-most focus node
-////      if(focus.ancestors != null && focus.ancestors.length > 0) {
-////        focus = focus.ancestors.last;
-////        print("top-most focus node: $focus");
-////        focus.requestFocus();
-////        await tester.pumpAndSettle(Duration(milliseconds: 500));
-////        await tester.pumpAndSettle();
-////        print("focus after refocus: ${findFocusedNode(root).toString()}");
-////      }
-//
-//      // Traverse the whole thing, starting from the top and going top-to-bottom, left-to-right
-//      List<SemanticsNode> hitList = List<SemanticsNode>();
-//      SemanticsNode currFocus = findFocusedNode(root);
-//      print("initial focus: $currFocus");
-//      SemanticsNode prevFocus = null;
-//      while(currFocus != prevFocus) {
-//        hitList.add(currFocus);
-//        prevFocus = currFocus;
-//        SemanticsNode rightFocus = await moveRight(tester);
-//        if(rightFocus != currFocus) {
-//          currFocus = rightFocus;
-//        }
-//        else {
-//          currFocus = await moveDown(tester);
-//        }
-//      }
-//
-//      print("Nodes hit: ${hitList.map((node) => node.toString()).join("\n")}");
-//
-////      // Traverse downward
-////      List<SemanticsNode> hitListDown = List<SemanticsNode>();
-////      SemanticsNode prevFocus = null;
-////      SemanticsNode currFocus = findFocusedNode(root);
-////      if(currFocus != null) {
-////        hitListDown.add(currFocus);
-////      }
-////      while(currFocus != prevFocus) {
-////        prevFocus = currFocus;
-////        currFocus = await moveDown(tester);
-////        if (currFocus != prevFocus) {
-////          hitListDown.add(currFocus);
-////        }
-////      }
-////
-////      // Traverse upward
-////      List<SemanticsNode> hitListUp = List<SemanticsNode>();
-////      prevFocus = null;
-////      if(currFocus != null) {
-////        hitListUp.add(currFocus);
-////      }
-////      while(currFocus != prevFocus) {
-////        prevFocus = currFocus;
-////        currFocus = await moveUp(tester);
-////        if (currFocus != prevFocus) {
-////          hitListUp.add(currFocus);
-////        }
-////      }
-//
-//
-//
-////      if(hitListUp.length != hitListDown.length) {
-////        result += Evaluation.fail("""
-////            down-navigation length different than up-navigation length
-////            down-path=${hitListDown.map((node) => node.label).join(",")}
-////            up-path=${hitListUp.map((node) => node.label).join(",")}
-////            """);
-////      }
-//
-//      focusableNodes.map((node) => node.label).forEach((label) {
-//        if(!hitList.map((node) => node.label).contains(label)) {
-//          result += Evaluation.fail("Label '$label' is focusable but did not appear in down-navigation\n");
-//        }
-////        if(!hitListUp.contains(focusableNode)) {
-////          result += Evaluation.fail("$focusableNode is focusable but did not appear in up-navigation");
-////        }
-//      });
-//
-//      return result; // Returns aggregate result
-//    }
-//
-
-//
-//    List<FocusNode> focusNodeTraversal = List<FocusNode>();
-//    traverseFocusNodes(focusNodeTraversal, tester.binding.focusManager.rootScope);
-//
-//    print("FOCUS Traversal order: ${focusNodeTraversal.map((fn) => fn.toString()).join("\n")}");
-//
-//    return traverse(root, focusableNodes); // Start traversing at the root.
-//  }
 }
