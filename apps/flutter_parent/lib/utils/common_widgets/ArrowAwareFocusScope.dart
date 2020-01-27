069c8adadce5317ee72d710af19c6cf9d19209e5
Joe Hoag <jhoag@instructure.com>
2020-01-27 14:32:17 -0700

    MBL-13852: Fixed a code module header.

--- apps/flutter_parent/lib/utils/common_widgets/ArrowAwareFocusScope.dart
+++ apps/flutter_parent/lib/utils/common_widgets/ArrowAwareFocusScope.dart
@@ -1,5 +1,3 @@
-
-import 'package:flutter/services.dart';
 // Copyright (C) 2019 - present Instructure, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
@@ -14,6 +12,7 @@ import 'package:flutter/services.dart';
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 
 // A key-catch callback for the class below
 FocusOnKeyCallback _onDirectionKeyCallback = (node, event) {
