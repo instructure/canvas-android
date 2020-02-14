0c21f672e5ee0c1e97884d1014c8abfc1b4bf3d5
Calvin Kern <calvinmkern@gmail.com>
2020-02-14 11:18:02 -0700

    Testing fixing a broken string to make the build pass

--- apps/flutter_parent/lib/l10n/app_localizations.dart
+++ apps/flutter_parent/lib/l10n/app_localizations.dart
@@ -164,12 +164,12 @@ class AppLocalizations {
 
   String get switchUsers => Intl.message('Switch Users', desc: 'Label text for the Switch Users nav drawer button');
 
-  String appVersion(String version) =>
-      Intl.message('v. $version', args: [version], desc: 'App version shown in the navigation drawer');
+  String appVersion(String version) => Intl.message('v. $version',
+      name: 'appVersion', args: [version], desc: 'App version shown in the navigation drawer');
 
   String get logoutConfirmation => Intl.message(
         'Are you sure you want to log out?',
-        desc: 'Confirmation message displayed when the user tries to log out',
+        desc: 'Confirmation message displayed when the user tries to log out of the app',
       );
 
   /// Login landing screen
