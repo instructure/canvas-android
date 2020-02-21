ce3a49ffb73ca01f8607775f00d01e01d3cc2e75
Blackchai <needham.trevor@gmail.com>
2020-02-21 08:49:11 -0700

    [Student][Teacher] Quick fix for studio vids embedded in pages (#591)
    * [Student][Teacher] Quick fix for studio vids embedded in pages
    
    * quick fix

--- apps/teacher/src/main/java/com/instructure/teacher/fragments/PageDetailsFragment.kt
+++ apps/teacher/src/main/java/com/instructure/teacher/fragments/PageDetailsFragment.kt
@@ -170,16 +170,11 @@ class PageDetailsFragment : BasePresenterFragment<
     override fun populatePageDetails(page: Page) {
         mPage = page
         if (CanvasWebView.containsLTI(page.body.orEmpty(), "UTF-8")) {
-            if (page.body.orEmpty().contains("arc_media")) {
-                // For embedded Studio videos, we just want to load the HTML we get back from the server
-                loadPageHtml(page.body.orEmpty())
-            } else {
-                canvasWebView.addJavascriptInterface(JsExternalToolInterface {
-                    val args = LTIWebViewFragment.makeLTIBundle(URLDecoder.decode(it, "utf-8"), "LTI Launch", true)
-                    RouteMatcher.route(requireContext(), Route(LTIWebViewFragment::class.java, canvasContext, args))
-                }, "accessor")
-                loadHtmlJob = canvasWebView.loadHtmlWithLTIs(requireContext(), isTablet, page.body.orEmpty(), ::loadPageHtml)
-            }
+            canvasWebView.addJavascriptInterface(JsExternalToolInterface {
+                val args = LTIWebViewFragment.makeLTIBundle(URLDecoder.decode(it, "utf-8"), "LTI Launch", true)
+                RouteMatcher.route(requireContext(), Route(LTIWebViewFragment::class.java, canvasContext, args))
+            }, "accessor")
+            loadHtmlJob = canvasWebView.loadHtmlWithLTIs(requireContext(), isTablet, page.body.orEmpty(), ::loadPageHtml)
         } else {
             loadPageHtml(page.body.orEmpty())
         }
