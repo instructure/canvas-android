f832af09f95ffba000697f44186c67893092a4a0
Trevor Needham <tneedham@instructure.com>
2020-03-06 08:46:01 -0700

    [Student][Teacher] Add route support for new rce files

--- apps/teacher/src/main/java/com/instructure/teacher/router/RouteMatcher.kt
+++ apps/teacher/src/main/java/com/instructure/teacher/router/RouteMatcher.kt
@@ -93,6 +93,7 @@ object RouteMatcher : BaseRouteMatcher() {
         routes.add(Route(courseOrGroup("/:course_id/files/folder(\\/.*)*"), FileListFragment::class.java))
         // Same as above, but if they access nested user files instead of course files
         routes.add(Route("/files/folder(\\/.*)*", RouteContext.FILE))
+        routes.add(Route("/files/:${RouterParams.FILE_ID}", RouteContext.FILE)) // Triggered by new RCE content file links
 
         routes.add(Route(courseOrGroup("/:course_id/files"), FileListFragment::class.java))
 
