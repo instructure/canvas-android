0281be90f7438018b9ac32bf9796969f0259fe20
CalvinKern <calvinmkern@gmail.com>
2020-03-16 12:47:29 -0600

    [Student][MBL-14066] Find Studio for submission type with lots of LTIs (#630)

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/apis/ExternalToolAPI.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/apis/ExternalToolAPI.kt
@@ -29,8 +29,10 @@ import retrofit2.http.Url
 
 
 internal object ExternalToolAPI {
+    // This returns a paged response, so either we have to depaginate, or pull down 100, since we're typically looking
+    // for the Studio LTI.
     internal interface ExternalToolInterface {
-        @GET("{contextId}/external_tools?include_parents=true")
+        @GET("{contextId}/external_tools?include_parents=true&per_page=100")
         fun getExternalToolsForCanvasContext(@Path("contextId") contextId: Long): Call<List<LTITool>>
 
         @GET
