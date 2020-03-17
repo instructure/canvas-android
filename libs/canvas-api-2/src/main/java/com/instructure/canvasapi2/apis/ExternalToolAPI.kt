0d074a96f947039069f9522670fd64048206e006
CalvinKern <calvinmkern@gmail.com>
2020-03-17 08:22:43 -0600

    [Student][MBL-14066] Studio for submission type with lots of LTIs part 2 (#634)

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/apis/ExternalToolAPI.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/apis/ExternalToolAPI.kt
@@ -32,7 +32,7 @@ internal object ExternalToolAPI {
     // This returns a paged response, so either we have to depaginate, or pull down 100, since we're typically looking
     // for the Studio LTI.
     internal interface ExternalToolInterface {
-        @GET("{contextId}/external_tools?include_parents=true&per_page=100")
+        @GET("{contextId}/external_tools?include_parents=true")
         fun getExternalToolsForCanvasContext(@Path("contextId") contextId: Long): Call<List<LTITool>>
 
         @GET
