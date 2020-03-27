27daf571f7a6e0e0dfbbd48df338b9507c89a07c
Calvin Kern <calvinmkern@gmail.com>
2020-03-27 08:33:02 -0600

    Fix for region names

--- translations/local-import.rb
+++ translations/local-import.rb
@@ -59,8 +59,8 @@ Dir.glob("#{import_dir}/*/") do |src_dir|
         if file.end_with?('.xml')
             language = language.gsub('-', '+').prepend('b+')
         end
-    else
-      language = language.gsub('-', '-r')
+    else if file.end_with?('.xml')
+        language = language.gsub('-', '-r') # Android specific naming convention
     end
     if language.eql? 'en'
       puts "Skipping redundant 'en' resource"
