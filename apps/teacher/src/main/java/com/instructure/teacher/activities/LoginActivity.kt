19ed7e2c1fa87e8f1c7529042732d7f5ddd49649
Trevor Needham <tneedham@instructure.com>
2020-04-01 11:48:21 -0600

    Fixes for tests and CR

--- apps/teacher/src/main/java/com/instructure/teacher/activities/LoginActivity.kt
+++ apps/teacher/src/main/java/com/instructure/teacher/activities/LoginActivity.kt
@@ -48,7 +48,7 @@ class LoginActivity : BaseLoginInitActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Check to see if we are switching users from an external QR scan
-        if(QRLogin.verifySSOLoginUri(intent.data)) {
+        if(QRLogin.verifySSOLoginUri(intent.data, true)) {
             startActivity(RouteValidatorActivity.createIntent(this, intent.data!!))
             finish()
         }
