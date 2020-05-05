d2c5bca61a2ee5fe91c53c19b1143447397f5beb
Trevor Needham <tneedham@instructure.com>
2020-05-05 10:42:03 -0600

    [Parent] Student dropdown name text alignment

--- apps/flutter_parent/lib/screens/dashboard/student_horizontal_list_view.dart
+++ apps/flutter_parent/lib/screens/dashboard/student_horizontal_list_view.dart
@@ -80,6 +80,7 @@ class StudentHorizontalListViewState extends State<StudentHorizontalListView> {
                   student.shortName,
                   key: Key("${student.shortName}_text"),
                   style: Theme.of(context).textTheme.subtitle.copyWith(color: ParentTheme.of(context).onSurfaceColor),
+                  textAlign: TextAlign.center,
                   overflow: TextOverflow.ellipsis,
                 ),
               ],
