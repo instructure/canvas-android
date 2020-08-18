3143a195ee9a02adaa7d6d86e4114431d23b9bfe
Jordan Marshall <jmarshall@instructure.com>
2020-08-18 14:49:40 -0600

    SyncFragment adapter fix

--- libs/blueprint/src/main/java/instructure/androidblueprint/SyncFragment.kt
+++ libs/blueprint/src/main/java/instructure/androidblueprint/SyncFragment.kt
@@ -102,7 +102,7 @@ abstract class SyncFragment<
     }
 
     fun clearAdapter() {
-        createAdapter().clear()
+        adapter.clear()
     }
 
     // Override in case of fragment not implementing Presenter<View> interface
