c773fc42f96a20b7beb831d31be420c5010043be
Joe Hoag <jhoag@instructure.com>
2020-08-20 07:35:28 -0600

    MBL-14649: Populated open_source_data/dataseeding-flutter (#939)
    With blank strings.

---
+++ open_source_data/dataseeding-flutter/private_consts.dart
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+// Changes to these values will not persist unless made in the original private-data/dataseeding-flutter folder.
+const String DATA_SEEDING_ADMIN_TOKEN = "";
+const String DATA_SEEDING_CLIENT_ID = "";
+const String DATA_SEEDING_CLIENT_SECRET = "";
