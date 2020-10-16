5f3841495ae3a37f2950ad638aece8a048f31fe5
Joe Hoag <jhoag@instructure.com>
2020-10-16 09:05:07 -0600

    MBL-14756: Defensive programming tweak
    Added protection against junk characters being inserted into a content description, which can happen on later versions of Android.

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/PandaAvatarPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/PandaAvatarPage.kt
@@ -2,6 +2,7 @@ package com.instructure.student.ui.pages
 
 import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.instructure.canvas.espresso.stringContainsTextCaseInsensitive
 import com.instructure.espresso.click
 import com.instructure.espresso.page.BasePage
 import com.instructure.espresso.page.onView
@@ -10,6 +11,7 @@ import com.instructure.espresso.page.withParent
 import com.instructure.espresso.scrollTo
 import com.instructure.student.R
 import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.containsString
 
 class PandaAvatarPage : BasePage(R.id.panda_create_layout) {
 
@@ -37,12 +39,12 @@ class PandaAvatarPage : BasePage(R.id.panda_create_layout) {
     }
 
     fun setAsAvatar() {
-        onView(withContentDescription("More options")).click()
+        onView(withContentDescription(stringContainsTextCaseInsensitive("More options"))).click()
         onView(allOf(withId(R.id.title), withText(R.string.setAsAvatar))).click()
     }
 
     fun save() {
-        onView(withContentDescription("More options")).click()
+        onView(withContentDescription(stringContainsTextCaseInsensitive("More options"))).click()
         onView(allOf(withId(R.id.title), withText(R.string.save))).click()
     }
 
