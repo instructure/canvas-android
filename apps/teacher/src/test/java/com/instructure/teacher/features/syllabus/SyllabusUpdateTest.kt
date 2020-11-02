cee0dd0839fe9f09d06b665843dfe3e06ab8d80f
Tamas Kozmer <tamas.kozmer@instructure.com>
2020-11-02 14:11:45 +0100

    Added unit tests for SyllabusUpdate.

---
+++ apps/teacher/src/test/java/com/instructure/teacher/features/syllabus/SyllabusUpdateTest.kt
@@ -0,0 +1,123 @@
+package com.instructure.teacher.features.syllabus
+
+import com.instructure.canvasapi2.models.Assignment
+import com.instructure.canvasapi2.models.Course
+import com.instructure.canvasapi2.models.ScheduleItem
+import com.instructure.canvasapi2.utils.DataResult
+import com.instructure.teacher.unit.utils.matchesEffects
+import com.instructure.teacher.unit.utils.matchesFirstEffects
+import com.spotify.mobius.test.FirstMatchers
+import com.spotify.mobius.test.InitSpec
+import com.spotify.mobius.test.NextMatchers
+import com.spotify.mobius.test.UpdateSpec
+import org.junit.Before
+import org.junit.Test
+
+class SyllabusUpdateTest {
+
+    private val initSpec = InitSpec(SyllabusUpdate()::init)
+    private val updateSpec = UpdateSpec(SyllabusUpdate()::update)
+
+    private lateinit var initModel: SyllabusModel
+    private lateinit var course: Course
+    private lateinit var syllabus: ScheduleItem
+    private var courseId: Long = 0
+
+    @Before
+    fun setup() {
+        courseId = 1234L
+        course = Course(id = courseId, name = "Course Name", syllabusBody = "This is a syllabus")
+        syllabus = ScheduleItem.createSyllabus(course.name, course.syllabusBody)
+        initModel = SyllabusModel(courseId = courseId)
+    }
+
+    @Test
+    fun `Init should start loading data and `() {
+        val expectedModel = initModel.copy(isLoading = true)
+        initSpec
+            .whenInit(initModel)
+            .then(
+                InitSpec.assertThatFirst(
+                    FirstMatchers.hasModel(expectedModel),
+                    matchesFirstEffects<SyllabusModel, SyllabusEffect>(SyllabusEffect.LoadData(course.id, false))
+                )
+            )
+    }
+
+    @Test
+    fun `PullToRefresh event should force network reload`() {
+        val expectedModel = initModel.copy(isLoading = true)
+        updateSpec
+            .given(initModel)
+            .whenEvent(SyllabusEvent.PullToRefresh)
+            .then(
+                UpdateSpec.assertThatNext(
+                    NextMatchers.hasModel(expectedModel),
+                    matchesEffects<SyllabusModel, SyllabusEffect>(SyllabusEffect.LoadData(course.id, true))
+                )
+            )
+    }
+
+    @Test
+    fun `DataLoaded event should update the model`() {
+        val course = DataResult.Success(course)
+        val events = DataResult.Success(List(4) {
+            ScheduleItem(itemId = it.toString())
+        })
+
+        val expectedModel = initModel.copy(isLoading = false, course = course, events = events, syllabus = syllabus)
+
+        updateSpec
+            .given(initModel.copy(isLoading = true))
+            .whenEvent(SyllabusEvent.DataLoaded(course, events))
+            .then(
+                UpdateSpec.assertThatNext(NextMatchers.hasModel(expectedModel))
+            )
+    }
+
+    @Test
+    fun `DataLoaded event should update the model without the syllabus when failed to get course`() {
+        val initModel = initModel.copy(isLoading = true, course = DataResult.Success(course), events = DataResult.Success(emptyList()), syllabus = ScheduleItem.createSyllabus(null, null))
+
+        val course = DataResult.Fail()
+        val expectedModel = initModel.copy(isLoading = false, course = course, events = course, syllabus = null)
+
+        updateSpec
+            .given(initModel)
+            .whenEvent(SyllabusEvent.DataLoaded(course, course))
+            .then(
+                UpdateSpec.assertThatNext(NextMatchers.hasModel(expectedModel))
+            )
+    }
+
+    @Test
+    fun `SyllabusItemClicked event should dispatch ShowAssignmentView effect when item has an assignment`() {
+        val assignmentId = 123L
+        val courseResult = DataResult.Success(course)
+        val events = DataResult.Success(listOf(ScheduleItem(itemId = "1", assignment = Assignment(id = assignmentId))))
+
+        val initModel = initModel.copy(course = courseResult, events = events)
+
+        updateSpec
+            .given(initModel)
+            .whenEvent(SyllabusEvent.SyllabusItemClicked("1"))
+            .then(
+                UpdateSpec.assertThatNext(matchesEffects<SyllabusModel, SyllabusEffect>(SyllabusEffect.ShowAssignmentView(events.data[0].assignment!!, course)))
+            )
+    }
+
+    @Test
+    fun `SyllabusItemClicked event should dispatch ShowScheduleItemView when item does not have an assignment`() {
+        val courseResult = DataResult.Success(course)
+        val events = DataResult.Success(listOf(ScheduleItem(itemId = "1")))
+
+        val initModel = initModel.copy(course = courseResult, events = events)
+
+        updateSpec
+            .given(initModel)
+            .whenEvent(SyllabusEvent.SyllabusItemClicked("1"))
+            .then(
+                UpdateSpec.assertThatNext(matchesEffects<SyllabusModel, SyllabusEffect>(SyllabusEffect.ShowScheduleItemView(events.data[0], course)))
+            )
+    }
+}
