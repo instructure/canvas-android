98cd2a80aeea777ac716c3ab7df3940af55aec54
Tamas Kozmer <72397075+tamaskozmer@users.noreply.github.com>
2020-11-13 12:50:39 +0100

    [MBL-14885][Teacher] Native view for scheduled items from syllabus summary (#1093)
    refs: MBL-14885
    affects: Teacher
    release note: Added option to open a calendar event from the syllabus.
    
    * Added native view for calendar event in teacher.
    
    * Fixed licenses
    
    * Added new changes based on PR suggestions.

---
+++ apps/teacher/src/main/java/com/instructure/teacher/features/calendar/event/CalendarEventFragment.kt
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.instructure.teacher.features.calendar.event
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import androidx.core.content.ContextCompat
+import com.instructure.canvasapi2.models.CanvasContext
+import com.instructure.canvasapi2.models.ScheduleItem
+import com.instructure.canvasapi2.utils.ApiPrefs
+import com.instructure.interactions.router.Route
+import com.instructure.pandautils.fragments.BaseFragment
+import com.instructure.pandautils.utils.*
+import com.instructure.pandautils.views.CanvasWebView
+import com.instructure.teacher.R
+import com.instructure.teacher.activities.InternalWebViewActivity
+import com.instructure.teacher.fragments.LtiLaunchFragment
+import com.instructure.teacher.router.RouteMatcher
+import kotlinx.android.synthetic.main.fragment_calendar_event.*
+import kotlinx.android.synthetic.main.fragment_syllabus.toolbar
+import kotlinx.coroutines.Job
+import java.net.URLDecoder
+
+class CalendarEventFragment : BaseFragment() {
+
+    private val transformer = CalendarEventStateTransformer()
+
+    private var scheduleItem: ScheduleItem? by NullableParcelableArg(key = SCHEDULE_ITEM)
+
+    private var loadHtmlJob: Job? = null
+
+    override fun layoutResId(): Int {
+        return R.layout.fragment_calendar_event
+    }
+
+    override fun onResume() {
+        super.onResume()
+        calendarEventWebView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        calendarEventWebView?.onPause()
+    }
+
+    override fun onCreateView(view: View) {}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewState = transformer.transformScheduleItem(requireContext(), scheduleItem)
+        applyTheme(viewState)
+        initWebView()
+        setupViews(viewState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        loadHtmlJob?.cancel()
+    }
+
+    private fun applyTheme(viewState: CalendarEventViewState) {
+        toolbar?.title = viewState.eventTitle
+        ViewStyler.themeToolbar(context as Activity, toolbar, canvasContext)
+        toolbar.setupAsBackButton { (context as? Activity)?.onBackPressed() }
+    }
+
+    private fun initWebView() {
+        with(calendarEventWebView) {
+            addVideoClient(requireActivity())
+            canvasEmbeddedWebViewCallback = object : CanvasWebView.CanvasEmbeddedWebViewCallback {
+                override fun launchInternalWebViewFragment(url: String) {
+                    activity?.startActivity(InternalWebViewActivity.createIntent(calendarEventWebView.context, url, "", true))
+                }
+
+                override fun shouldLaunchInternalWebViewFragment(url: String): Boolean = true
+            }
+
+            canvasWebViewClientCallback = object : CanvasWebView.CanvasWebViewClientCallback {
+                override fun openMediaFromWebView(mime: String, url: String, filename: String) {
+                    RouteMatcher.openMedia(activity, url)
+                }
+
+                override fun onPageStartedCallback(webView: WebView, url: String) = Unit
+                override fun onPageFinishedCallback(webView: WebView, url: String) = Unit
+
+                override fun canRouteInternallyDelegate(url: String): Boolean = RouteMatcher.canRouteInternally(requireActivity(), url, ApiPrefs.domain, false)
+
+                override fun routeInternallyCallback(url: String) {
+                    RouteMatcher.canRouteInternally(requireActivity(), url, ApiPrefs.domain, true)
+                }
+            }
+        }
+    }
+
+    private fun setupViews(viewState: CalendarEventViewState) {
+        dateTitle.text = viewState.dateTitle
+        dateSubtitle.text = viewState.dateSubtitle
+        locationTitle.text = viewState.locationTitle
+        locationSubtitle.text = viewState.locationSubtitle
+
+        if (viewState.htmlContent.isNotEmpty()) {
+            loadHtmlJob = calendarEventWebView.loadHtmlWithIframes(requireContext(), isTablet, viewState.htmlContent, ::loadCalendarHtml, { url ->
+                val args = LtiLaunchFragment.makeBundle(canvasContext, URLDecoder.decode(url, "utf-8"), getString(R.string.utils_externalToolTitle), true)
+                RouteMatcher.route(requireContext(), Route(LtiLaunchFragment::class.java, canvasContext, args))
+            }, viewState.eventTitle)
+        }
+    }
+
+    private fun loadCalendarHtml(html: String, contentDescription: String?) {
+        calendarEventWebView?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.canvasBackgroundLight))
+        calendarEventWebView?.loadHtml(html, contentDescription)
+    }
+
+    companion object {
+
+        private const val SCHEDULE_ITEM = "schedule_item"
+
+        fun newInstance(bundle: Bundle): CalendarEventFragment {
+            return CalendarEventFragment().apply {
+                arguments = bundle
+            }
+        }
+
+        fun createArgs(canvasContext: CanvasContext, scheduleItem: ScheduleItem): Bundle {
+            return createBundle(canvasContext).apply {
+                putParcelable(SCHEDULE_ITEM, scheduleItem)
+            }
+        }
+    }
+}
