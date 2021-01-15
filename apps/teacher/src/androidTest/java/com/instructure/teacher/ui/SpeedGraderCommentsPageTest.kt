fa31ae53234beb1d62c19f964fc067b04c7b6b73
Akos Hermann <72087159+hermannakos@users.noreply.github.com>
2021-01-15 13:55:04 +0100

    [MBL-15080][Automation] Swipe up action created (#1172)
    * [MBL-15080][Automation] Swipe up action created
    
    refs: MBL-15080
    affects: Automation
    release note: -
    
    test plan: -
    
    * [MBL-15080][Automation] error fix
    
    refs: MBL-15080
    affects: Automation
    release note: -
    
    test plan: -

--- apps/teacher/src/androidTest/java/com/instructure/teacher/ui/SpeedGraderCommentsPageTest.kt
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/SpeedGraderCommentsPageTest.kt
@@ -26,6 +26,7 @@ import com.instructure.canvasapi2.models.CanvasContextPermission
 import com.instructure.canvasapi2.models.Submission
 import com.instructure.canvasapi2.models.SubmissionComment
 import com.instructure.espresso.randomString
+import com.instructure.espresso.swipeToTop
 import com.instructure.teacher.ui.utils.TeacherTest
 import com.instructure.teacher.ui.utils.tokenLogin
 import org.junit.Test
