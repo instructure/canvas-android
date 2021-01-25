619e28f8095cce86d314bca72ce84aab053a904d
David Varga <david.varga@instructure.com>
2021-01-25 10:03:03 +0100

    Modules E2E tests added for teacher
    refs: MBL-15154
    affects: Teacher
    release note: Extended test matrix
    
    test plan:

---
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/pages/ModulesPage.kt
@@ -0,0 +1,42 @@
+package com.instructure.teacher.ui.pages
+
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import com.instructure.espresso.assertDisplayed
+import com.instructure.espresso.assertNotDisplayed
+import com.instructure.espresso.click
+import com.instructure.espresso.page.*
+import com.instructure.espresso.swipeDown
+import org.hamcrest.Matchers.allOf
+import com.instructure.teacher.R
+
+class ModulesPage : BasePage() {
+
+    fun assertEmptyView() {
+        onView(allOf(withId(R.id.moduleListEmptyView), withAncestor(R.id.moduleList))).assertDisplayed()
+    }
+
+    fun assertModuleIsUnpublished() {
+        onView(withId(R.id.unpublishedIcon)).assertDisplayed()
+    }
+
+    fun assertModuleIsPublished() {
+        onView(withId(R.id.unpublishedIcon)).assertNotDisplayed()
+        onView(withId(R.id.publishedIcon)).assertDisplayed()
+    }
+
+    fun assertModuleIsPresent(moduleTitle: String) {
+        onView(allOf(withId(R.id.moduleName), withText(moduleTitle))).assertDisplayed()
+    }
+
+    fun navigateBack() {
+        onView(withParent(R.id.toolbar) + withContentDescription("Navigate up")).click()
+    }
+
+    fun refresh() {
+        onView(allOf(withId(R.id.swipeRefreshLayout), withAncestor(R.id.moduleList))).swipeDown()
+    }
+
+    fun assertModuleItemIsPresent(itemTitle: String) {
+        onView(allOf(withId(R.id.moduleItemTitle), withText(itemTitle))).assertDisplayed()
+    }
+}
