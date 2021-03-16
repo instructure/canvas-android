abc941133bd973cccc09359fb920f932f11785a2
David Varga <david.varga@instructure.com>
2021-03-16 09:33:35 +0100

    Added scroll to Announcements interaction on course browser
    refs: MBL-15281
    affects: Teacher
    release note: None
    
    test plan:

--- apps/teacher/src/androidTest/java/com/instructure/teacher/ui/pages/CourseBrowserPage.kt
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/pages/CourseBrowserPage.kt
@@ -63,6 +63,7 @@ class CourseBrowserPage : BasePage() {
     }
 
     fun openAnnouncementsTab() {
+        scrollDownToCourseBrowser()
         waitForViewWithText("Announcements").click()
     }
 
