7105ca9b8eaf259d78d66af269d11ab90e5de796
Ákos Hermann <akos.hermann@instructure.com>
2021-04-08 16:39:37 +0200

    select/unselect all

--- apps/student/src/main/java/com/instructure/student/features/dashboard/edit/EditDashboardViewModel.kt
+++ apps/student/src/main/java/com/instructure/student/features/dashboard/edit/EditDashboardViewModel.kt
@@ -24,11 +24,8 @@ import androidx.lifecycle.viewModelScope
 import com.instructure.canvasapi2.managers.CourseManager
 import com.instructure.canvasapi2.managers.GroupManager
 import com.instructure.canvasapi2.models.Course
-import com.instructure.canvasapi2.models.Favorite
 import com.instructure.canvasapi2.models.Group
-import com.instructure.canvasapi2.utils.DataResult
 import com.instructure.canvasapi2.utils.Logger
-import com.instructure.canvasapi2.utils.weave.awaitOrThrow
 import com.instructure.pandautils.mvvm.Event
 import com.instructure.pandautils.mvvm.ItemViewModel
 import com.instructure.pandautils.mvvm.ViewState
@@ -36,10 +33,8 @@ import com.instructure.student.features.dashboard.edit.itemViewModel.EditDashboa
 import com.instructure.student.features.dashboard.edit.itemViewModel.EditDashboardGroupItemViewModel
 import com.instructure.student.features.dashboard.edit.itemViewModel.EditDashboardHeaderViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.collections.associateBy as associateBy
 
 @HiltViewModel
 class EditDashboardViewModel @Inject constructor(private val courseManager: CourseManager,
@@ -63,6 +58,9 @@ class EditDashboardViewModel @Inject constructor(private val courseManager: Cour
     private val favoriteCourseMap: MutableMap<Long, Course> = mutableMapOf()
     private val favoriteGroupMap: MutableMap<Long, Group> = mutableMapOf()
 
+    private lateinit var coursesViewData: List<EditDashboardCourseItemViewModel>
+    private lateinit var groupsViewData: List<EditDashboardGroupItemViewModel>
+
     private lateinit var groupHeader: EditDashboardHeaderViewModel
     private lateinit var courseHeader: EditDashboardHeaderViewModel
 
@@ -76,7 +74,7 @@ class EditDashboardViewModel @Inject constructor(private val courseManager: Cour
 
             try {
                 val courses = courseManager.getCoursesAsync(true).await().dataOrThrow
-                val coursesViewData = courses.map {
+                coursesViewData = courses.map {
                     if (it.isFavorite) {
                         favoriteCourseMap[it.id] = it
                     }
@@ -86,7 +84,7 @@ class EditDashboardViewModel @Inject constructor(private val courseManager: Cour
                 courseHeader = EditDashboardHeaderViewModel("All courses", favoriteCourseMap.isNotEmpty(), ::selectAllCourses, ::deselectAllCourses)
 
                 val groups = groupManager.getAllGroupsAsync(true).await().dataOrThrow
-                val groupsViewData = groups.map {
+                groupsViewData = groups.map {
                     if (it.isFavorite) {
                         favoriteGroupMap[it.id] = it
                     }
@@ -217,19 +215,19 @@ class EditDashboardViewModel @Inject constructor(private val courseManager: Cour
     }
 
     private fun selectAllGroups() {
-
+        groupsViewData.forEach { if (!it.isFavorite) favoriteGroup(it) }
     }
 
     private fun deselectAllGroups() {
-
+        groupsViewData.forEach { if (it.isFavorite) unfavoriteGroup(it) }
     }
 
     private fun selectAllCourses() {
-
+        coursesViewData.forEach { if (!it.isFavorite) favoriteCourse(it) }
     }
 
     private fun deselectAllCourses() {
-
+        coursesViewData.forEach { if (it.isFavorite) unfavoriteCourse(it) }
     }
 
 }
