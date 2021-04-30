fc876edf02e2fa670e3e83dbe55073cf9403246b
Ákos Hermann <akos.hermann@instructure.com>
2021-04-30 17:28:10 +0200

    typefaceutil added
    refs:
    affects:
    release note:
    
    test plan:

---
+++ libs/pandautils/src/main/java/com/instructure/pandautils/utils/TypefaceUtil.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 - present Instructure, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.instructure.pandautils.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import java.lang.reflect.Field
+
+object TypefaceUtil {
+
+    fun overrideFont(context: Context, fontsToOverride: Map<String, String>) {
+        try {
+            val fontMap = fontsToOverride.mapValues { Typeface.createFromAsset(context.assets, it.value) }
+            val staticField: Field = Typeface::class.java
+                    .getDeclaredField("sSystemFontMap")
+            staticField.isAccessible = true
+            staticField.set(null, fontMap)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
