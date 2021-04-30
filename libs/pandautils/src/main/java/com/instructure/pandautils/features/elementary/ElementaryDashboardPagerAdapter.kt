346ff13a1ad1a1df25f807fd9e9ee8db013d9707
Tamas Kozmer <tamas.kozmer@instructure.com>
2021-04-30 14:59:09 +0200

    PR changes.

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/elementary/ElementaryDashboardPagerAdapter.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/elementary/ElementaryDashboardPagerAdapter.kt
@@ -24,31 +24,24 @@ import com.instructure.pandautils.features.elementary.grades.GradesFragment
 import com.instructure.pandautils.features.elementary.homeroom.HomeroomFragment
 import com.instructure.pandautils.features.elementary.resources.ResourcesFragment
 import com.instructure.pandautils.features.elementary.schedule.ScheduleFragment
-import java.lang.UnsupportedOperationException
-
-private const val PAGES_COUNT = 4
-
-private const val HOMEROOM_TAB_POSITION = 0
-private const val SCHEDULE_TAB_POSITION = 1
-private const val GRADES_TAB_POSITION = 2
-private const val RESOURCES_TAB_POSITION = 3
 
 class ElementaryDashboardPagerAdapter(
     private val canvasContext: CanvasContext,
     fragmentManager: FragmentManager
 ) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
+    private val fragments = listOf(
+        HomeroomFragment.newInstance(),
+        ScheduleFragment.newInstance(),
+        GradesFragment.newInstance(),
+        ResourcesFragment.newInstance()
+    )
+
     override fun getItem(position: Int): Fragment {
-        return when (position) {
-            HOMEROOM_TAB_POSITION -> HomeroomFragment.newInstance()
-            SCHEDULE_TAB_POSITION -> ScheduleFragment.newInstance()
-            GRADES_TAB_POSITION -> GradesFragment.newInstance()
-            RESOURCES_TAB_POSITION -> ResourcesFragment.newInstance()
-            else -> throw UnsupportedOperationException()
-        }
+        return fragments[position]
     }
 
     override fun getCount(): Int {
-        return PAGES_COUNT
+        return fragments.size
     }
 }
