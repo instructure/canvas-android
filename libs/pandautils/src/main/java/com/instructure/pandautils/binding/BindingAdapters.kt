17b635c67c39b1adad2a396e2ec287f144e94625
Tamas Kozmer <tamas.kozmer@instructure.com>
2021-07-27 09:50:46 +0200

    Added post to imageview to set image when card is measured.

--- libs/pandautils/src/main/java/com/instructure/pandautils/binding/BindingAdapters.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/binding/BindingAdapters.kt
@@ -122,7 +122,9 @@ private class JSInterface(private val onLtiButtonPressed: OnLtiButtonPressed) {
 @BindingAdapter(value = ["imageUrl", "overlayColor"], requireAll = true)
 fun bindImageWithOverlay(imageView: ImageView, imageUrl: String?, overlayColor: Int?) {
     if (overlayColor != null) {
-        imageView.setCourseImage(imageUrl, overlayColor, true)
+        imageView.post {
+            imageView.setCourseImage(imageUrl, overlayColor, true)
+        }
     }
 }
 
