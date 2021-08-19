946199a0b98acf28932b003e56333c3c9f1a63a4
Tamas Kozmer <tamas.kozmer@instructure.com>
2021-08-19 11:22:09 +0200

    Added color parsing for 3-digit format.

--- libs/pandautils/src/main/java/com/instructure/pandautils/utils/ColorUtils.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/utils/ColorUtils.kt
@@ -43,4 +43,19 @@ object ColorUtils {
         canvas.drawBitmap(mutableBitmap, 0f, 0f, paint)
         return mutableBitmap
     }
+
+    @JvmStatic
+    @JvmOverloads
+    fun parseColor(colorCode: String, defaultColorCode: String = ColorApiHelper.K5_DEFAULT_COLOR): Int {
+        return try {
+            val fullColorCode = if (colorCode.length == 4 && colorCode[0].toString() == "#") {
+                "#${colorCode[1]}${colorCode[1]}${colorCode[2]}${colorCode[2]}${colorCode[3]}${colorCode[3]}"
+            } else {
+                colorCode
+            }
+            Color.parseColor(fullColorCode)
+        } catch (e: IllegalArgumentException) {
+            Color.parseColor(defaultColorCode)
+        }
+    }
 }
