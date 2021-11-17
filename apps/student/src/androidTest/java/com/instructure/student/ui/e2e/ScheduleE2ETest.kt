102fd1d35276f3f5931382e6a31a318ac2e9ee97
kdeakinstructure <kristof.deak@instructure.com>
2021-11-17 14:08:21 +0100

    refs: MBL-15742 affects: Student release note: none
    ScheduleE2E test basics and config.

---
+++ apps/student/src/androidTest/java/com/instructure/student/ui/e2e/ScheduleE2ETest.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.instructure.student.ui.e2e
+
+import com.instructure.canvas.espresso.E2E
+import com.instructure.canvasapi2.utils.toApiString
+import com.instructure.dataseeding.api.AssignmentsApi
+import com.instructure.dataseeding.model.GradingType
+import com.instructure.dataseeding.model.SubmissionType
+import com.instructure.panda_annotations.FeatureCategory
+import com.instructure.panda_annotations.Priority
+import com.instructure.panda_annotations.TestCategory
+import com.instructure.panda_annotations.TestMetaData
+import com.instructure.student.ui.utils.StudentTest
+import com.instructure.student.ui.utils.seedDataForK5
+import com.instructure.student.ui.utils.tokenLoginElementary
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Test
+import java.util.*
+
+@HiltAndroidTest
+class ScheduleE2ETest : StudentTest() {
+
+    override fun displaysPageObjects() = Unit
+
+    @E2E
+    @Test
+    @TestMetaData(Priority.P0, FeatureCategory.K5_DASHBOARD, TestCategory.E2E)
+    fun scheduleE2ETest() {
+
+        // Seed data for K5 sub-account
+        val data = seedDataForK5(
+            teachers = 1,
+            students = 1,
+            courses = 4,
+            homeroomCourses = 1,
+            announcements = 3
+        )
+        val student = data.studentsList[0]
+        val teacher = data.teachersList[0]
+        val homeroomCourse = data.coursesList[0]
+        val homeroomAnnouncement = data.announcementsList[0]
+        val nonHomeroomCourses = data.coursesList.filter { !it.homeroomCourse }
+
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 23)
+        calendar.set(Calendar.MINUTE, 59)
+        calendar.set(Calendar.SECOND, 55)
+
+        val missingCalendar = Calendar.getInstance()
+        missingCalendar.set(Calendar.HOUR_OF_DAY, 0)
+        missingCalendar.set(Calendar.MINUTE, 1)
+        missingCalendar.set(Calendar.SECOND, 10)
+
+        val testAssignment = AssignmentsApi.createAssignment(
+            AssignmentsApi.CreateAssignmentRequest(
+                courseId = nonHomeroomCourses[2].id,
+                submissionTypes = listOf(SubmissionType.ONLINE_TEXT_ENTRY),
+                gradingType = GradingType.LETTER_GRADE,
+                teacherToken = teacher.token,
+                pointsPossible = 100.0,
+                dueAt = calendar.time.toApiString()
+            )
+        )
+
+        // Sign in with elementary (K5) student
+        tokenLoginElementary(student)
+        elementaryDashboardPage.waitForRender()
+        elementaryDashboardPage.selectScheduleTab()
+        schedulePage.assertPageObjects()
+
+        schedulePage.scrollToPosition(10)
+        schedulePage.assertCourseHeaderDisplayed(homeroomCourse.name)
+        schedulePage.assertScheduleItemDisplayed(homeroomAnnouncement.title)
+    }
+
+}
+
