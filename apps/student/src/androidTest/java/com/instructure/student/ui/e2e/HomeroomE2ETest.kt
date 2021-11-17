b1dc7c0efd397068e59f9efab76ff6a82f0c726e
kdeakinstructure <kristof.deak@instructure.com>
2021-11-17 17:08:03 +0100

    refs: MBL-15716 affects: Student release note: none
    set calendar to UTC timezone (to be able to run the test locally and on Bitrise as well)

--- apps/student/src/androidTest/java/com/instructure/student/ui/e2e/HomeroomE2ETest.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/e2e/HomeroomE2ETest.kt
@@ -34,6 +34,7 @@ import com.instructure.student.ui.utils.seedDataForK5
 import com.instructure.student.ui.utils.tokenLoginElementary
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Test
+import java.text.SimpleDateFormat
 import java.util.*
 
 @HiltAndroidTest
@@ -62,10 +63,19 @@ class HomeroomE2ETest : StudentTest() {
         val homeroomAnnouncement = data.announcementsList[0]
         val nonHomeroomCourses = data.coursesList.filter { !it.homeroomCourse }
 
-        val calendar = Calendar.getInstance()
+        val utcTimeZone = TimeZone.getTimeZone("UTC")
+        val calendar = Calendar.getInstance(utcTimeZone)
+
         calendar.set(Calendar.HOUR_OF_DAY, 13)
         calendar.set(Calendar.MINUTE, 59)
         calendar.set(Calendar.SECOND, 55)
+
+        calendar.set(Calendar.HOUR_OF_DAY, 23)
+        calendar.set(Calendar.MINUTE, 59)
+        calendar.set(Calendar.SECOND, 55)
+
+        val simpleDateFormat = SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy", Locale.US)
+        simpleDateFormat.setTimeZone(utcTimeZone)
 
         val missingCalendar = Calendar.getInstance()
         missingCalendar.set(Calendar.HOUR_OF_DAY, 0)
