7b43ea88fae4fdab989788016e3bc0a682ab83a7
Tamas Kozmer <tamas.kozmer@instructure.com>
2021-12-02 13:17:49 +0100

    Fixed unit tests.

--- apps/flutter_parent/test/screens/manage_students/student_color_picker_interactor_test.dart
+++ apps/flutter_parent/test/screens/manage_students/student_color_picker_interactor_test.dart
@@ -13,6 +13,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_parent/models/color_change_response.dart';
 import 'package:flutter_parent/models/login.dart';
 import 'package:flutter_parent/models/user_color.dart';
 import 'package:flutter_parent/network/api/user_api.dart';
@@ -27,14 +28,14 @@ import '../../utils/test_app.dart';
 import '../../utils/test_helpers/mock_helpers.dart';
 import '../dashboard/dashboard_interactor_test.dart';
 
-void main() {
+void main() async {
   Login login = Login((b) => b
     ..user = CanvasModelTestUtils.mockUser(id: '123').toBuilder()
     ..domain = 'test-domain');
   MockUserApi userApi = MockUserApi();
   MockUserColorsDb db = MockUserColorsDb();
 
-  setupTestLocator((locator) {
+  await setupTestLocator((locator) {
     locator.registerLazySingleton<UserApi>(() => userApi);
     locator.registerLazySingleton<UserColorsDb>(() => db);
   });
@@ -49,13 +50,36 @@ void main() {
     String studentId = '456';
     Color color = Colors.pinkAccent;
 
+    final colorChangeResponse =
+        ColorChangeResponse((b) => b..hexCode = '#123456');
+    when(userApi.setUserColor('user_456', color))
+        .thenAnswer((_) async => Future.value(colorChangeResponse));
+
     await StudentColorPickerInteractor().save(studentId, color);
     verify(userApi.setUserColor('user_456', color));
+  });
+
+  test('Throws exception when color change response color is null', () async {
+    String studentId = '456';
+    Color color = Colors.pinkAccent;
+
+    final colorChangeResponse = ColorChangeResponse((b) => b..hexCode = null);
+    when(userApi.setUserColor('user_456', color))
+        .thenAnswer((_) async => Future.value(colorChangeResponse));
+
+    expect(
+        () async => await StudentColorPickerInteractor().save(studentId, color),
+        throwsException);
   });
 
   test('Calls database with correct data', () async {
     String studentId = '456';
     Color color = Colors.pinkAccent;
+
+    final colorChangeResponse =
+        ColorChangeResponse((b) => b..hexCode = '#123456');
+    when(userApi.setUserColor('user_456', color))
+        .thenAnswer((_) async => Future.value(colorChangeResponse));
 
     UserColor expectedData = UserColor((b) => b
       ..userId = login.user.id
