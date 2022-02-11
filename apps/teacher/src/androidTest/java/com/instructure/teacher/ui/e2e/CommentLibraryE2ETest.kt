c49459a5d5e6d738f4dcbaf597956d6f740c9c24
kdeakinstructure <kristof.deak@instructure.com>
2022-02-11 17:08:58 +0100

    initial commit for comment library e2e test.
    refs: MBL-15883
    affects: Teacher
    release note: none

---
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/e2e/CommentLibraryE2ETest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.instructure.teacher.ui.e2e
+
+import com.instructure.canvas.espresso.E2E
+import com.instructure.panda_annotations.FeatureCategory
+import com.instructure.panda_annotations.Priority
+import com.instructure.panda_annotations.TestCategory
+import com.instructure.panda_annotations.TestMetaData
+import com.instructure.teacher.ui.utils.TeacherTest
+import com.instructure.teacher.ui.utils.seedData
+import com.instructure.teacher.ui.utils.tokenLogin
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Test
+
+@HiltAndroidTest
+class CommentLibraryE2ETest : TeacherTest() {
+
+    override fun displaysPageObjects() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    @E2E
+    @Test
+    @TestMetaData(Priority.P0, FeatureCategory.DASHBOARD, TestCategory.E2E)
+    fun testCommentLibraryE2E() {
+
+        val data = seedData(teachers = 1, courses = 2)
+        val teacher = data.teachersList[0]
+
+        tokenLogin(teacher)
+
+
+    }
+}
