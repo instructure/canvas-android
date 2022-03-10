c688aa63ecab3e29b34a171f927f29a47e943acb
Tamas Kozmer <72397075+tamaskozmer@users.noreply.github.com>
2022-03-10 16:23:46 +0100

    [MBL-15937][Student] Group discussion reply causes anomaly (#1520)
    refs: MBL-15937
    affects: Student
    release note: None
    
    * Do not send discussion update event in case of group discussion.
    
    * Fixed failing test

--- apps/student/src/test/java/com/instructure/student/test/util/ModuleUtilityTest.kt
+++ apps/student/src/test/java/com/instructure/student/test/util/ModuleUtilityTest.kt
@@ -238,6 +238,7 @@ class ModuleUtilityTest : TestCase() {
         expectedBundle.putParcelable(Const.CANVAS_CONTEXT, course)
         expectedBundle.putLong(DiscussionDetailsFragment.DISCUSSION_TOPIC_HEADER_ID, 123456789)
         expectedBundle.putString(DiscussionDetailsFragment.DISCUSSION_TITLE, null)
+        expectedBundle.putBoolean(DiscussionDetailsFragment.GROUP_DISCUSSION, false)
         val parentFragment = callGetFragment(moduleItem, course, null)
         TestCase.assertNotNull(parentFragment)
         TestCase.assertEquals(DiscussionDetailsFragment::class.java, parentFragment!!.javaClass)
