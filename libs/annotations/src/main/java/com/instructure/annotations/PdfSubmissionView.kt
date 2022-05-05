86aa05f48c1571fb2e5a614b842c59baf6f77bbd
Tamas Kozmer <tamas.kozmer@instructure.com>
2022-05-05 12:49:43 +0200

    Fixed PSPDFKit styles in Student. Made some UI changes also in light mode.

--- libs/annotations/src/main/java/com/instructure/annotations/PdfSubmissionView.kt
+++ libs/annotations/src/main/java/com/instructure/annotations/PdfSubmissionView.kt
@@ -241,7 +241,6 @@ abstract class PdfSubmissionView(context: Context) : FrameLayout(context), Annot
 
         val marginDp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12f, context.resources.displayMetrics)
         val layoutParams = commentsButton.layoutParams as LayoutParams
-        commentsButton.drawable.setTint(Color.WHITE)
         layoutParams.gravity = Gravity.END or Gravity.TOP
         layoutParams.topMargin = marginDp.toInt() + actionBarDp
         layoutParams.rightMargin = marginDp.toInt()
