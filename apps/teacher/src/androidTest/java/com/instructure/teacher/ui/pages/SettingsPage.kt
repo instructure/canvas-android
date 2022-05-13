dab64b36176743890934f62034b06499bf737105
kdeakinstructure <kristof.deak@instructure.com>
2022-05-13 16:43:54 +0200

    Implement Teacher Settings E2E test. Give some ids to page layouts to make them automation friendly. Fix legacy typo issue discussionsDetatailsPage to discussionsDetailsPage
    refs: MBL-16022
    affects: Teacher
    release note: none

---
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/pages/SettingsPage.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.instructure.teacher.ui.pages
+
+import com.instructure.espresso.OnViewWithId
+import com.instructure.espresso.click
+import com.instructure.espresso.page.BasePage
+import com.instructure.espresso.scrollTo
+import com.instructure.teacher.R
+
+class SettingsPage : BasePage(R.id.settingsPage) {
+    private val toolbar by OnViewWithId(R.id.toolbar)
+    private val profileSettingLabel by OnViewWithId(R.id.profileButton)
+    private val pushNotificationsLabel by OnViewWithId(R.id.notificationPreferenesButton)
+    private val rateAppLabel by OnViewWithId(R.id.rateButton)
+    private val legalLabel by OnViewWithId(R.id.legalButton)
+    private val featureFlagLabel by OnViewWithId(R.id.featureFlagButton)
+    private val remoteConfigLabel by OnViewWithId(R.id.remoteConfigButton)
+
+    fun openProfileSettingsPage() {
+        profileSettingLabel.scrollTo().click()
+    }
+
+    fun openPushNotificationsPage() {
+        pushNotificationsLabel.scrollTo().click()
+    }
+
+    fun openRateAppDialog() {
+        rateAppLabel.scrollTo().click()
+    }
+
+    fun openLegalPage() {
+        legalLabel.scrollTo().click()
+    }
+
+    fun openFeatureFlagsPage() {
+        featureFlagLabel.scrollTo().click()
+    }
+
+    fun openRemoteConfigParamsPage() {
+        remoteConfigLabel.scrollTo().click()
+    }
+}
