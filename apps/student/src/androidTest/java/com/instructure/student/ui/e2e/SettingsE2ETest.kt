7beef4689b01d71d4bfc630241f107cc0ebd5fe5
kdeakinstructure <kristof.deak@instructure.com>
2022-06-13 15:23:59 +0200

    Extend SettingsE2ETest in Student.
    refs: MBL-16102
    affects: Student
    release note: none

--- apps/student/src/androidTest/java/com/instructure/student/ui/e2e/SettingsE2ETest.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/e2e/SettingsE2ETest.kt
@@ -21,14 +21,17 @@ import androidx.test.espresso.Espresso
 import com.instructure.canvas.espresso.E2E
 import com.instructure.canvasapi2.utils.RemoteConfigParam
 import com.instructure.canvasapi2.utils.RemoteConfigUtils
+import com.instructure.espresso.ViewUtils
 import com.instructure.panda_annotations.FeatureCategory
 import com.instructure.panda_annotations.Priority
 import com.instructure.panda_annotations.TestCategory
 import com.instructure.panda_annotations.TestMetaData
+import com.instructure.student.R
 import com.instructure.student.ui.utils.StudentTest
 import com.instructure.student.ui.utils.seedData
 import com.instructure.student.ui.utils.tokenLogin
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert
 import org.junit.Test
 
 @HiltAndroidTest
@@ -44,7 +47,69 @@ class SettingsE2ETest : StudentTest() {
     @E2E
     @Test
     @TestMetaData(Priority.MANDATORY, FeatureCategory.SETTINGS, TestCategory.E2E)
-    fun testSettingsE2E() {
+    fun testProfileSettingsE2E() {
+
+        Log.d(PREPARATION_TAG, "Seeding data.")
+        val data = seedData(students = 1, teachers = 1, courses = 1)
+        val teacher = data.teachersList[0]
+
+        Log.d(STEP_TAG, "Login with user: ${teacher.name}, login id: ${teacher.loginId} , password: ${teacher.password}")
+        tokenLogin(teacher)
+        dashboardPage.waitForRender()
+
+        Log.d(STEP_TAG,"Navigate to User Settings Page.")
+        dashboardPage.launchSettingsPage()
+        settingsPage.assertPageObjects()
+
+        Log.d(STEP_TAG,"Open Profile Settings Page.")
+        settingsPage.launchProfileSettings()
+        profileSettingsPage.assertPageObjects()
+
+        val newUserName = "John Doe"
+        Log.d(STEP_TAG,"Edit username to: $newUserName. Click on 'Save' button.")
+        profileSettingsPage.changeUserNameTo(newUserName)
+
+        Log.d(STEP_TAG,"Navigate back to Dashboard Page. Assert that the username has been changed to $newUserName.")
+        ViewUtils.pressBackButton(2)
+        dashboardPage.assertUserLoggedIn(newUserName)
+
+        val originalSavedPandaAvatarCount = getSavedPandaAvatarCount()
+
+        Log.d(STEP_TAG,"Navigate to Settings Page again and open Panda Avatar Creator.")
+        dashboardPage.launchSettingsPage()
+        settingsPage.assertPageObjects()
+        settingsPage.launchProfileSettings()
+        profileSettingsPage.assertPageObjects()
+        profileSettingsPage.launchPandaAvatarCreator()
+
+        Log.d(STEP_TAG,"Set panda avatar head.")
+        pandaAvatarPage.selectChangeHead()
+        pandaAvatarPage.choosePart(R.string.content_description_panda_head_4)
+        pandaAvatarPage.clickBackButton()
+
+        Log.d(STEP_TAG,"Set panda avatar body.")
+        pandaAvatarPage.selectChangeBody()
+        pandaAvatarPage.choosePart(R.string.content_description_panda_body_4)
+        pandaAvatarPage.clickBackButton()
+
+        Log.d(STEP_TAG,"Set panda avatar legs.")
+        pandaAvatarPage.selectChangeLegs()
+        pandaAvatarPage.choosePart(R.string.content_description_panda_feet_5)
+        pandaAvatarPage.clickBackButton()
+
+        Log.d(STEP_TAG,"Click on 'Save as avatar' button.")
+        pandaAvatarPage.save()
+
+        val newSavedPandaAvatarCount = getSavedPandaAvatarCount()
+        Log.d(STEP_TAG,"Assert that saved panda avatar count has increased by one. Old value: $originalSavedPandaAvatarCount, new value: $newSavedPandaAvatarCount.")
+        Assert.assertTrue(newSavedPandaAvatarCount == originalSavedPandaAvatarCount + 1)
+
+    }
+
+    @E2E
+    @Test
+    @TestMetaData(Priority.MANDATORY, FeatureCategory.SETTINGS, TestCategory.E2E)
+    fun testLegalPageE2E() {
 
         Log.d(PREPARATION_TAG, "Seeding data.")
         val data = seedData(students = 1, teachers = 1, courses = 1)
@@ -61,6 +126,32 @@ class SettingsE2ETest : StudentTest() {
         Log.d(STEP_TAG,"Click on 'Legal' link to open Legal Page. Assert that Legal Page has opened.")
         settingsPage.launchLegalPage()
         legalPage.assertPageObjects()
+    }
+
+    @E2E
+    @Test
+    @TestMetaData(Priority.IMPORTANT, FeatureCategory.SETTINGS, TestCategory.E2E)
+    fun testAboutE2E() {
+
+        Log.d(PREPARATION_TAG, "Seeding data.")
+        val data = seedData(students = 1, teachers = 1, courses = 1)
+        val student = data.studentsList[0]
+
+        Log.d(STEP_TAG, "Login with user: ${student.name}, login id: ${student.loginId} , password: ${student.password}")
+        tokenLogin(student)
+        dashboardPage.waitForRender()
+
+        Log.d(STEP_TAG,"Navigate to Settings Page on the left-side menu.")
+        dashboardPage.launchSettingsPage()
+        settingsPage.assertPageObjects()
+
+        Log.d(STEP_TAG,"Click on 'About' link to open About Page. Assert that About Page has opened.")
+        settingsPage.launchAboutPage()
+        aboutPage.assertPageObjects()
+        aboutPage.domainIs(student.domain)
+        aboutPage.loginIdIs(student.loginId)
+        aboutPage.emailIs(student.loginId)
+
     }
 
     //The remote config settings page only available on debug builds.
