b6233dd3e8b5ab6c11b9d0ddc315429fdc51dbe4
Tamas Kozmer <tamas.kozmer@instructure.com>
2022-07-06 13:57:56 +0200

    Fixed test build failure.

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/DashboardPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/DashboardPage.kt
@@ -164,6 +164,7 @@ class DashboardPage : BasePage(R.id.dashboardPage) {
 
     fun assertUnreadEmails(count: Int) {
         onView(allOf(withParent(R.id.bottomNavigationInbox), withId(R.id.badge), withText(count.toString()))).assertDisplayed()
+        onView(withId(R.id.bottomBar)).check(NotificationBadgeAssertion(R.id.bottomNavigationInbox, count))
     }
 
     fun clickCalendarTab() {
