0ecc76f4cbb514588807ac8023520229868d46e9
Ákos Hermann <akos.hermann@instructure.com>
2022-07-20 10:37:54 +0200

    refactor

--- apps/student/src/main/java/com/instructure/student/activity/ShareFileUploadActivity.kt
+++ apps/student/src/main/java/com/instructure/student/activity/ShareFileUploadActivity.kt
@@ -1,19 +1,19 @@
- /*
- * Copyright (C) 2016 - present Instructure, Inc.
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, version 3 of the License.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+/*
+* Copyright (C) 2016 - present Instructure, Inc.
+*
+*     This program is free software: you can redistribute it and/or modify
+*     it under the terms of the GNU General Public License as published by
+*     the Free Software Foundation, version 3 of the License.
+*
+*     This program is distributed in the hope that it will be useful,
+*     but WITHOUT ANY WARRANTY; without even the implied warranty of
+*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*     GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 package com.instructure.student.activity
 
@@ -21,8 +21,6 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
-import android.content.DialogInterface
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
@@ -30,29 +28,20 @@ import android.view.ViewTreeObserver
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
-import androidx.lifecycle.viewModelScope
-import com.instructure.canvasapi2.managers.CourseManager
 import com.instructure.canvasapi2.models.Assignment
 import com.instructure.canvasapi2.models.CanvasContext
 import com.instructure.canvasapi2.models.Course
 import com.instructure.canvasapi2.models.StorageQuotaExceededError
-import com.instructure.canvasapi2.utils.weave.awaitApi
-import com.instructure.canvasapi2.utils.weave.catch
-import com.instructure.canvasapi2.utils.weave.tryWeave
-import com.instructure.pandautils.dialogs.UploadFilesDialog
 import com.instructure.pandautils.features.file.upload.FileUploadDialogFragment
 import com.instructure.pandautils.features.file.upload.FileUploadType
 import com.instructure.pandautils.features.shareextension.ShareExtensionAction
-import com.instructure.pandautils.utils.*
-import com.instructure.student.R
-import com.instructure.student.dialog.ShareFileDestinationDialog
 import com.instructure.pandautils.features.shareextension.ShareExtensionViewModel
 import com.instructure.pandautils.features.shareextension.target.ShareExtensionTargetFragment
+import com.instructure.pandautils.utils.*
+import com.instructure.student.R
 import com.instructure.student.util.Analytics
-import com.instructure.pandautils.utils.AnimationHelpers
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.activity_share_file.*
@@ -60,27 +49,23 @@ import kotlinx.coroutines.Job
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
-import java.util.*
 
 @Parcelize
 data class ShareFileSubmissionTarget(
-    val course: Course,
-    val assignment: Assignment
+        val course: Course,
+        val assignment: Assignment
 ) : Parcelable
 
 @AndroidEntryPoint
-class ShareFileUploadActivity : AppCompatActivity(), ShareFileDestinationDialog.DialogCloseListener {
+class ShareFileUploadActivity : AppCompatActivity() {
 
     private val shareExtensionViewModel: ShareExtensionViewModel by viewModels()
 
-    private val PERMISSION_REQUEST_WRITE_STORAGE = 0
-
     private var loadCoursesJob: Job? = null
     private var uploadFileSourceFragment: DialogFragment? = null
-    private var courses: ArrayList<Course>? = null
 
     private val submissionTarget: ShareFileSubmissionTarget? by lazy {
-        intent?.extras?.getParcelable<ShareFileSubmissionTarget>(Const.SUBMISSION_TARGET)
+        intent?.extras?.getParcelable(Const.SUBMISSION_TARGET)
     }
 
     private var sharedURI: Uri? = null
@@ -95,17 +80,10 @@ class ShareFileUploadActivity : AppCompatActivity(), ShareFileDestinationDialog.
             sharedURI = shareExtensionViewModel.parseIntentType(intent)
             shareExtensionViewModel.uri = sharedURI
             if (submissionTarget != null) {
-                // If targeted for submission, skip the picker and go immediately to the submission workflow
-                val bundle = UploadFilesDialog.createAssignmentBundle(
-                    sharedURI,
-                    submissionTarget!!.course,
-                    submissionTarget!!.assignment
-                )
-                onNext(bundle)
+                handleAction(ShareExtensionAction.ShowUploadDialog(submissionTarget!!.course, submissionTarget!!.assignment, sharedURI!!, FileUploadType.ASSIGNMENT))
             } else {
-                getCourses()
+                showDestinationDialog()
             }
-            askForStoragePermissionIfNecessary()
         } else {
             exitActivity()
         }
@@ -140,51 +118,6 @@ class ShareFileUploadActivity : AppCompatActivity(), ShareFileDestinationDialog.
                     it.start()
                 }
             }
-        }
-    }
-
-    private fun askForStoragePermissionIfNecessary() {
-        if ((sharedURI?.scheme?.equals("file") == true || sharedURI?.scheme?.equals("content") == true) && !PermissionUtils.hasPermissions(this, PermissionUtils.WRITE_EXTERNAL_STORAGE)) {
-            ActivityCompat.requestPermissions(this, PermissionUtils.makeArray(PermissionUtils.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_WRITE_STORAGE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == UploadFilesDialog.CAMERA_PIC_REQUEST ||
-                requestCode == UploadFilesDialog.PICK_FILE_FROM_DEVICE ||
-                requestCode == UploadFilesDialog.PICK_IMAGE_GALLERY) {
-            //File Dialog Fragment will not be notified of onActivityResult(), alert manually
-            OnActivityResults(ActivityResult(requestCode, resultCode, data), null).postSticky()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode) {
-            PERMISSION_REQUEST_WRITE_STORAGE -> {
-                if (!PermissionUtils.allPermissionsGrantedResultSummary(grantResults)) {
-                    Toast.makeText(this, R.string.permissionDenied, Toast.LENGTH_LONG).show()
-                    finish()
-                }
-            }
-        }
-    }
-
-    private fun getCourses() {
-        loadCoursesJob = tryWeave {
-            val courses = awaitApi<List<Course>> { CourseManager.getCourses(true, it) }
-            if (courses.isNotEmpty()) {
-                this@ShareFileUploadActivity.courses = ArrayList(courses)
-                if (uploadFileSourceFragment == null) showDestinationDialog()
-            } else {
-                Toast.makeText(applicationContext, R.string.uploadingFromSourceFailed, Toast.LENGTH_LONG).show()
-                exitActivity()
-            }
-        } catch {
-            Toast.makeText(this@ShareFileUploadActivity, R.string.uploadingFromSourceFailed, Toast.LENGTH_LONG).show()
-            exitActivity()
         }
     }
 
@@ -228,16 +161,10 @@ class ShareFileUploadActivity : AppCompatActivity(), ShareFileDestinationDialog.
         if (sharedURI == null) {
             Toast.makeText(applicationContext, R.string.uploadingFromSourceFailed, Toast.LENGTH_LONG).show()
         } else {
-            ShareExtensionTargetFragment().show(supportFragmentManager, ShareExtensionTargetFragment.TAG)
-            /*uploadFileSourceFragment = ShareFileDestinationDialog.newInstance(ShareFileDestinationDialog.createBundle(sharedURI!!, courses!!))
-            uploadFileSourceFragment!!.show(supportFragmentManager, ShareFileDestinationDialog.TAG)*/
+            uploadFileSourceFragment = ShareExtensionTargetFragment()
+            uploadFileSourceFragment!!.show(supportFragmentManager, ShareExtensionTargetFragment.TAG)
         }
     }
-
-    override fun onCancel(dialog: DialogInterface?) {
-        finish()
-    }
-
 
     @Suppress("unused", "UNUSED_PARAMETER")
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -246,7 +173,7 @@ class ShareFileUploadActivity : AppCompatActivity(), ShareFileDestinationDialog.
     }
 
     private fun getColor(bundle: Bundle?): Int {
-        return if(bundle != null && bundle.containsKey(Const.CANVAS_CONTEXT)) {
+        return if (bundle != null && bundle.containsKey(Const.CANVAS_CONTEXT)) {
             val color = ColorKeeper.getOrGenerateColor(bundle.getParcelable<Parcelable>(Const.CANVAS_CONTEXT) as CanvasContext)
             ViewStyler.setStatusBarDark(this, color)
             color
@@ -254,23 +181,6 @@ class ShareFileUploadActivity : AppCompatActivity(), ShareFileDestinationDialog.
             val color = ContextCompat.getColor(this, R.color.login_studentAppTheme)
             ViewStyler.setStatusBarDark(this, color)
             color
-        }
-    }
-
-    override fun onNext(bundle: Bundle) {
-        ValueAnimator.ofObject(ArgbEvaluator(), ContextCompat.getColor(this, R.color.login_studentAppTheme), getColor(bundle)).let {
-            it.addUpdateListener { animation -> rootView!!.setBackgroundColor(animation.animatedValue as Int) }
-            it.duration = 500
-            it.addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationStart(animation: Animator) {
-                    UploadFilesDialog.show(supportFragmentManager, bundle) { event ->
-                        if(event == UploadFilesDialog.EVENT_ON_UPLOAD_BEGIN || event == UploadFilesDialog.EVENT_DIALOG_CANCELED) {
-                            finish()
-                        }
-                    }
-                }
-            })
-            it.start()
         }
     }
 }
