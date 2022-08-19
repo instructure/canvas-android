c1537a4550f061d4715a0dce6eb71c97f4b557f3
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2022-08-19 15:51:45 +0200

    Fix some nightly tests. (#1679)
    * fix profileSettingsE2E on nightly (we are using different device than in local thats what caused the flakyness)
    
    refs:
    affects:
    release note:
    
    test plan:
    
    * attempt to fix testAnnouncement_reply test case (it may do not wait enough for the webview to be loaded)
    
    refs:
    affects:
    release note:
    
    test plan:
    
    * stub testFilters method on landscape mode because on lowres device its too narrow to scroll properly. Will be checked/refactored when we migrated from lowres device in nightly runs.
    
    refs:
    affects: Student
    release note: none
    
    * put back todoe2e tests on weekends. (to test on bitrise)
    
    refs:
    affects: Student, Teacher
    release note: none
    
    * put back todoe2e tests on weekends. (to test on bitrise)
    
    refs:
    affects: Student, Teacher
    release note: none
    
    * Fix breaking render tests on 'multi_api_level' jobs. (It was stubbed on Teacher because of the same issue, now, both Teacher and Student were fixed so Stubs has removed as well.)
    
    refs:
    affects:
    release note:
    
    test plan:
    
    * fix some nightly tests.
    
    * remove unnecessary refresh after sending reply --> fix another nightly test.
    
    * give some time to the sent reply to propagate
    
    * put back refresh to get the attachment icon.

--- apps/teacher/src/androidTest/java/com/instructure/teacher/ui/e2e/PagesE2ETest.kt
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/e2e/PagesE2ETest.kt
@@ -20,9 +20,7 @@ import org.junit.Test
 class PagesE2ETest : TeacherTest() {
     override fun displaysPageObjects() = Unit
 
-    override fun enableAndConfigureAccessibilityChecks() {
-        //We don't want to see accessibility errors on E2E tests
-    }
+    override fun enableAndConfigureAccessibilityChecks() = Unit
 
     @E2E
     @Test
