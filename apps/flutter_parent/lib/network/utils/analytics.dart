eafea6ed535246402b633c370f9fa7a20d89489e
Ákos Hermann <akos.hermann@instructure.com>
2023-01-03 15:18:57 +0100

    flutter analytics

--- apps/flutter_parent/lib/network/utils/analytics.dart
+++ apps/flutter_parent/lib/network/utils/analytics.dart
@@ -14,6 +14,7 @@
 import 'package:device_info/device_info.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_parent/network/api/heap_api.dart';
 import 'package:flutter_parent/utils/debug_flags.dart';
 
 /// Event names
@@ -79,10 +80,11 @@ class AnalyticsParamConstants {
 
 class Analytics {
 
+  HeapApi get _heap => HeapApi();
   /// Set the current screen in analytics
   void setCurrentScreen(String screenName) async {
     if (kReleaseMode) {
-
+      await _heap.track(screenName);
     }
 
     if (DebugFlags.isDebug) {
@@ -98,7 +100,7 @@ class Analytics {
   /// * [extras] a map of keys [AnalyticsParamConstants] to values. Use sparingly, we only get 25 unique parameters
   void logEvent(String event, {Map<String, dynamic> extras = const {}}) async {
     if (kReleaseMode) {
-
+      await _heap.track(event, extras: extras);
     }
 
     if (DebugFlags.isDebug) {
