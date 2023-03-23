e17e11adb1ce0e0734155d72888bf4df5ac4fb03
Tamas Kozmer <tamas.kozmer@instructure.com>
2023-03-23 12:58:51 +0100

    Upgraded testing dependencies

--- buildSrc/src/main/java/GlobalDependencies.kt
+++ buildSrc/src/main/java/GlobalDependencies.kt
@@ -93,11 +93,11 @@ object Libs {
     /* Testing */
     const val JUNIT = "junit:junit:${Versions.JUNIT}"
     const val ROBOLECTRIC = "org.robolectric:robolectric:${Versions.ROBOLECTRIC}"
-    const val ANDROIDX_TEST_JUNIT = "androidx.test.ext:junit:1.1.3"
-    const val MOCKK = "io.mockk:mockk:1.12.3"
-    const val THREETEN_BP = "org.threeten:threetenbp:1.3.8"
+    const val ANDROIDX_TEST_JUNIT = "androidx.test.ext:junit:1.1.5"
+    const val MOCKK = "io.mockk:mockk:1.13.4"
+    const val THREETEN_BP = "org.threeten:threetenbp:1.6.5"
     const val UI_AUTOMATOR = "com.android.support.test.uiautomator:uiautomator-v18:2.1.3"
-    const val TEST_ORCHESTRATOR = "androidx.test:orchestrator:1.3.0" // Newer version needs AGP 4.2+
+    const val TEST_ORCHESTRATOR = "androidx.test:orchestrator:1.4.2" // Newer version needs AGP 4.2+
 
     /* Qr Code (zxing) */
     const val JOURNEY_ZXING = "com.journeyapps:zxing-android-embedded:4.3.0"
