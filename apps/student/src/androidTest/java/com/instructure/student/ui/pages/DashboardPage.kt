8d5c497fd35df04152823fa6dc508fc720f576fb
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2023-05-09 09:48:05 +0200

    Fix breaking DashboardE2E test on the offline branch in Student application. (#1969)

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/DashboardPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/DashboardPage.kt
@@ -27,8 +27,8 @@ import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
 import com.instructure.canvas.espresso.scrollRecyclerView
 import com.instructure.canvas.espresso.withCustomConstraints
 import com.instructure.canvasapi2.models.AccountNotification
@@ -241,7 +241,9 @@ class DashboardPage : BasePage(R.id.dashboardPage) {
     }
 
     fun switchCourseView() {
-        onView(ViewMatchers.withId(R.id.menu_dashboard_cards)).click()
+        Espresso.openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().targetContext)
+        onView(withText(containsString("Switch to")))
+            .perform(click());
     }
 
     fun clickEditDashboard() {
