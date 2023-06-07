16d0e0bb80f35036691a003592476767abdfa6ed
Akos Hermann <72087159+hermannakos@users.noreply.github.com>
2023-06-07 19:09:43 +0200

    [MBL-16776][Student] Offline Page list (#2015)

--- apps/student/src/main/java/com/instructure/student/router/RouteResolver.kt
+++ apps/student/src/main/java/com/instructure/student/router/RouteResolver.kt
@@ -17,6 +17,7 @@ import com.instructure.student.features.assignmentdetails.AssignmentDetailsFragm
 import com.instructure.student.features.assignmentlist.AssignmentListFragment
 import com.instructure.student.features.elementary.course.ElementaryCourseFragment
 import com.instructure.student.features.files.search.FileSearchFragment
+import com.instructure.student.features.pages.list.PageListFragment
 import com.instructure.student.fragment.*
 import com.instructure.student.mobius.assignmentDetails.submission.annnotation.AnnotationSubmissionUploadFragment
 import com.instructure.student.mobius.assignmentDetails.submission.file.ui.UploadStatusSubmissionFragment
