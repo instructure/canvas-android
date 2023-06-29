dfe6957b0507d5d866ecdf4e4461a5906893bdb3
kdeakinstructure <kristof.deak@instructure.com>
2023-06-29 11:55:33 +0200

    Stub breaking notification test only from Landscape mode.

--- apps/teacher/src/androidTest/java/com/instructure/teacher/ui/InAppUpdatePageTest.kt
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/InAppUpdatePageTest.kt
@@ -24,6 +24,7 @@ import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
 import com.google.android.play.core.appupdate.testing.FakeAppUpdateManager
 import com.instructure.canvas.espresso.Stub
+import com.instructure.canvas.espresso.StubLandscape
 import com.instructure.canvas.espresso.mockCanvas.MockCanvas
 import com.instructure.canvas.espresso.mockCanvas.init
 import com.instructure.canvasapi2.utils.toApiString
@@ -238,7 +239,7 @@ class InAppUpdatePageTest : TeacherTest() {
     }
 
     @Test
-    @Stub
+    @StubLandscape("Stubbed in Landscape because on API lvl 29 device the notification will remain opened even though we push the back button at the end. Should be investigated and make some workaround once.")
     fun showNotificationOnFlexibleDownloadFinish() {
         updatePrefs.clearPrefs()
         val expectedTitle = context.getString(R.string.appUpdateReadyTitle)
