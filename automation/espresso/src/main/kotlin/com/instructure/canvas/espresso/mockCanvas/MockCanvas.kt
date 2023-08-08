551cfd63ee59cd86ffc87c69f5c2170eae1b84e8
Kristof Nemere <109959688+kristofnemere@users.noreply.github.com>
2023-08-08 15:11:11 +0200

    [MBL-16921][Student] Modules LGO
    refs: MBL-16921
    affects: Student
    release note: none

--- automation/espresso/src/main/kotlin/com/instructure/canvas/espresso/mockCanvas/MockCanvas.kt
+++ automation/espresso/src/main/kotlin/com/instructure/canvas/espresso/mockCanvas/MockCanvas.kt
@@ -1482,7 +1482,8 @@ fun MockCanvas.addItemToModule(
         course: Course,
         moduleId: Long,
         item: Any,
-        published: Boolean = true
+        published: Boolean = true,
+        moduleContentDetails: ModuleContentDetails? = null
 ) : ModuleItem {
 
     // Placeholders for itemType and itemTitle values that we will compute below
@@ -1544,7 +1545,8 @@ fun MockCanvas.addItemToModule(
             // I don't really know if these two should be the same, but I needed
             // htmlUrl populated in order to get external url module items to work.
             url = itemUrl,
-            htmlUrl = itemUrl
+            htmlUrl = itemUrl,
+            moduleDetails = moduleContentDetails
     )
 
     // Copy/update/replace the module
