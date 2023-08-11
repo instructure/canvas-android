332a03953dd4ef3d38a4c33b98c548ba3aba6130
Tamas Kozmer <72397075+tamaskozmer@users.noreply.github.com>
2023-08-11 08:00:32 +0200

    [MBL-16931][Student] K5 Grades LGO (#2096)
    refs: MBL-16931
    affects: Student
    release note: none
    
    * Elementary grades LGO.
    
    * Added computedCurrentLetterGrade changes for the Parent app.
    
    * Unit tests.
    
    * Integration tests.

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/apis/CourseAPI.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/apis/CourseAPI.kt
@@ -121,7 +121,7 @@ object CourseAPI {
         @GET("courses/{courseId}/rubrics/{rubricId}")
         fun getRubricSettings(@Path("courseId") courseId: Long, @Path("rubricId") rubricId: Long): Call<RubricSettings>
 
-        @GET("courses?include[]=total_scores&include[]=current_grading_period_scores&include[]=grading_periods&include[]=course_image&enrollment_state=active")
+        @GET("courses?include[]=total_scores&include[]=current_grading_period_scores&include[]=grading_periods&include[]=course_image&include[]=settings&enrollment_state=active")
         fun getFirstPageCoursesWithGrades(): Call<List<Course>>
     }
 
