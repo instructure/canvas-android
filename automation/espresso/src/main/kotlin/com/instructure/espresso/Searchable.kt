4ff203dd9699e602c672ed7ac6b1e2195be15701
kdeakinstructure <kristof.deak@instructure.com>
2023-08-15 13:31:23 +0200

    use let in Searchable.

--- automation/espresso/src/main/kotlin/com/instructure/espresso/Searchable.kt
+++ automation/espresso/src/main/kotlin/com/instructure/espresso/Searchable.kt
@@ -8,18 +8,18 @@ import com.instructure.espresso.matchers.WaitForViewMatcher.waitForView
 class Searchable(private val searchButtonId: Int? = null, private val queryInputId: Int? = null, private val clearButtonId: Int? = null, private val backButtonId: Int? = null) {
 
     fun clickOnSearchButton() {
-        onView(withId(searchButtonId!!)).click()
+        onView(searchButtonId?.let { withId(it) }).click()
     }
 
     fun typeToSearchBar(textToType: String) {
-        onView(withId(queryInputId!!)).perform(ViewActions.replaceText(textToType))
+        onView(queryInputId?.let { withId(it) }).perform(ViewActions.replaceText(textToType))
     }
 
     fun clickOnClearSearchButton() {
-        waitForView(withId(clearButtonId!!)).click()
+        waitForView(clearButtonId?.let { withId(it) }).click()
     }
 
     fun pressSearchBackButton() {
-        onView(withId(backButtonId!!)).click()
+        onView(backButtonId?.let { withId(it) }).click()
     }
 }
