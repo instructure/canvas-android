e56325727a37812f998b959c059c0def2df8fc62
Ákos Hermann <akos.hermann@instructure.com>
2023-08-24 10:09:14 +0200

    Update NotATeacherPageTest.kt

--- apps/teacher/src/androidTest/java/com/instructure/teacher/ui/NotATeacherPageTest.kt
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/NotATeacherPageTest.kt
@@ -15,6 +15,7 @@
  */
 package com.instructure.teacher.ui
 
+import com.instructure.canvas.espresso.Stub
 import com.instructure.teacher.ui.utils.TeacherTest
 import com.instructure.teacher.ui.utils.slowLogInAsStudent
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -25,6 +26,7 @@ class NotATeacherPageTest : TeacherTest() {
 
     // Runs live; no MockCanvas
     @Test
+    @Stub
     override fun displaysPageObjects() {
         slowLogInAsStudent()
         notATeacherPage.assertPageObjects()
