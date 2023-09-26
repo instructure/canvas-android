16e727957a561724bc9d606d4419c4d6d9d3918a
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2023-09-26 10:45:59 +0200

    [MBL-17071][Student] - Investigate and fix offline branch tests - E2E workflow (#2162)

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/CourseGradesPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/CourseGradesPage.kt
@@ -25,7 +25,6 @@ import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.hasSibling
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
 import androidx.test.espresso.matcher.ViewMatchers.withChild
 import com.instructure.canvas.espresso.containsTextCaseInsensitive
@@ -36,7 +35,6 @@ import com.instructure.espresso.assertDisplayed
 import com.instructure.espresso.assertHasText
 import com.instructure.espresso.assertNotDisplayed
 import com.instructure.espresso.click
-import com.instructure.espresso.matchers.WaitForViewMatcher.waitForView
 import com.instructure.espresso.page.BasePage
 import com.instructure.espresso.page.onView
 import com.instructure.espresso.page.plus
@@ -46,7 +44,6 @@ import com.instructure.espresso.page.withId
 import com.instructure.espresso.page.withParent
 import com.instructure.espresso.page.withText
 import com.instructure.espresso.scrollTo
-import com.instructure.espresso.swipeDown
 import com.instructure.espresso.typeText
 import com.instructure.student.R
 import org.hamcrest.Matcher
@@ -94,8 +91,6 @@ class CourseGradesPage : BasePage(R.id.courseGradesPage) {
     fun refresh() {
         onView(withId(R.id.swipeRefreshLayout) + withAncestor(R.id.courseGradesPage))
             .perform(withCustomConstraints(swipeDown(), isDisplayingAtLeast(5)))
-        onView(allOf(withId(R.id.swipeRefreshLayout), isDisplayed()))
-                .perform(withCustomConstraints(swipeDown(), isDisplayingAtLeast(5)))
         sleep(1000) // Allow some time to react to the update.
     }
 
