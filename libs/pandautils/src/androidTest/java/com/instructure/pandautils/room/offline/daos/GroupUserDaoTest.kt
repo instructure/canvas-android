b8d245332942dc5b2031480f72de78983e88728c
domonkosadam <domonkosadam01@gmail.com>
2023-10-09 14:44:20 +0200

    Fix dao tests

--- libs/pandautils/src/androidTest/java/com/instructure/pandautils/room/offline/daos/GroupUserDaoTest.kt
+++ libs/pandautils/src/androidTest/java/com/instructure/pandautils/room/offline/daos/GroupUserDaoTest.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.instructure.canvasapi2.models.Group
+import com.instructure.canvasapi2.models.User
 import com.instructure.pandautils.room.offline.OfflineDatabase
+import com.instructure.pandautils.room.offline.entities.GroupEntity
 import com.instructure.pandautils.room.offline.entities.GroupUserEntity
+import com.instructure.pandautils.room.offline.entities.UserEntity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -20,12 +24,16 @@ class GroupUserDaoTest {
     private lateinit var db: OfflineDatabase
 
     private lateinit var groupUserDao: GroupUserDao
+    private lateinit var groupDao: GroupDao
+    private lateinit var userDao: UserDao
 
     @Before
     fun setUp() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(context, OfflineDatabase::class.java).build()
         groupUserDao = db.groupUserDao()
+        groupDao = db.groupDao()
+        userDao = db.userDao()
     }
 
     @After
@@ -35,10 +43,14 @@ class GroupUserDaoTest {
 
     @Test
     fun testInsertReplace() = runTest {
-        val groupUserEntity = GroupUserEntity(1L, 1L)
-        groupUserDao.insert(groupUserEntity)
+        groupDao.insert(GroupEntity(Group(1L)))
+        groupDao.insert(GroupEntity(Group(2L)))
+        userDao.insert(UserEntity(User(1L)))
 
-        val updated = groupUserEntity.copy(groupId = 2L)
+        val groupUserEntity = GroupUserEntity(1L, 1L)
+        val id = groupUserDao.insert(groupUserEntity)
+
+        val updated = groupUserEntity.copy(id = id, groupId = 2L)
         groupUserDao.insert(updated)
 
         val result = groupUserDao.findByUserId(1L)
@@ -48,13 +60,18 @@ class GroupUserDaoTest {
 
     @Test
     fun testInsertAllReplace() = runTest {
+        groupDao.insert(GroupEntity(Group(1L)))
+        groupDao.insert(GroupEntity(Group(2L)))
+        userDao.insert(UserEntity(User(1L)))
+        userDao.insert(UserEntity(User(2L)))
+
         val discussionEntryEntities = listOf(
             GroupUserEntity(1L, 1L),
             GroupUserEntity(1L, 2L)
         )
-        groupUserDao.insertAll(discussionEntryEntities)
+        val ids = groupUserDao.insertAll(discussionEntryEntities)
 
-        val updated = discussionEntryEntities.map { it.copy(groupId = 2L) }
+        val updated = discussionEntryEntities.mapIndexed { index, entity ->  entity.copy(id = ids[index], groupId = 2L) }
         groupUserDao.insertAll(updated)
 
         val result0 = groupUserDao.findByUserId(1L)
@@ -64,13 +81,19 @@ class GroupUserDaoTest {
         Assert.assertEquals(updated[1].groupId, result1?.get(0))
     }
 
+    @Test
     fun testFindByUserId() = runTest {
+        groupDao.insert(GroupEntity(Group(1L)))
+        groupDao.insert(GroupEntity(Group(2L)))
+        userDao.insert(UserEntity(User(1L)))
+        userDao.insert(UserEntity(User(2L)))
+
         val discussionEntryEntities = listOf(
             GroupUserEntity(1L, 1L),
             GroupUserEntity(2L, 2L)
         )
         groupUserDao.insertAll(discussionEntryEntities)
-
+        
         val result = groupUserDao.findByUserId(2L)
 
         Assert.assertEquals(discussionEntryEntities[1].groupId, result?.get(0))
