d3bd9472eaf07260572ec0872a766b3b98d4fb6a
Ákos Hermann <akos.hermann@instructure.com>
2023-10-26 13:35:33 +0200

    unused imports

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/offline/sync/progress/SyncProgressViewModel.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/offline/sync/progress/SyncProgressViewModel.kt
@@ -28,10 +28,7 @@ import androidx.work.WorkManager
 import com.instructure.pandautils.R
 import com.instructure.pandautils.features.offline.sync.AggregateProgressObserver
 import com.instructure.pandautils.features.offline.sync.AggregateProgressViewData
-import com.instructure.pandautils.features.offline.sync.CourseSyncWorker
-import com.instructure.pandautils.features.offline.sync.FileSyncWorker
 import com.instructure.pandautils.features.offline.sync.OfflineSyncHelper
-import com.instructure.pandautils.features.offline.sync.OfflineSyncWorker
 import com.instructure.pandautils.features.offline.sync.ProgressState
 import com.instructure.pandautils.features.offline.sync.progress.itemviewmodels.AdditionalFilesProgressItemViewModel
 import com.instructure.pandautils.features.offline.sync.progress.itemviewmodels.CourseProgressItemViewModel
