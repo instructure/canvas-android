55a2af5941d6ca2c6adbaf10e28caf54997ee5d5
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2023-11-02 12:03:12 +0100

    [MBL-17117][Student] - Manage Offline Content Page E2E test implementation (#2223)

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/offline/ManageOfflineContentPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/offline/ManageOfflineContentPage.kt
@@ -17,8 +17,18 @@
 
 package com.instructure.student.ui.pages.offline
 
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.hasSibling
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import com.instructure.canvas.espresso.containsTextCaseInsensitive
+import com.instructure.canvas.espresso.hasCheckedState
+import com.instructure.espresso.ConstraintLayoutItemCountAssertion
+import com.instructure.espresso.ConstraintLayoutItemCountAssertionWithMatcher
+import com.instructure.espresso.DoesNotExistAssertion
 import com.instructure.espresso.OnViewWithId
+import com.instructure.espresso.WaitForViewWithId
+import com.instructure.espresso.actions.ForceClick
+import com.instructure.espresso.assertDisplayed
 import com.instructure.espresso.click
 import com.instructure.espresso.page.BasePage
 import com.instructure.espresso.page.onView
@@ -26,21 +36,35 @@ import com.instructure.espresso.page.plus
 import com.instructure.espresso.page.waitForView
 import com.instructure.espresso.page.withAncestor
 import com.instructure.espresso.page.withId
+import com.instructure.espresso.page.withParent
 import com.instructure.espresso.page.withText
+import com.instructure.espresso.scrollTo
 import com.instructure.pandautils.R
+import org.hamcrest.CoreMatchers.allOf
 
 class ManageOfflineContentPage : BasePage(R.id.manageOfflineContentPage) {
 
-    private val toolbar by OnViewWithId(R.id.toolbar)
+    private val syncButton by OnViewWithId(R.id.syncButton)
+    private val storageInfoContainer by WaitForViewWithId(R.id.storageInfoContainer)
 
     //OfflineMethod
-    fun selectEntireCourseForSync(courseName: String) {
-        onView(withId(R.id.checkbox) + hasSibling(withId(R.id.title) + withText(courseName))).click()
+    fun changeItemSelectionState(courseName: String) {
+        onView(withId(R.id.checkbox) + hasSibling(withId(R.id.title) + withText(courseName))).scrollTo().click()
+    }
+
+    //OfflineMethod
+    fun expandCollapseItem(itemName: String) {
+        onView(withId(R.id.arrow) + withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE) + hasSibling(withId(R.id.title) + withText(itemName))).scrollTo().perform(ForceClick())
+    }
+
+    //OfflineMethod
+    fun expandCollapseFiles() {
+        expandCollapseItem("Files")
     }
 
     //OfflineMethod
     fun clickOnSyncButton() {
-        onView(withId(R.id.syncButton)).click()
+        syncButton.click()
         confirmSync()
     }
 
@@ -48,4 +72,60 @@ class ManageOfflineContentPage : BasePage(R.id.manageOfflineContentPage) {
     private fun confirmSync() {
         waitForView(withText("Sync") + withAncestor(R.id.buttonPanel)).click()
     }
+
+    //OfflineMethod
+    fun confirmDiscardChanges() {
+        waitForView(withText("Discard") + withAncestor(R.id.buttonPanel)).click()
+    }
+
+    //OfflineMethod
+    fun assertStorageInfoDetails() {
+        onView(withId(R.id.storageLabel) + withText(R.string.offline_content_storage)).assertDisplayed()
+        onView(withId(R.id.storageInfo) + containsTextCaseInsensitive("Used")).assertDisplayed()
+        onView(withId(R.id.progress) + withParent(withId(R.id.storageInfoContainer))).assertDisplayed()
+        onView(withId(R.id.otherLabel) + withText(R.string.offline_content_other)).assertDisplayed()
+        onView(withId(R.id.canvasLabel) + withText(R.string.offline_content_canvas_student)).assertDisplayed()
+        onView(withId(R.id.remainingLabel) + withText(R.string.offline_content_remaining)).assertDisplayed()
+    }
+
+    //OfflineMethod
+    fun assertSelectButtonText(selectAll: Boolean) {
+        if(selectAll) waitForView(withId(R.id.menu_select_all) + withText(R.string.offline_content_select_all)).assertDisplayed()
+        else waitForView(withId(R.id.menu_select_all) + withText(R.string.offline_content_deselect_all)).assertDisplayed()
+    }
+
+    //OfflineMethod
+    fun clickOnSelectAllButton() {
+        waitForView(withId(R.id.menu_select_all) + withText(R.string.offline_content_select_all)).click()
+    }
+
+    //OfflineMethod
+    fun clickOnDeselectAllButton() {
+        waitForView(withId(R.id.menu_select_all) + withText(R.string.offline_content_deselect_all)).click()
+    }
+
+    //OfflineMethod
+    fun assertCourseCountWithMatcher(expectedCount: Int) {
+        ConstraintLayoutItemCountAssertionWithMatcher((allOf(withId(R.id.arrow), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))), expectedCount)
+    }
+
+    //OfflineMethod
+    fun assertCourseCount(expectedCount: Int) {
+        onView((allOf(withId(R.id.arrow), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))).check(ConstraintLayoutItemCountAssertion(expectedCount))
+    }
+
+    //OfflineMethod
+    fun assertToolbarTexts(courseName: String) {
+        onView(withText(courseName) + withParent(R.id.toolbar) + withAncestor(R.id.manageOfflineContentPage)).assertDisplayed()
+        onView(withText(R.string.offline_content_toolbar_title) + withParent(R.id.toolbar) + withAncestor(R.id.manageOfflineContentPage)).assertDisplayed()
+    }
+
+    fun assertCheckedStateOfItem(itemName: String, state: Int) {
+        onView(withId(R.id.checkbox) + hasSibling(withId(R.id.title) + withText(itemName)) + hasCheckedState(state)).scrollTo().assertDisplayed()
+    }
+
+    fun waitForItemDisappear(itemName: String) {
+        onView(withId(R.id.checkbox) + hasSibling(withId(R.id.title) + withText(itemName))).check(DoesNotExistAssertion(5))
+    }
+
 }
