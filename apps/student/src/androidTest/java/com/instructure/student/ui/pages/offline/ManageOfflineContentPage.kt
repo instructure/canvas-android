ec9e9d98774ded93639b1ca3c87dbff6d057b81d
kristof.nemere <kristof.nemere@instructure.com>
2023-11-02 16:01:41 +0100

    Test fix

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/offline/ManageOfflineContentPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/offline/ManageOfflineContentPage.kt
@@ -41,7 +41,7 @@ class ManageOfflineContentPage : BasePage(R.id.manageOfflineContentPage) {
 
     //OfflineMethod
     fun expandCollapseItem(itemName: String) {
-        waitForView(withId(R.id.arrow) + withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE) + hasSibling(withId(R.id.title) + withText(itemName))).scrollTo().perform(ForceClick())
+        onView(withId(R.id.arrow) + withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE) + hasSibling(withId(R.id.title) + withText(itemName))).scrollTo().perform(ForceClick())
     }
 
     //OfflineMethod
@@ -143,7 +143,7 @@ class ManageOfflineContentPage : BasePage(R.id.manageOfflineContentPage) {
 
     //OfflineMethod
     fun assertItemDisplayed(title: String) {
-        onView(withId(R.id.title) + withText(title)).assertDisplayed()
+        waitForView(withId(R.id.title) + withText(title)).assertDisplayed()
     }
 
     //OfflineMethod
