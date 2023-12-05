19923f2b17c649c1c63a22416d3132d908e883fd
Ákos Hermann <akos.hermann@instructure.com>
2023-12-05 15:04:30 +0100

    firebase

--- buildSrc/src/main/java/GlobalDependencies.kt
+++ buildSrc/src/main/java/GlobalDependencies.kt
@@ -75,7 +75,7 @@ object Libs {
     const val ANDROIDX_DATABINDING_COMPILER = "androidx.databinding:databinding-compiler:${Versions.ANDROID_GRADLE_TOOLS}" // This is bundled with the gradle plugin so we use the same version
 
     /* Firebase */
-    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:31.2.3"
+    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:32.6.0"
     const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging"
     const val FIREBASE_CONFIG = "com.google.firebase:firebase-config"
