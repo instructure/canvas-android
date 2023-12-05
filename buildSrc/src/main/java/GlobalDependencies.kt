ae0f109d9574fb91fb7110a1d189f5dfc7972ebd
Ákos Hermann <akos.hermann@instructure.com>
2023-12-05 10:18:42 +0100

    fragment

--- buildSrc/src/main/java/GlobalDependencies.kt
+++ buildSrc/src/main/java/GlobalDependencies.kt
@@ -31,7 +31,7 @@ object Versions {
     const val HILT = "2.49"
     const val HILT_ANDROIDX = "1.1.0"
     const val LIFECYCLE = "2.6.2"
-    const val FRAGMENT = "1.5.5"
+    const val FRAGMENT = "1.6.2"
     const val WORK_MANAGER = "2.8.1"
     const val GLIDE_VERSION = "4.15.1"
     const val RETROFIT = "2.9.0"
