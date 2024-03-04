4ab65af64a7bada8c2152113a8587827f01ac242
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2024-03-04 13:30:51 +0100

    [MBL-17419][Student] - Fix breaking Offline E2E tests (#2366)
    * Fix breaking Offline E2E tests.
    
    refs: MBL-17419
    affects: Student
    release note: none
    
    * Fix more offline e2e tests.
    
    * Refactor Offline Sync Progress E2E test.
    Fix AllCourses E2E test
    
    * Extend sync content to let the waitForView have enough time to find the dashboard notification.
    
    * Increase testShards to 10 in Offline E2E workflow.
    
    * Add one more course to sync to extend dashboard notification appearance on Dashboard.
    
    * Change waitForView to sleep and onView (check(matches(..)) evaluation lasts too long on CI so the view disappear until the evaluation finished).

--- apps/student/src/androidTest/java/com/instructure/student/ui/e2e/offline/OfflineCourseBrowserE2ETest.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/e2e/offline/OfflineCourseBrowserE2ETest.kt
@@ -32,7 +32,6 @@ import com.instructure.student.ui.utils.tokenLogin
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.After
 import org.junit.Test
-import java.lang.Thread.sleep
 
 @HiltAndroidTest
 class OfflineCourseBrowserE2ETest : StudentTest() {
@@ -49,23 +48,23 @@ class OfflineCourseBrowserE2ETest : StudentTest() {
         Log.d(PREPARATION_TAG,"Seeding data.")
         val data = seedData(students = 1, teachers = 1, courses = 1, announcements = 1)
         val student = data.studentsList[0]
-        val course1 = data.coursesList[0]
+        val course = data.coursesList[0]
 
-        Log.d(STEP_TAG,"Login with user: ${student.name}, login id: ${student.loginId}.")
+        Log.d(STEP_TAG,"Login with user: '${student.name}', login id: '${student.loginId}'.")
         tokenLogin(student)
         dashboardPage.waitForRender()
 
         Log.d(STEP_TAG, "Open global 'Manage Offline Content' page via the more menu of the Dashboard Page.")
         dashboardPage.openGlobalManageOfflineContentPage()
 
-        Log.d(STEP_TAG, "Select the entire '${course1.name}' course for sync. Click on the 'Sync' button.")
-        Log.d(STEP_TAG, "Expand '${course1.name}' course. Select only the 'Announcements' of the '${course1.name}' course. Click on the 'Sync' button and confirm the sync process.")
-        manageOfflineContentPage.expandCollapseItem(course1.name)
+        Log.d(STEP_TAG, "Expand '${course.name}' course. Select only the 'Announcements' of the '${course.name}' course. Click on the 'Sync' button and confirm the sync process.")
+        manageOfflineContentPage.expandCollapseItem(course.name)
         manageOfflineContentPage.changeItemSelectionState("Announcements")
         manageOfflineContentPage.clickOnSyncButtonAndConfirm()
 
-        Log.d(STEP_TAG, "Wait for the 'Download Started' and 'Syncing Offline Content' dashboard notifications to be displayed, and then to disappear.")
-        dashboardPage.waitForOfflineSyncDashboardNotifications()
+        Log.d(STEP_TAG, "Assert that the offline sync icon only displayed on the synced course's course card.")
+        dashboardPage.assertCourseOfflineSyncIconVisible(course.name)
+        device.waitForIdle()
 
         Log.d(PREPARATION_TAG, "Turn off the Wi-Fi and Mobile Data on the device, so it will go offline.")
         turnOffConnectionViaADB()
@@ -74,8 +73,8 @@ class OfflineCourseBrowserE2ETest : StudentTest() {
         Log.d(STEP_TAG, "Wait for the Dashboard Page to be rendered. Refresh the page.")
         dashboardPage.waitForRender()
 
-        Log.d(STEP_TAG, "Select '${course1.name}' course and open 'Announcements' menu.")
-        dashboardPage.selectCourse(course1)
+        Log.d(STEP_TAG, "Select '${course.name}' course and open 'Announcements' menu.")
+        dashboardPage.selectCourse(course)
 
         Log.d(STEP_TAG, "Assert that only the 'Announcements' tab is enabled because it is the only one which has been synced, and assert that all the other, previously synced tabs are disabled, because they weren't synced now.")
         var enabledTabs = arrayOf("Announcements")
@@ -91,27 +90,22 @@ class OfflineCourseBrowserE2ETest : StudentTest() {
         Log.d(STEP_TAG, "Open global 'Manage Offline Content' page via the more menu of the Dashboard Page.")
         dashboardPage.openGlobalManageOfflineContentPage()
 
-        Log.d(STEP_TAG, "Deselect the entire '${course1.name}' course for sync.")
-        manageOfflineContentPage.changeItemSelectionState(course1.name)
+        Log.d(STEP_TAG, "Deselect the entire '${course.name}' course for sync.")
+        manageOfflineContentPage.changeItemSelectionState(course.name)
         manageOfflineContentPage.clickOnSyncButtonAndConfirm()
 
-        Log.d(STEP_TAG, "Wait for the 'Download Started' dashboard notification to be displayed, and the to disappear.")
-        dashboardPage.waitForRender()
-        dashboardPage.waitForSyncProgressDownloadStartedNotification()
-        dashboardPage.waitForSyncProgressDownloadStartedNotificationToDisappear()
-
-        Log.d(STEP_TAG, "Wait for the 'Syncing Offline Content' dashboard notification to be displayed, and the to disappear. (It should be displayed after the 'Download Started' notification immediately.)")
-        dashboardPage.waitForSyncProgressStartingNotification()
-        dashboardPage.waitForSyncProgressStartingNotificationToDisappear()
+        Log.d(STEP_TAG, "Assert that the offline sync icon only displayed on the synced course's course card.")
+        dashboardPage.assertCourseOfflineSyncIconVisible(course.name)
+        device.waitForIdle()
 
         Log.d(PREPARATION_TAG, "Turn off the Wi-Fi and Mobile Data on the device, so it will go offline.")
         turnOffConnectionViaADB()
 
-        Log.d(STEP_TAG, "Select '${course1.name}' course and open 'Announcements' menu.")
-        sleep(10000) //Need to wait a bit here because of a UI glitch that when network state change, the dashboard page 'pops' a bit and it can confuse the automation script.
-        device.waitForIdle()
-        device.waitForWindowUpdate(null, 10000)
-        dashboardPage.selectCourse(course1)
+        Log.d(STEP_TAG, "Select '${course.name}' course and open 'Announcements' menu.")
+        OfflineTestUtils.waitForNetworkToGoOffline(device)
+
+        Log.d(STEP_TAG, "Select '${course.name}' course.")
+        dashboardPage.selectCourse(course)
 
         Log.d(STEP_TAG, "Assert that the 'Google Drive' and 'Collaborations' tabs are disabled because they aren't supported in offline mode, but the rest of the tabs are enabled because the whole course has been synced.")
         enabledTabs = arrayOf("Announcements", "Discussions", "Grades", "People", "Syllabus", "BigBlueButton")
