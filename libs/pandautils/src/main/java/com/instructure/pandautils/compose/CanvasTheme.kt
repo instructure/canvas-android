7176a9c846d5ddd17393fb75dfb230bdefe882cd
Tamas Kozmer <72397075+tamaskozmer@users.noreply.github.com>
2024-03-06 13:47:20 +0100

    [MBL-17390][Student][Teacher] Fix Compose fonts #2371
    refs: MBL-17930
    affects: Student, Teacher
    release note: none

--- libs/pandautils/src/main/java/com/instructure/pandautils/compose/CanvasTheme.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/compose/CanvasTheme.kt
@@ -18,8 +18,12 @@
 
 package com.instructure.pandautils.compose
 
+import android.content.Context
 import androidx.annotation.FontRes
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
 import androidx.compose.material.ripple.LocalRippleTheme
@@ -28,18 +32,29 @@ import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import com.instructure.pandautils.R
 
 @Composable
 fun CanvasTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        typography = typography
+        typography = typography.copy(
+            button = typography.button.copy(letterSpacing = TextUnit(0.5f, TextUnitType.Sp))
+        )
     ) {
         CompositionLocalProvider(
             LocalRippleTheme provides CanvasRippleTheme,
+            LocalTextSelectionColors provides getCustomTextSelectionColors(context = LocalContext.current),
+            LocalTextStyle provides TextStyle(
+                fontFamily = lato,
+                letterSpacing = TextUnit(0f, TextUnitType.Sp)
+            ),
             content = content
         )
     }
@@ -71,5 +86,13 @@ private object CanvasRippleTheme : RippleTheme {
     override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(
         Color.Black,
         lightTheme = !isSystemInDarkTheme()
+    )
+}
+
+private fun getCustomTextSelectionColors(context: Context): TextSelectionColors {
+    val color = Color(context.getColor(R.color.textDarkest))
+    return TextSelectionColors(
+        handleColor = color,
+        backgroundColor = color.copy(alpha = 0.4f)
     )
 }
