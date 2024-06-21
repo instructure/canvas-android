909acab97f879b6fa60a3f53e1e9e3869e707e6d
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2024-06-21 13:09:08 +0200

    [MBL-17267][Student][Teacher] - Implement Calendar Screen E2E tests #2478

--- apps/teacher/src/androidTest/java/com/instructure/teacher/ui/utils/TeacherComposeTest.kt
+++ apps/teacher/src/androidTest/java/com/instructure/teacher/ui/utils/TeacherComposeTest.kt
@@ -21,6 +21,7 @@ package com.instructure.teacher.ui.utils
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import com.instructure.canvas.espresso.common.pages.compose.CalendarEventCreateEditPage
 import com.instructure.canvas.espresso.common.pages.compose.CalendarEventDetailsPage
+import com.instructure.canvas.espresso.common.pages.compose.CalendarFilterPage
 import com.instructure.canvas.espresso.common.pages.compose.CalendarScreenPage
 import com.instructure.canvas.espresso.common.pages.compose.CalendarToDoCreateUpdatePage
 import com.instructure.canvas.espresso.common.pages.compose.CalendarToDoDetailsPage
@@ -39,5 +40,6 @@ abstract class TeacherComposeTest : TeacherTest() {
     val calendarEventDetailsPage = CalendarEventDetailsPage(composeTestRule)
     val calendarToDoCreateUpdatePage = CalendarToDoCreateUpdatePage(composeTestRule)
     val calendarToDoDetailsPage = CalendarToDoDetailsPage(composeTestRule)
+    val calendarFilterPage = CalendarFilterPage(composeTestRule)
     val progressPage = ProgressPage(composeTestRule)
 }
