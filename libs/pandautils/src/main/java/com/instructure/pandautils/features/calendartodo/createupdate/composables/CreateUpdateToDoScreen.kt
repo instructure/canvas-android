ec2b74a4b67332930dde02ad6aa6d413a3a3d698
Akos Hermann <72087159+hermannakos@users.noreply.github.com>
2024-06-24 13:22:24 +0200

    [MBL-17638][Student][Teacher] Calendar selection A11y (#2482)
    Test plan: See ticket.
    
    refs: MBL-17638
    affects: Student, Teacher
    release note: none

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/calendartodo/createupdate/composables/CreateUpdateToDoScreen.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/calendartodo/createupdate/composables/CreateUpdateToDoScreen.kt
@@ -57,6 +57,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -91,6 +92,9 @@ internal fun CreateUpdateToDoScreenWrapper(
     actionHandler: (CreateUpdateToDoAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    val localView = LocalView.current
+    val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
     CanvasTheme {
@@ -98,6 +102,9 @@ internal fun CreateUpdateToDoScreenWrapper(
             SelectCalendarScreen(
                 uiState = uiState.selectCalendarUiState,
                 onCalendarSelected = {
+                    localView.announceForAccessibility(
+                        context.getString(R.string.a11y_calendarSelected, it.name.orEmpty())
+                    )
                     actionHandler(CreateUpdateToDoAction.UpdateCanvasContext(it))
                     coroutineScope.launch {
                         // We need to add this delay to give the user some feedback about the selection before closing the screen
