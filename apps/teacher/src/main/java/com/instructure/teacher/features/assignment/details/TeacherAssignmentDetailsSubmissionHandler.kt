86b0074eb44f75d2ceafdb21ef5abb0641931ace
domonkosadam <domonkosadam01@gmail.com>
2024-10-02 09:34:57 +0200

    Fix teacher di

--- apps/teacher/src/main/java/com/instructure/teacher/features/assignment/details/TeacherAssignmentDetailsSubmissionHandler.kt
+++ apps/teacher/src/main/java/com/instructure/teacher/features/assignment/details/TeacherAssignmentDetailsSubmissionHandler.kt
@@ -1,28 +1,53 @@
 package com.instructure.teacher.features.assignment.details
 
 import android.content.Context
+import android.content.res.Resources
 import android.net.Uri
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.MutableLiveData
 import com.instructure.canvasapi2.models.Assignment
 import com.instructure.canvasapi2.models.Course
 import com.instructure.canvasapi2.models.LTITool
-import com.instructure.canvasapi2.models.Submission
 import com.instructure.pandautils.features.assignments.details.AssignmentDetailsSubmissionHandler
+import com.instructure.pandautils.features.assignments.details.AssignmentDetailsViewData
+import kotlinx.coroutines.CoroutineScope
 import java.io.File
 
 class TeacherAssignmentDetailsSubmissionHandler: AssignmentDetailsSubmissionHandler {
     override var isUploading: Boolean = false
-    override var lastSubmission: Submission? = null
+    override var lastSubmissionAssignmentId: Long? = null
+    override var lastSubmissionSubmissionType: String? = null
     override var lastSubmissionIsDraft: Boolean = false
     override var lastSubmissionEntry: String? = null
+    override fun addAssignmentSubmissionObserver(
+        assignmentId: Long,
+        userId: Long,
+        resources: Resources,
+        coroutineScope: CoroutineScope,
+        data: MutableLiveData<AssignmentDetailsViewData>,
+        refreshAssignment: () -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
 
-    override fun addAssignmentSubmissionObserver() = Unit
+    override fun removeAssignmentSubmissionObserver() {
+        TODO("Not yet implemented")
+    }
 
-    override fun removeAssignmentSubmissionObserver() = Unit
+    override fun uploadAudioSubmission(
+        context: Context?,
+        course: Course?,
+        assignment: Assignment?,
+        file: File?
+    ) {
+        TODO("Not yet implemented")
+    }
 
-    override fun uploadAudioSubmission(context: Context?, course: Course?, assignment: Assignment?, file: File?) = Unit
+    override fun getVideoUri(fragment: FragmentActivity): Uri? {
+        TODO("Not yet implemented")
+    }
 
-    override fun getVideoUri(fragment: FragmentActivity): Uri? = null
-
-    override suspend fun getStudioLTITool(assignment: Assignment, courseId: Long?): LTITool? = null
+    override suspend fun getStudioLTITool(assignment: Assignment, courseId: Long?): LTITool? {
+        TODO("Not yet implemented")
+    }
 }
