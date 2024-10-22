6bfa7b090aef0b3c4c62beb6b650f73fd35ea506
Tamas Kozmer <tamas.kozmer@instructure.com>
2024-10-22 09:22:11 +0200

    Filter hidden calendar events in Teacher/Parent.

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/calendar/CalendarRepository.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/calendar/CalendarRepository.kt
@@ -16,24 +16,66 @@
 package com.instructure.pandautils.features.calendar
 
 import com.instructure.canvasapi2.models.CanvasContext
+import com.instructure.canvasapi2.models.Plannable
+import com.instructure.canvasapi2.models.PlannableType
 import com.instructure.canvasapi2.models.PlannerItem
 import com.instructure.canvasapi2.utils.DataResult
+import com.instructure.canvasapi2.utils.toDate
 import com.instructure.pandautils.room.calendar.entities.CalendarFilterEntity
 
-interface CalendarRepository {
+abstract class CalendarRepository {
 
-    suspend fun getPlannerItems(
+    protected var canvasContexts: List<CanvasContext> = emptyList()
+
+    abstract suspend fun getPlannerItems(
         startDate: String,
         endDate: String,
         contextCodes: List<String>,
         forceNetwork: Boolean
     ): List<PlannerItem>
 
-    suspend fun getCanvasContexts(): DataResult<Map<CanvasContext.Type, List<CanvasContext>>>
+    abstract suspend fun getCanvasContexts(): DataResult<Map<CanvasContext.Type, List<CanvasContext>>>
 
-    suspend fun getCalendarFilterLimit(): Int
+    abstract suspend fun getCalendarFilterLimit(): Int
 
-    suspend fun getCalendarFilters(): CalendarFilterEntity?
+    abstract suspend fun getCalendarFilters(): CalendarFilterEntity?
 
-    suspend fun updateCalendarFilters(calendarFilterEntity: CalendarFilterEntity)
+    abstract suspend fun updateCalendarFilters(calendarFilterEntity: CalendarFilterEntity)
+
+    protected fun List<Plannable>.toPlannerItems(): List<PlannerItem> {
+        return mapNotNull { plannable ->
+            val contextType = if (plannable.courseId != null) CanvasContext.Type.COURSE.apiString else CanvasContext.Type.USER.apiString
+            val contextName = if (plannable.courseId != null) canvasContexts.find { it.id == plannable.courseId }?.name else null
+            val plannableDate = plannable.todoDate.toDate()
+            if (plannableDate == null) {
+                null
+            } else {
+                PlannerItem(
+                    courseId = plannable.courseId,
+                    groupId = plannable.groupId,
+                    userId = plannable.userId,
+                    contextType = contextType,
+                    contextName = contextName,
+                    plannableType = PlannableType.PLANNER_NOTE,
+                    plannable = plannable,
+                    plannableDate = plannableDate,
+                    htmlUrl = null,
+                    submissionState = null,
+                    newActivity = false,
+                    plannerOverride = null
+                )
+            }
+        }
+    }
+
+    protected fun List<PlannerItem>.mapContextName(): List<PlannerItem> {
+        return map { plannerItem ->
+            if (!plannerItem.contextName.isNullOrEmpty()) {
+                plannerItem
+            } else {
+                val contextName = if (plannerItem.courseId != null) canvasContexts.find { it.id == plannerItem.courseId }?.name else null
+                plannerItem.copy(contextName = contextName)
+            }
+        }
+    }
 }
