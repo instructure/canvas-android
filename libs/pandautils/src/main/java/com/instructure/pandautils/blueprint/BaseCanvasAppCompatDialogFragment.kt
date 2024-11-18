eb6ccb0ce3955545b5a3c9546fcef990665dc88d
Tamas Kozmer <tamas.kozmer@instructure.com>
2024-11-18 18:06:06 +0100

    PageViewFragmentDelegate

--- libs/pandautils/src/main/java/com/instructure/pandautils/blueprint/BaseCanvasAppCompatDialogFragment.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/blueprint/BaseCanvasAppCompatDialogFragment.kt
@@ -19,86 +19,56 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDialogFragment
-import com.instructure.canvasapi2.utils.pageview.PageViewVisibilityTracker
 import com.instructure.canvasapi2.utils.pageview.PageViewWindowFocus
-import com.instructure.canvasapi2.utils.pageview.PageViewWindowFocusListener
-import com.instructure.pandautils.utils.AppConfigProvider
-import com.instructure.pandautils.utils.PageViewAnnotationProcessor
-import com.instructure.pandautils.utils.ScreenViewAnnotationProcessor
 import com.instructure.pandautils.utils.showMasqueradeNotification
 
-open class BaseCanvasAppCompatDialogFragment : AppCompatDialogFragment(), PageViewWindowFocus {
+open class BaseCanvasAppCompatDialogFragment : AppCompatDialogFragment(), PageViewWindowFocus, PageViewPrerequisites {
 
     override fun onStart() {
         super.onStart()
         showMasqueradeNotification()
     }
 
-    private val visibilityTracker = PageViewVisibilityTracker()
-    private val pageViewAnnotationProcessor = PageViewAnnotationProcessor(this::class.java, this)
+    private val delegate by lazy { PageViewFragmentDelegate(this) }
 
-    open fun beforePageViewPrerequisites(): List<String> = emptyList()
+    override fun beforePageViewPrerequisites(): List<String> = emptyList()
 
     protected fun completePageViewPrerequisite(prerequisite: String) {
-        if (visibilityTracker.trackCustom(prerequisite, true, this)) {
-            pageViewAnnotationProcessor.startEvent()
-        }
+        delegate.completePageViewPrerequisite(prerequisite)
     }
 
     override fun onAttach(context: Context) {
-        visibilityTracker.addCustomConditions(beforePageViewPrerequisites())
-        if (AppConfigProvider.appConfig?.appName == "teacher") {
-            ScreenViewAnnotationProcessor.processScreenView(this::class.java)
-        }
+        delegate.onAttach(context)
         super.onAttach(context)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        if (AppConfigProvider.appConfig?.appName == "student" && isAdded && isVisible && userVisibleHint) {
-            ScreenViewAnnotationProcessor.processScreenView(this::class.java)
-        }
-
-        view.viewTreeObserver.addOnWindowFocusChangeListener(PageViewWindowFocusListener(this))
+        delegate.onViewCreated(view, savedInstanceState)
         super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
-        if (visibilityTracker.trackHidden(hidden, this)) {
-            pageViewAnnotationProcessor.startEvent()
-        } else {
-            pageViewAnnotationProcessor.stopEvent()
-        }
+        delegate.onHiddenChanged(hidden)
         super.onHiddenChanged(hidden)
     }
 
     @Deprecated("See description in superclass")
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
-        if (visibilityTracker.trackHidden(isVisibleToUser, this)) {
-            pageViewAnnotationProcessor.startEvent()
-        } else {
-            pageViewAnnotationProcessor.stopEvent()
-        }
+        delegate.setUserVisibleHint(isVisibleToUser)
         super.setUserVisibleHint(isVisibleToUser)
     }
 
     override fun onResume() {
-        if (visibilityTracker.trackResume(true, this)) {
-            pageViewAnnotationProcessor.startEvent()
-        }
+        delegate.onResume()
         super.onResume()
     }
 
     override fun onPause() {
-        visibilityTracker.trackResume(false, this)
-        pageViewAnnotationProcessor.stopEvent()
+        delegate.onPause()
         super.onPause()
     }
 
     override fun onPageViewWindowFocusChanged(hasFocus: Boolean) {
-        if (visibilityTracker.trackCustom("windowFocus", hasFocus, this)) {
-            pageViewAnnotationProcessor.startEvent()
-        } else {
-            pageViewAnnotationProcessor.stopEvent()
-        }
+        delegate.onPageViewWindowFocusChanged(hasFocus)
     }
 }
---
+++ libs/pandautils/src/main/java/com/instructure/pandautils/blueprint/PageViewFragmentDelegate.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2024 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use fragment file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package com.instructure.pandautils.blueprint
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.instructure.canvasapi2.utils.pageview.PageViewVisibilityTracker
+import com.instructure.canvasapi2.utils.pageview.PageViewWindowFocus
+import com.instructure.canvasapi2.utils.pageview.PageViewWindowFocusListener
+import com.instructure.pandautils.utils.AppConfigProvider
+import com.instructure.pandautils.utils.PageViewAnnotationProcessor
+import com.instructure.pandautils.utils.ScreenViewAnnotationProcessor
+
+class PageViewFragmentDelegate<T>(
+    private val fragment: T
+) where T : Fragment, T : PageViewWindowFocus, T : PageViewPrerequisites {
+
+    private val visibilityTracker = PageViewVisibilityTracker()
+    private val pageViewAnnotationProcessor = PageViewAnnotationProcessor(fragment::class.java, fragment)
+
+    fun completePageViewPrerequisite(prerequisite: String) {
+        if (visibilityTracker.trackCustom(prerequisite, true, fragment)) {
+            pageViewAnnotationProcessor.startEvent()
+        }
+    }
+
+    fun onAttach(context: Context) {
+        visibilityTracker.addCustomConditions(fragment.beforePageViewPrerequisites())
+        if (AppConfigProvider.appConfig?.appName == "teacher") {
+            ScreenViewAnnotationProcessor.processScreenView(fragment::class.java)
+        }
+    }
+
+    fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (AppConfigProvider.appConfig?.appName == "student" && fragment.isAdded && fragment.isVisible && fragment.userVisibleHint) {
+            ScreenViewAnnotationProcessor.processScreenView(fragment::class.java)
+        }
+
+        view.viewTreeObserver.addOnWindowFocusChangeListener(PageViewWindowFocusListener(fragment))
+    }
+
+    fun onHiddenChanged(hidden: Boolean) {
+        if (visibilityTracker.trackHidden(hidden, fragment)) {
+            pageViewAnnotationProcessor.startEvent()
+        } else {
+            pageViewAnnotationProcessor.stopEvent()
+        }
+    }
+
+    @Deprecated("See description in superclass")
+    fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        if (visibilityTracker.trackHidden(isVisibleToUser, fragment)) {
+            pageViewAnnotationProcessor.startEvent()
+        } else {
+            pageViewAnnotationProcessor.stopEvent()
+        }
+    }
+
+    fun onResume() {
+        if (visibilityTracker.trackResume(true, fragment)) {
+            pageViewAnnotationProcessor.startEvent()
+        }
+    }
+
+    fun onPause() {
+        visibilityTracker.trackResume(false, fragment)
+        pageViewAnnotationProcessor.stopEvent()
+    }
+
+    fun onPageViewWindowFocusChanged(hasFocus: Boolean) {
+        if (visibilityTracker.trackCustom("windowFocus", hasFocus, fragment)) {
+            pageViewAnnotationProcessor.startEvent()
+        } else {
+            pageViewAnnotationProcessor.stopEvent()
+        }
+    }
+}
