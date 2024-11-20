56b0bbc4d999eecb19c581b1671c7f8d39fd204b
domonkosadam <domonkosadam01@gmail.com>
2024-11-20 15:13:58 +0100

    Replace content with lazycolumn

--- apps/parent/src/main/java/com/instructure/parentapp/features/courses/details/summary/SummaryScreen.kt
+++ apps/parent/src/main/java/com/instructure/parentapp/features/courses/details/summary/SummaryScreen.kt
@@ -22,13 +22,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -139,12 +140,10 @@ private fun SummaryContentScreen(
     navigateToAssignmentDetails: (Long, Long) -> Unit,
     navigateToCalendarEvent: (String, Long, Long) -> Unit
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
+    LazyColumn(
+        contentPadding = PaddingValues(bottom = 64.dp),
     ) {
-        items.forEach {
+        items(items) {
             ScheduleItemRow(it, course, navigateToAssignmentDetails, navigateToCalendarEvent)
         }
     }
