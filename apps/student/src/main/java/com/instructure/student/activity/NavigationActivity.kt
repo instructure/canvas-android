26c378b6276573549acc72f1af153ea94a42b368
domonkosadam <53480952+domonkosadam@users.noreply.github.com>
2024-11-22 16:21:21 +0100

    [MBL-18013][Student][Parent] Refactor reminder logic (#2628)
    refs: MBL-18013
    affects: Student
    release note: Reminders can be set for assignments without due date.

--- apps/student/src/main/java/com/instructure/student/activity/NavigationActivity.kt
+++ apps/student/src/main/java/com/instructure/student/activity/NavigationActivity.kt
@@ -79,8 +79,8 @@ import com.instructure.loginapi.login.dialog.MasqueradingDialog
 import com.instructure.loginapi.login.tasks.LogoutTask
 import com.instructure.pandautils.analytics.OfflineAnalyticsManager
 import com.instructure.pandautils.binding.viewBinding
+import com.instructure.pandautils.features.reminder.AlarmScheduler
 import com.instructure.pandautils.utils.LocaleUtils
-import com.instructure.pandautils.features.assignments.details.reminder.AlarmScheduler
 import com.instructure.pandautils.features.calendar.CalendarFragment
 import com.instructure.pandautils.features.calendarevent.details.EventFragment
 import com.instructure.pandautils.features.help.HelpDialogFragment
