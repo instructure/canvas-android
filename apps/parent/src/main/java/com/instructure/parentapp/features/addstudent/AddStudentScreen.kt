7390d20e255b051a878376be566351a8924c1b3e
Kristof Nemere <109959688+kristofnemere@users.noreply.github.com>
2025-02-17 16:11:12 +0100

    [MBL-17730][Parent] Revise parent accessibility
    refs: MBL-17730
    affects: Parent
    release note: none
    
    * Navigation drawer a11y
    
    * Student selector a11y
    
    * Manage students a11y
    
    * Pairing a11y
    
    * Alert settings a11y
    
    * Alert list a11y
    
    * Grades a11y
    
    * Grade preferences a11y
    
    * Summary a11y
    
    * large font fixes
    
    * comment fix

--- apps/parent/src/main/java/com/instructure/parentapp/features/addstudent/AddStudentScreen.kt
+++ apps/parent/src/main/java/com/instructure/parentapp/features/addstudent/AddStudentScreen.kt
@@ -31,6 +31,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,7 +47,11 @@ fun AddStudentScreen(
     CanvasTheme {
         Column(modifier = Modifier.padding(vertical = 16.dp)) {
             Text(
-                modifier = Modifier.padding(start = 12.dp, end = 12.dp, bottom = 12.dp),
+                modifier = Modifier
+                    .padding(start = 12.dp, end = 12.dp, bottom = 12.dp)
+                    .semantics {
+                        heading()
+                    },
                 text = stringResource(id = R.string.addStudentTitle),
                 color = colorResource(id = R.color.textDarkest),
                 fontSize = 14.sp
