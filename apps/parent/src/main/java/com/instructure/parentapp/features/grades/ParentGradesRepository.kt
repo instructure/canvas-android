0f4e60cec590d6c7e716a9f68d92fd5bc3687441
Akos Hermann <akos.hermann@instructure.com>
2025-02-19 13:23:57 +0100

    Revert "[MBL-18583][Parent] 'Not Graded' type assignment hidden from Grades"
    This reverts commit 7f06207561d7701800b3adfea579332b0eb3e80b.

--- apps/parent/src/main/java/com/instructure/parentapp/features/grades/ParentGradesRepository.kt
+++ apps/parent/src/main/java/com/instructure/parentapp/features/grades/ParentGradesRepository.kt
@@ -20,8 +20,6 @@ package com.instructure.parentapp.features.grades
 import com.instructure.canvasapi2.apis.AssignmentAPI
 import com.instructure.canvasapi2.apis.CourseAPI
 import com.instructure.canvasapi2.builders.RestParams
-import com.instructure.canvasapi2.models.Assignment
-import com.instructure.canvasapi2.models.Assignment.Companion.getGradingTypeFromAPIString
 import com.instructure.canvasapi2.models.AssignmentGroup
 import com.instructure.canvasapi2.models.Course
 import com.instructure.canvasapi2.models.CourseGrade
@@ -48,9 +46,7 @@ class ParentGradesRepository(
             assignmentApi.getNextPageAssignmentGroupListWithAssignmentsForObserver(it, params)
         }.map {
             it.map { group ->
-                val filteredAssignments = group.assignments
-                    .filter { assignment -> assignment.published }
-                    .filter { assignment -> getGradingTypeFromAPIString(assignment.gradingType.orEmpty()) != Assignment.GradingType.NOT_GRADED }
+                val filteredAssignments = group.assignments.filter { assignment -> assignment.published }
                 group.copy(assignments = filteredAssignments).toAssignmentGroup(studentId)
             }
         }.dataOrThrow
