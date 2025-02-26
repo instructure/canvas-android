784c640e0c51a284ac753543e54fd880d01e461c
kristof.deak <kristof.deak@instructure.com>
2025-02-26 22:04:58 +0100

    Implement Course Details - Summary E2E test. Add some page object methods. Extend updateCourse API. Add/Refactor some logs to Syllabus E2E test.
    refs: MBL-18129
    affects: Parent
    release note:

---
+++ apps/parent/src/androidTest/java/com/instructure/parentapp/ui/e2e/CourseDetailsSummaryE2ETest.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+package com.instructure.parentapp.ui.e2e
+
+import android.util.Log
+import androidx.test.espresso.Espresso
+import com.instructure.canvasapi2.models.CanvasContext
+import com.instructure.canvasapi2.utils.toApiString
+import com.instructure.dataseeding.api.AssignmentsApi
+import com.instructure.dataseeding.api.CalendarEventApi
+import com.instructure.dataseeding.api.CoursesApi
+import com.instructure.dataseeding.api.QuizzesApi
+import com.instructure.dataseeding.model.SubmissionType
+import com.instructure.dataseeding.model.UpdateCourse
+import com.instructure.dataseeding.util.days
+import com.instructure.dataseeding.util.fromNow
+import com.instructure.dataseeding.util.iso8601
+import com.instructure.parentapp.utils.ParentComposeTest
+import com.instructure.parentapp.utils.seedData
+import com.instructure.parentapp.utils.tokenLogin
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Test
+import java.util.Date
+
+@HiltAndroidTest
+class CourseDetailsSummaryE2ETest : ParentComposeTest() {
+
+    override fun displaysPageObjects() = Unit
+
+    override fun enableAndConfigureAccessibilityChecks() = Unit
+
+    @Test
+    fun testCourseDetailsSummaryE2E() {
+
+        Log.d(PREPARATION_TAG, "Seeding data.")
+        val data = seedData(students = 1, teachers = 1, parents = 1, courses = 1)
+        val course = data.coursesList[0]
+        val teacher = data.teachersList[0]
+        val parent = data.parentsList[0]
+
+        Log.d(PREPARATION_TAG, "Seed assignment for '${course.name}' course.")
+        val assignment = AssignmentsApi.createAssignment(
+            course.id, teacher.token, submissionTypes = listOf(
+                SubmissionType.ON_PAPER
+            ), withDescription = true, pointsPossible = 15.0, dueAt = 1.days.fromNow.iso8601
+        )
+
+        Log.d(PREPARATION_TAG, "Seed a calendar event because it should be displayed on the Summary Page.")
+        val testCalendarEvent = CalendarEventApi.createCalendarEvent(
+            teacher.token,
+            CanvasContext.makeContextId(CanvasContext.Type.COURSE, course.id),
+            "Test Calendar Event",
+            Date().toApiString()
+        )
+
+        Log.d(PREPARATION_TAG,"Seed a published quiz because it should be displayed on the Summary Page.")
+        val testPublishedQuiz = QuizzesApi.createAndPublishQuiz(course.id, teacher.token, listOf())
+
+        Log.d(PREPARATION_TAG, "Update ${course.name} course to set Syllabus as Home Page (with some syllabus body) and enable 'Show Course Summary' setting to make the Sumary tab displayed in the Parent app.")
+        CoursesApi.updateCourse(course.id, UpdateCourse(syllabusBody = "Test syllabus body...", homePage = "syllabus", showSummary = 1))
+
+        Log.d(STEP_TAG, "Login with user: '${parent.name}', login id: '${parent.loginId}'.")
+        tokenLogin(parent)
+
+        Log.d(STEP_TAG, "Wait for the Dashboard Page to be rendered. Select '${course.name}' course.")
+        dashboardPage.waitForRender()
+        coursesPage.clickCourseItem(course.name)
+
+        Log.d(STEP_TAG,"Navigate to Summary Page by selecting Summary Tab.")
+        courseDetailsPage.selectTab("SUMMARY")
+
+        Log.d(ASSERTION_TAG, "Assert that the 'SUMMARY' tab has been selected.")
+        courseDetailsPage.assertTabSelected("SUMMARY")
+
+        Log.d(STEP_TAG, "Assert that all the' ${assignment.name}' assignment, '${testPublishedQuiz.title}' quiz and '${testCalendarEvent.title}' calendar event items are displayed on the Summary Page. ")
+        summaryPage.assertItemDisplayed(assignment.name)
+        summaryPage.assertItemDisplayed(testCalendarEvent.title.orEmpty())
+        summaryPage.assertItemDisplayed(testPublishedQuiz.title)
+
+        Log.d(STEP_TAG, "Select '${assignment.name}' Summary item (assignment) to navigate to the Assignment Details Page.")
+        summaryPage.selectItem(assignment.name)
+
+        Log.d(STEP_TAG, "Assert that the Assignment Details Page is loaded successfully.")
+        assignmentDetailsPage.assertAssignmentDetails(assignment)
+
+        Log.d(STEP_TAG, "Navigate back to the Summary Page.")
+        Espresso.pressBack()
+
+        Log.d(STEP_TAG, "Select '${testCalendarEvent.title}' Summary item (calendar event) to navigate to the Calendar Event Details Page.")
+        summaryPage.selectItem(testCalendarEvent.title.orEmpty())
+
+        Log.d(STEP_TAG, "Assert that the Calendar Event Details Page is loaded successfully.")
+        calendarEventDetailsPage.assertEventTitle(testCalendarEvent.title.orEmpty())
+    }
+
+}
--- apps/parent/src/androidTest/java/com/instructure/parentapp/ui/e2e/SyllabusE2ETest.kt
+++ apps/parent/src/androidTest/java/com/instructure/parentapp/ui/e2e/SyllabusE2ETest.kt
@@ -32,15 +32,15 @@ import org.junit.Test
 @HiltAndroidTest
 class SyllabusE2ETest : ParentComposeTest() {
 
+    override fun displaysPageObjects() = Unit
+
+    override fun enableAndConfigureAccessibilityChecks() = Unit
+
     @Test
     fun testSyllabusE2E() {
+
         Log.d(PREPARATION_TAG, "Seeding data.")
-        val data = seedData(
-            students = 1,
-            teachers = 1,
-            parents = 1,
-            courses = 1
-        )
+        val data = seedData(students = 1, teachers = 1, parents = 1, courses = 1)
         val course = data.coursesList[0]
         val teacher = data.teachersList[0]
         val parent = data.parentsList[0]
@@ -55,27 +55,30 @@ class SyllabusE2ETest : ParentComposeTest() {
         val url = "https://mobileqa.beta.instructure.com/courses/${course.id}/assignments/${assignment.id}"
         val syllabusBody = "this is the syllabus body <a id=\"assignmentLink\" href=\"$url\">Assignment</a>"
 
+        Log.d(PREPARATION_TAG, "Update ${course.name} course to set Syllabus with some syllabus body and a link to the '${assignment.name}' assignment.")
         CoursesApi.updateCourse(course.id, UpdateCourse(syllabusBody = syllabusBody))
 
-        Log.d(STEP_TAG, "Login with user: ${parent.name}, login id: ${parent.loginId}.")
+        Log.d(STEP_TAG, "Login with user: '${parent.name}', login id: '${parent.loginId}'.")
         tokenLogin(parent)
 
         Log.d(STEP_TAG, "Wait for the Dashboard Page to be rendered. Select '${course.name}' course.")
         dashboardPage.waitForRender()
         coursesPage.clickCourseItem(course.name)
 
-        Log.d(STEP_TAG,"Navigate to Syllabus Page. Assert that the syllabus body string is displayed and click the link for the assignment.")
+        Log.d(STEP_TAG,"Navigate to Syllabus Page.")
         courseDetailsPage.selectTab("SYLLABUS")
+
+        Log.d(ASSERTION_TAG, "Assert that the 'SYLLABUS' tab has been selected.")
         courseDetailsPage.assertTabSelected("SYLLABUS")
+
+        Log.d(ASSERTION_TAG, "Assert that the syllabus body string is displayed.")
         syllabusPage.assertSyllabusBody("this is the syllabus body Assignment")
+
+        Log.d(STEP_TAG, "Click the '${assignment.name}' assignment's link within the syllabus body.")
         syllabusPage.clickLink("assignmentLink")
 
         Log.d(STEP_TAG, "Assert that the Assignment Details Page is loaded successfully.")
         assignmentDetailsPage.assertAssignmentDetails(assignment)
-
     }
 
-    override fun displaysPageObjects() = Unit
-
-    override fun enableAndConfigureAccessibilityChecks() = Unit
 }
