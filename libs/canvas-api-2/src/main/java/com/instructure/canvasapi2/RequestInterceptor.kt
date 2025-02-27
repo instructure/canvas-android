371a00d1953f5a68a1a013f943798109da5bedfd
Akos Hermann <akos.hermann@instructure.com>
2025-02-27 15:15:54 +0100

    fix

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/RequestInterceptor.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/RequestInterceptor.kt
@@ -100,10 +100,9 @@ class RequestInterceptor : Interceptor {
 
 
         if (domain.isNotEmpty() && params.domain != null && domain != params.domain) {
-            val uri = Uri.parse(domain)
-            val host = uri.host?.takeIf { it.isNotBlank() } ?: domain.removePrefix("https://")
+            val uri = Uri.parse(params.domain)
             val url = request.url.newBuilder().scheme(uri.scheme ?: "https")
-                .host(host).build()
+                .host(uri.host ?: params.domain.removePrefix("https://")).build()
             request = request.newBuilder().url(url).build()
         }
 
