93221adf51f5f7d530d7d6e4d72cf823956ff0dd
kristof.nemere <kristof.nemere@instructure.com>
2025-03-03 16:29:15 +0100

    Fix webview content fragment navigation

--- apps/parent/src/main/java/com/instructure/parentapp/util/navigation/ParentWebViewRouter.kt
+++ apps/parent/src/main/java/com/instructure/parentapp/util/navigation/ParentWebViewRouter.kt
@@ -16,10 +16,14 @@
  */
 package com.instructure.parentapp.util.navigation
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import com.instructure.canvasapi2.models.CanvasContext
 import com.instructure.pandautils.navigation.WebViewRouter
+import com.instructure.pandautils.utils.toast
 import com.instructure.parentapp.R
 
 class ParentWebViewRouter(
@@ -35,19 +39,32 @@ class ParentWebViewRouter(
         navigation.canNavigate(activity, url, true)
     }
 
-    override fun openMedia(url: String, mime: String, filename: String, canvasContext: CanvasContext?) {
+    override fun openMedia(url: String, mime: String, filename: String, canvasContext: CanvasContext?) = safeNavigate(url) {
         navigation.navigate(activity, navigation.internalWebViewRoute(url, url))
     }
 
-    override fun routeExternally(url: String) {
+    override fun routeExternally(url: String) = safeNavigate(url) {
         navigation.navigate(activity, navigation.internalWebViewRoute(url, url))
     }
 
-    override fun openLtiScreen(canvasContext: CanvasContext?, url: String) {
+    override fun openLtiScreen(canvasContext: CanvasContext?, url: String) = safeNavigate(url) {
         navigation.navigate(activity, navigation.ltiLaunchRoute(url, activity.getString(R.string.utils_externalToolTitle), sessionlessLaunch = true))
     }
 
-    override fun launchInternalWebViewFragment(url: String, canvasContext: CanvasContext?) {
+    override fun launchInternalWebViewFragment(url: String, canvasContext: CanvasContext?) = safeNavigate(url) {
         navigation.navigate(activity, navigation.internalWebViewRoute(url, canvasContext?.name ?: url))
+    }
+
+    private fun safeNavigate(url: String, route: () -> Unit) = try {
+        route()
+    } catch (e: Exception) {
+        navigateToExternalBrowser(url)
+    }
+
+    private fun navigateToExternalBrowser(url: String) = try {
+        val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        activity.startActivity(urlIntent)
+    } catch (e: ActivityNotFoundException) {
+        activity.toast(R.string.inboxMessageFailedToOpenUrl)
     }
 }
