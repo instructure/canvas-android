54c03dc4d85d0996b20896af3a4529c8ba285de6
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2025-03-07 15:09:46 +0100

    [MBL-18129][Parent] - Create Course Details Summary Tab E2E Test (#2878)
    * Implement Course Details - Summary E2E test.
    Add some page object methods.
    Extend updateCourse API.
    Add/Refactor some logs to Syllabus E2E test.
    
    refs: MBL-18129
    affects: Parent
    release note:
    
    * Add E2E annotation and test metadata to syllabusE2E test.
    
    * Add annotation and metadata to tests + minor refactor on front page and syllabus e2e test
    
    * Fix breaking PushNotificationsE2E tests (material3 PR broke them).

--- apps/parent/src/androidTest/java/com/instructure/parentapp/ui/e2e/CourseDetailsFrontPageE2ETest.kt
+++ apps/parent/src/androidTest/java/com/instructure/parentapp/ui/e2e/CourseDetailsFrontPageE2ETest.kt
@@ -16,6 +16,12 @@
 package com.instructure.parentapp.ui.e2e
 
 import android.util.Log
+import com.instructure.canvas.espresso.E2E
+import com.instructure.canvas.espresso.FeatureCategory
+import com.instructure.canvas.espresso.Priority
+import com.instructure.canvas.espresso.SecondaryFeatureCategory
+import com.instructure.canvas.espresso.TestCategory
+import com.instructure.canvas.espresso.TestMetaData
 import com.instructure.dataseeding.api.AssignmentsApi
 import com.instructure.dataseeding.api.CoursesApi
 import com.instructure.dataseeding.api.PagesApi
@@ -31,54 +37,58 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Test
 
 @HiltAndroidTest
-class FontPageE2ETest : ParentComposeTest() {
+class CourseDetailsFrontPageE2ETest : ParentComposeTest() {
 
     override fun displaysPageObjects() = Unit
 
     override fun enableAndConfigureAccessibilityChecks() = Unit
 
+    @E2E
     @Test
-    fun testFrontPageE2E() {
+    @TestMetaData(Priority.MANDATORY, FeatureCategory.COURSE_DETAILS, TestCategory.E2E, SecondaryFeatureCategory.FRONT_PAGE)
+    fun testCourseDetailsFrontPageE2E() {
+
         Log.d(PREPARATION_TAG, "Seeding data.")
-        val data = seedData(
-            students = 1,
-            teachers = 1,
-            parents = 1,
-            courses = 1
-        )
+        val data = seedData(students = 1, teachers = 1, parents = 1, courses = 1)
         val course = data.coursesList[0]
         val teacher = data.teachersList[0]
         val parent = data.parentsList[0]
 
-        Log.d(PREPARATION_TAG, "Seed assignment.")
-        val assignment = AssignmentsApi.createAssignment(
-            course.id,
-            teacher.token,
+        Log.d(PREPARATION_TAG, "Seed assignment for '${course.name}' course.")
+        val assignment = AssignmentsApi.createAssignment(course.id, teacher.token,
             submissionTypes = listOf(SubmissionType.ON_PAPER),
             withDescription = true,
             pointsPossible = 15.0,
             dueAt = 1.days.fromNow.iso8601
         )
-        Log.d(PREPARATION_TAG, "Update course to have front page.")
+
         val url = "https://mobileqa.beta.instructure.com/courses/${course.id}/assignments/${assignment.id}"
         val frontPageBody = "This is the front page body <a id=\"assignmentLink\" href=\"$url\">Assignment</a>"
+
+        Log.d(PREPARATION_TAG, "Seed a (front) page to the ${course.name} course and set the Home Page of the course to the seeded front page.")
         PagesApi.createCoursePage(course.id, teacher.token, frontPage = true, editingRoles = "public", body = frontPageBody)
         CoursesApi.updateCourse(course.id, UpdateCourse(homePage = "wiki"))
 
-        Log.d(STEP_TAG, "Login with user: ${parent.name}, login id: ${parent.loginId}.")
+        Log.d(STEP_TAG, "Login with user: '${parent.name}', login id: '${parent.loginId}'.")
         tokenLogin(parent)
 
         Log.d(STEP_TAG, "Wait for the Dashboard Page to be rendered. Select '${course.name}' course.")
         dashboardPage.waitForRender()
         coursesPage.clickCourseItem(course.name)
 
-        Log.d(STEP_TAG, "Navigate to Front Page. Assert that the body string is displayed and click the link for the assignment.")
+        Log.d(STEP_TAG,"Navigate to Front Page Page by selecting Summary Tab.")
         courseDetailsPage.selectTab("FRONT PAGE")
+
+        Log.d(ASSERTION_TAG, "Assert that the 'FRONT PAGE' tab has been selected.")
         courseDetailsPage.assertTabSelected("FRONT PAGE")
+
+        Log.d(ASSERTION_TAG, "Assert that the body string is displayed and click the link for the assignment.")
         frontPagePage.assertFrontPageBody("This is the front page body Assignment")
+
+        Log.d(STEP_TAG, "Click on the 'assignmentLink' link text on the Front Page.")
         frontPagePage.clickLink("assignmentLink")
 
-        Log.d(STEP_TAG, "Assert that the Assignment Details Page is loaded successfully.")
+        Log.d(ASSERTION_TAG, "Assert that the Assignment Details Page is loaded successfully.")
         assignmentDetailsPage.assertAssignmentDetails(assignment)
     }
 }
