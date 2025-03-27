db864a5c79965cf91a08a31736c0721645531803
Tamas Kozmer <72397075+tamaskozmer@users.noreply.github.com>
2025-03-27 15:38:08 +0100

    [Horizon] Buttons and badges #2923

--- libs/horizon/src/main/java/com/instructure/horizon/horizonui/foundation/HorizonBorder.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/horizonui/foundation/HorizonBorder.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
-// This color is the UI Colors/Lines and Borders/Stroke color from the Figma file. We should define it here, because we can't use composables in BorderStroke.
-private val borderStrokeColor = Color(0xFF273540)
-
 object HorizonBorder {
-    val level1 = BorderStroke(1.dp, borderStrokeColor)
-    val level2 = BorderStroke(2.dp, borderStrokeColor)
+    fun level1(color: Color = HorizonColors.LineAndBorder.lineStroke()) = BorderStroke(1.dp, color)
+    fun level2(color: Color = HorizonColors.LineAndBorder.lineStroke()) = BorderStroke(2.dp, color)
 }
