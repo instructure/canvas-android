79a87ccbbfa4af20c78ef572d0b83537cff75db3
domonkosadam <domonkosadam01@gmail.com>
2025-04-17 16:44:08 +0200

    Module card animation improvements

--- libs/horizon/src/main/java/com/instructure/horizon/features/learn/progress/LearnProgressScreen.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/features/learn/progress/LearnProgressScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -73,6 +74,7 @@ private fun LearnProgressContent(
                     moduleHeaderState.second.forEach { moduleItemState ->
                         ModuleItemCard(
                             state = moduleItemState,
+                            modifier = Modifier.padding(bottom = 8.dp),
                         )
                     }
                 }
