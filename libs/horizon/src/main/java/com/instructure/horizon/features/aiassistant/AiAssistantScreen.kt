e5854276bffd952da4b5f09a9162634234112f0d
domonkosadam <domonkosadam01@gmail.com>
2025-05-06 13:39:45 +0200

    Implement design system

--- libs/horizon/src/main/java/com/instructure/horizon/features/aiassistant/AiAssistantScreen.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/features/aiassistant/AiAssistantScreen.kt
@@ -15,22 +15,46 @@
  */
 package com.instructure.horizon.features.aiassistant
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.instructure.horizon.features.aiassistant.common.AiAssistDetailedFeedback
+import com.instructure.horizon.features.aiassistant.common.AiAssistFeedback
+import com.instructure.horizon.features.aiassistant.common.AiAssistFeedbackType
+import com.instructure.horizon.features.aiassistant.common.AiAssistInput
+import com.instructure.horizon.features.aiassistant.common.AiAssistPagination
+import com.instructure.horizon.features.aiassistant.common.AiAssistResponseTextBlock
+import com.instructure.horizon.features.aiassistant.common.AiAssistResponseTextBlockSource
+import com.instructure.horizon.features.aiassistant.common.AiAssistSuggestionTextBlock
+import com.instructure.horizon.features.aiassistant.common.AiAssistToolbar
+import com.instructure.horizon.features.aiassistant.common.AiAssistUserTextBlock
+import com.instructure.horizon.features.aiassistant.flashcard.AiAssistFlashCard
+import com.instructure.horizon.features.aiassistant.quiz.AiAssistQuizAnswer
+import com.instructure.horizon.features.aiassistant.quiz.AiAssistQuizAnswerStatus
+import com.instructure.horizon.features.aiassistant.quiz.AiAssistQuizFooter
+import com.instructure.horizon.features.aiassistant.quiz.AiAssistQuizRatingFooter
 import com.instructure.horizon.horizonui.foundation.HorizonColors
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun AiAssistantScreen(navHostController: NavHostController) {
     val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
@@ -42,7 +66,107 @@ fun AiAssistantScreen(navHostController: NavHostController) {
                     brush = HorizonColors.Surface.aiGradient()
                 )
         ) {
-            Text(text = "Ai assistant Screen", modifier = Modifier.align(Alignment.Center), fontSize = 48.sp)
+            LazyColumn(
+                contentPadding = PaddingValues(24.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+            ) {
+                stickyHeader {
+                    AiAssistToolbar({}, {})
+                }
+
+                item {
+                    var selected: AiAssistFeedbackType? by remember { mutableStateOf(null) }
+                    AiAssistFeedback(selected, { selected = AiAssistFeedbackType.POSITIVE }, { selected = AiAssistFeedbackType.NEGATIVE })
+                }
+
+                item {
+                    var value by remember { mutableStateOf(TextFieldValue("")) }
+                    AiAssistInput(
+                        value = value,
+                        onValueChange = { value = it },
+                        onSubmitPressed = {}
+                    )
+                }
+
+                item {
+                    AiAssistQuizFooter(true, {}, {})
+                }
+
+                item {
+                    AiAssistQuizFooter(false, {}, {})
+                }
+
+                item {
+                    var selected: AiAssistFeedbackType? by remember { mutableStateOf(null) }
+                    AiAssistQuizRatingFooter(selected, { selected = AiAssistFeedbackType.POSITIVE }, { selected = AiAssistFeedbackType.NEGATIVE }, {})
+                }
+
+                item {
+                    var currentPage by remember { mutableStateOf(1) }
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        AiAssistPagination(currentPage, 5, { currentPage++ }, { currentPage-- })
+                    }
+                }
+
+                item {
+                    AiAssistSuggestionTextBlock("This is a suggestion for the AI Assist", {})
+                }
+
+                item {
+                    AiAssistUserTextBlock("This is an input from the User")
+                }
+
+                item {
+                    var selected: AiAssistFeedbackType? by remember { mutableStateOf(null) }
+                    AiAssistResponseTextBlock(
+                        "This is a response from the AI Assist",
+                        listOf(
+                            AiAssistResponseTextBlockSource("Source 1", "https://source1.com"),
+                            AiAssistResponseTextBlockSource("Source 2", "https://source2.com")
+                        ),
+                        selected,
+                        {},
+                        { selected = AiAssistFeedbackType.POSITIVE },
+                        { selected = AiAssistFeedbackType.NEGATIVE },
+                    )
+                }
+
+                item {
+                    AiAssistDetailedFeedback(
+                        onSubmit = {},
+                        onDismiss = {}
+                    )
+                }
+
+                item {
+                    var selectedIndex by remember { mutableStateOf(0) }
+                    AiAssistQuizAnswer(
+                        "This is a quiz answer",
+                        AiAssistQuizAnswerStatus.entries[selectedIndex],
+                        {
+                            if (selectedIndex == AiAssistQuizAnswerStatus.entries.lastIndex) {
+                                selectedIndex = 0
+                            } else {
+                                selectedIndex++
+                            }
+                        }
+                    )
+                }
+
+                item {
+                    var isFlippedToAnswer by remember { mutableStateOf(false) }
+
+                    AiAssistFlashCard(
+                        question = "What is the capital of France?",
+                        answer = "Paris",
+                        isFlippedToAnswer = isFlippedToAnswer,
+                        onClick = { isFlippedToAnswer = !isFlippedToAnswer },
+                    )
+                }
+            }
         }
     }
 }
---
+++ libs/horizon/src/main/java/com/instructure/horizon/features/aiassistant/common/AiAssistInput.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2025 - present Instructure, Inc.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.instructure.horizon.features.aiassistant.common
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.instructure.horizon.R
+import com.instructure.horizon.horizonui.foundation.HorizonSpace
+import com.instructure.horizon.horizonui.foundation.SpaceSize
+import com.instructure.horizon.horizonui.molecules.IconButton
+import com.instructure.horizon.horizonui.molecules.IconButtonColor
+
+@Composable
+fun AiAssistInput(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    onSubmitPressed: () -> Unit,
+    modifier: Modifier = Modifier,
+    label: String = stringResource(R.string.aiAssistEnterAPromptLabel),
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.Bottom,
+    ) {
+        AiAssistTextArea(
+            value = value,
+            onValueChange = onValueChange,
+            label = label,
+            modifier = Modifier.weight(1f),
+        )
+
+        HorizonSpace(SpaceSize.SPACE_16)
+
+        IconButton(
+            iconRes = R.drawable.arrow_upward,
+            contentDescription = stringResource(R.string.aiAssistSubmitPrompt),
+            color = IconButtonColor.INVERSE,
+            onClick = onSubmitPressed,
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun AiAssistInputPreview() {
+    AiAssistInput(
+        value = TextFieldValue(""),
+        onValueChange = {},
+        onSubmitPressed = {},
+        modifier = Modifier,
+    )
+}
