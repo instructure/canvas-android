6a2807a4b7258ddfebf23b0f32946e5c5b4e1773
domonkosadam <domonkosadam01@gmail.com>
2025-05-14 10:25:36 +0200

    Fix finding

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/QLClientConfig.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/QLClientConfig.kt
@@ -58,7 +58,7 @@ open class QLClientConfig {
     var fetchPolicy: HttpFetchPolicy = HttpFetchPolicy.CacheFirst
 
     /** Builds a new [ApolloClient] based on the current config values. */
-    fun buildClient(customFetchPolicy: HttpFetchPolicy? = null): ApolloClient {
+    fun buildClient(): ApolloClient {
         val builder = ApolloClient.Builder()
             .serverUrl(url)
             .okHttpClient(httpClient)
@@ -66,7 +66,7 @@ open class QLClientConfig {
             .addCustomScalarAdapter(URL.type, stringAdapter)
             .addCustomScalarAdapter(GraphQLID.type, stringAdapter)
             .httpCache(cacheFile, CACHE_SIZE)
-            .httpFetchPolicy(customFetchPolicy ?: fetchPolicy)
+            .httpFetchPolicy(fetchPolicy)
             .httpExpireTimeout(TimeUnit.HOURS.toMillis(1))
 
         return builder.build()
