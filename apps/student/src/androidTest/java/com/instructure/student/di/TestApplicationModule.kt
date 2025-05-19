8e510712bc8665e3e520dc76535d6125ce37b93e
kristof.nemere <kristof.nemere@instructure.com>
2025-05-19 11:34:11 +0200

    Test fix

--- apps/student/src/androidTest/java/com/instructure/student/di/TestApplicationModule.kt
+++ apps/student/src/androidTest/java/com/instructure/student/di/TestApplicationModule.kt
@@ -1,9 +1,13 @@
 package com.instructure.student.di
 
+import com.instructure.canvasapi2.apis.CourseAPI
+import com.instructure.canvasapi2.apis.GroupAPI
+import com.instructure.canvasapi2.apis.PlannerAPI
 import com.instructure.pandautils.utils.LogoutHelper
 import com.instructure.student.espresso.fakes.FakeEnabledTabs
 import com.instructure.student.router.EnabledTabs
 import com.instructure.student.util.StudentLogoutHelper
+import com.instructure.student.widget.todo.ToDoWidgetRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
@@ -25,5 +29,14 @@ class TestApplicationModule {
     @Singleton
     fun provideEnabledTabs(): EnabledTabs {
         return FakeEnabledTabs()
+    }
+
+    @Provides
+    fun provideToDoWidgetRepository(
+        plannerApi: PlannerAPI.PlannerInterface,
+        coursesApi: CourseAPI.CoursesInterface,
+        groupApi: GroupAPI.GroupInterface
+    ): ToDoWidgetRepository {
+        return ToDoWidgetRepository(plannerApi, coursesApi, groupApi)
     }
 }
