656502ef51414ceb35fc2117f8092c80a8f6803b
domonkosadam <domonkosadam01@gmail.com>
2025-07-08 13:57:04 +0200

    Extract string resource

--- libs/horizon/src/main/java/com/instructure/horizon/horizonui/organisms/inputs/common/InputDropDownPopup.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/horizonui/organisms/inputs/common/InputDropDownPopup.kt
@@ -34,11 +34,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
+import com.instructure.horizon.R
 import com.instructure.horizon.horizonui.foundation.HorizonColors
 import com.instructure.horizon.horizonui.foundation.HorizonCornerRadius
 import com.instructure.horizon.horizonui.foundation.HorizonElevation
@@ -115,7 +117,7 @@ fun <T>InputDropDownPopup(
                             modifier = Modifier
                                 .fillMaxWidth()
                         ) {
-                            SingleSelectItem("No options available")
+                            SingleSelectItem(stringResource(R.string.noOptionsAvailable))
                         }
                     } else {
                         options.forEach { selectionOption ->
