f7a72b131fd0c5ac2120f1b0562b4d884d05f168
Akos Hermann <akos.hermann@instructure.com>
2025-07-24 15:48:47 +0200

    add feature flag

--- libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/utils/RemoteConfigUtils.kt
+++ libs/canvas-api-2/src/main/java/com/instructure/canvasapi2/utils/RemoteConfigUtils.kt
@@ -15,7 +15,8 @@ enum class RemoteConfigParam(val rc_name: String, val safeValueAsString: String)
     TEST_BOOL("test_bool", "false"),
     TEST_FLOAT("test_float", "0f"),
     TEST_LONG("test_long", "42"),
-    TEST_STRING("test_string", "hey there")
+    TEST_STRING("test_string", "hey there"),
+    SPEEDGRADER_V2("speedgrader_v2", "false"),
 }
 
 /**
@@ -47,7 +48,7 @@ object RemoteConfigUtils {
         this.prefs = prefs
 
         val configSettings = FirebaseRemoteConfigSettings.Builder()
-                .setMinimumFetchIntervalInSeconds(3600) // once an hour allowed; you may want to adjust when debugging
+                .setMinimumFetchIntervalInSeconds(0) // once an hour allowed; you may want to adjust when debugging
                 .build()
         remoteConfig.setConfigSettingsAsync(configSettings)
 
