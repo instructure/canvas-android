13ba5c93f3b646b40f70cc846455ffdb65b1d27e
Akos Hermann <akos.hermann@instructure.com>
2025-07-24 13:33:08 +0200

    CR

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/speedgrader/SpeedGraderBottomSheetUiState.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/speedgrader/SpeedGraderBottomSheetUiState.kt
@@ -16,24 +16,7 @@
  */
 package com.instructure.pandautils.features.speedgrader
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import javax.inject.Inject
-
-@HiltViewModel
-class SpeedGraderBottomSheetViewModel @Inject constructor() : ViewModel() {
-
-    private val _uiState = MutableStateFlow(SpeedGraderBottomSheetUiState(onTabSelected = this::selectTab))
-    val uiState = _uiState.asStateFlow()
-
-    private fun selectTab(tabOrdinal: Int) {
-        viewModelScope.launch {
-            _uiState.update { it.copy(selectedTab = tabOrdinal) }
-        }
-    }
-}
+data class SpeedGraderBottomSheetUiState(
+    val selectedTab: Int = SpeedGraderTab.GRADE.ordinal,
+    val onTabSelected: (Int) -> Unit
+)
