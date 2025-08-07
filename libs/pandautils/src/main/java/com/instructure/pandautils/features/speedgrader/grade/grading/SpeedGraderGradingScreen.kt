5b76506b92b2bb14be75d1e637b8ac1584e66103
andras.maczak <andras.maczak@instructure.com>
2025-08-07 13:05:53 +0200

    Excused label fixed

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/speedgrader/grade/grading/SpeedGraderGradingScreen.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/speedgrader/grade/grading/SpeedGraderGradingScreen.kt
@@ -392,7 +392,8 @@ private fun LateHeader(daysLate: Int, dueDate: Date?) {
 @Composable
 private fun LetterGradeGradingTypeInput(uiState: SpeedGraderGradingUiState) {
     val options = uiState.letterGrades.map { it.name }
-    val notGraded = stringResource(R.string.not_graded)
+    val defaultItem =
+        if (uiState.excused) stringResource(R.string.gradeExcused) else stringResource(R.string.not_graded)
 
     var textFieldScore by remember(uiState.enteredScore) {
         mutableStateOf(uiState.enteredScore?.let {
@@ -414,7 +415,7 @@ private fun LetterGradeGradingTypeInput(uiState: SpeedGraderGradingUiState) {
                     uiState.pointsPossible.orDefault(),
                     uiState.letterGrades
                 )
-            } ?: notGraded
+            } ?: defaultItem
         )
     }
 
