830b1c52f2a332fad25c671bac6dcc13b4b5d42b
domonkosadam <domonkosadam01@gmail.com>
2025-09-30 10:06:45 +0200

    Implement notifications tests

--- libs/horizon/src/androidTest/java/com/instructure/horizon/ui/features/notification/HorizonNotificationUiTest.kt
+++ libs/horizon/src/androidTest/java/com/instructure/horizon/ui/features/notification/HorizonNotificationUiTest.kt
@@ -16,5 +16,194 @@
  */
 package com.instructure.horizon.ui.features.notification
 
+import androidx.compose.ui.test.assertHasClickAction
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.rememberNavController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.instructure.horizon.features.notification.NotificationItem
+import com.instructure.horizon.features.notification.NotificationItemCategory
+import com.instructure.horizon.features.notification.NotificationRoute
+import com.instructure.horizon.features.notification.NotificationScreen
+import com.instructure.horizon.features.notification.NotificationUiState
+import com.instructure.horizon.horizonui.molecules.StatusChipColor
+import com.instructure.horizon.horizonui.platform.LoadingState
+import com.instructure.pandautils.utils.localisedFormat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+@RunWith(AndroidJUnit4::class)
 class HorizonNotificationUiTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testLoadingState() {
+        val state = NotificationUiState(
+            screenState = LoadingState(isLoading = true),
+        )
+        composeTestRule.setContent {
+            NotificationScreen(state, rememberNavController())
+        }
+
+        composeTestRule.onNodeWithText("Notifications")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithTag("LoadingSpinner")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Navigate back")
+            .assertIsDisplayed()
+            .assertHasClickAction()
+    }
+
+    @Test
+    fun testContentStateTodayFormat() {
+        val state = NotificationUiState(
+            screenState = LoadingState(),
+            notificationItems = listOf(
+                NotificationItem(
+                    category = NotificationItemCategory("Announcement", StatusChipColor.Sky),
+                    courseLabel = "Biology 101",
+                    title = "New Announcement",
+                    date = Date(),
+                    isRead = false,
+                    route = NotificationRoute.DeepLink("")
+                )
+            )
+        )
+        composeTestRule.setContent {
+            NotificationScreen(state, rememberNavController())
+        }
+
+        composeTestRule.onNodeWithText("Notifications")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Navigate back")
+            .assertIsDisplayed()
+            .assertHasClickAction()
+
+        composeTestRule.onNodeWithText("Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("New Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Biology 101")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Today")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun testContentStateYesterdayFormat() {
+        val state = NotificationUiState(
+            screenState = LoadingState(),
+            notificationItems = listOf(
+                NotificationItem(
+                    category = NotificationItemCategory("Announcement", StatusChipColor.Sky),
+                    courseLabel = "Biology 101",
+                    title = "New Announcement",
+                    date = Calendar.getInstance().apply { add(Calendar.DAY_OF_MONTH, -1) }.time,
+                    isRead = false,
+                    route = NotificationRoute.DeepLink("")
+                )
+            )
+        )
+        composeTestRule.setContent {
+            NotificationScreen(state, rememberNavController())
+        }
+
+        composeTestRule.onNodeWithText("Notifications")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Navigate back")
+            .assertIsDisplayed()
+            .assertHasClickAction()
+
+        composeTestRule.onNodeWithText("Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("New Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Biology 101")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Yesterday")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun testContentStateDayOfWeekFormat() {
+        val date = Calendar.getInstance().apply { add(Calendar.DAY_OF_MONTH, -3) }.time
+        val state = NotificationUiState(
+            screenState = LoadingState(),
+            notificationItems = listOf(
+                NotificationItem(
+                    category = NotificationItemCategory("Announcement", StatusChipColor.Sky),
+                    courseLabel = "Biology 101",
+                    title = "New Announcement",
+                    date = date,
+                    isRead = false,
+                    route = NotificationRoute.DeepLink("")
+                )
+            )
+        )
+        composeTestRule.setContent {
+            NotificationScreen(state, rememberNavController())
+        }
+        val dateString = SimpleDateFormat("EEEE", Locale.getDefault()).format(date)
+
+        composeTestRule.onNodeWithText("Notifications")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Navigate back")
+            .assertIsDisplayed()
+            .assertHasClickAction()
+
+        composeTestRule.onNodeWithText("Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("New Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Biology 101")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText(dateString)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun testContentStateDateFormat() {
+        val date = Calendar.getInstance().apply { add(Calendar.DAY_OF_MONTH, -7) }.time
+        val state = NotificationUiState(
+            screenState = LoadingState(),
+            notificationItems = listOf(
+                NotificationItem(
+                    category = NotificationItemCategory("Announcement", StatusChipColor.Sky),
+                    courseLabel = "Biology 101",
+                    title = "New Announcement",
+                    date = date,
+                    isRead = false,
+                    route = NotificationRoute.DeepLink("")
+                )
+            )
+        )
+        composeTestRule.setContent {
+            NotificationScreen(state, rememberNavController())
+        }
+        val dateString = date.localisedFormat("MMM dd, yyyy")
+
+        composeTestRule.onNodeWithText("Notifications")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Navigate back")
+            .assertIsDisplayed()
+            .assertHasClickAction()
+
+        composeTestRule.onNodeWithText("Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("New Announcement")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText("Biology 101")
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithText(dateString)
+            .assertIsDisplayed()
+    }
 }
--- libs/horizon/src/test/java/com/instructure/horizon/features/notification/NotificationRepositoryTest.kt
+++ libs/horizon/src/test/java/com/instructure/horizon/features/notification/NotificationRepositoryTest.kt
@@ -16,5 +16,157 @@
  */
 package com.instructure.horizon.features.notification
 
+import com.instructure.canvasapi2.apis.AccountNotificationAPI
+import com.instructure.canvasapi2.apis.CourseAPI
+import com.instructure.canvasapi2.apis.StreamAPI
+import com.instructure.canvasapi2.managers.CourseWithProgress
+import com.instructure.canvasapi2.managers.HorizonGetCoursesManager
+import com.instructure.canvasapi2.models.AccountNotification
+import com.instructure.canvasapi2.models.Course
+import com.instructure.canvasapi2.models.StreamItem
+import com.instructure.canvasapi2.utils.ApiPrefs
+import com.instructure.canvasapi2.utils.DataResult
+import com.instructure.canvasapi2.utils.LinkHeaders
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
 class NotificationRepositoryTest {
+    private val apiPrefs: ApiPrefs = mockk(relaxed = true)
+    private val streamApi: StreamAPI.StreamInterface = mockk(relaxed = true)
+    private val courseApi: CourseAPI.CoursesInterface = mockk(relaxed = true)
+    private val accountNotificationApi: AccountNotificationAPI.AccountNotificationInterface = mockk(relaxed = true)
+    private val getCoursesManager: HorizonGetCoursesManager = mockk(relaxed = true)
+
+    private val userId = 1L
+    @Before
+    fun setup() {
+        every { apiPrefs.user?.id } returns userId
+    }
+
+    @Test
+    fun `Test successful Stream API notification items filtering`() = runTest {
+        val validCourse = CourseWithProgress(
+            courseId = 1L,
+            courseName = "Course 1",
+            courseSyllabus = "",
+            progress = 5.0
+        )
+        val notValidCourse = CourseWithProgress(
+            courseId = 2L,
+            courseName = "Course 2",
+            courseSyllabus = "",
+            progress = 0.0
+        )
+        val conversationStreamItem = StreamItem(
+            type = "Conversation",
+            course_id = validCourse.courseId,
+        )
+        val messageStreamItem = StreamItem(
+            type = "Message",
+            course_id = validCourse.courseId,
+        )
+        val discussionStreamItem = StreamItem(
+            type = "DiscussionTopic",
+            course_id = validCourse.courseId,
+        )
+        val announcementStreamItem = StreamItem(
+            type = "Announcement",
+            course_id = validCourse.courseId,
+        )
+        val notValidAnnouncementStreamItem = StreamItem(
+            type = "Announcement",
+            course_id = notValidCourse.courseId,
+        )
+        val dueDateStreamItem = StreamItem(
+            type = "Message",
+            notificationCategory = "Due Date",
+            course_id = validCourse.courseId,
+        )
+        val scoredGradeStreamItem = StreamItem(
+            type = "Message",
+            grade = "A",
+            course_id = validCourse.courseId,
+        )
+        val scoredScoreStreamItem = StreamItem(
+            type = "Message",
+            score = 95.0,
+            course_id = validCourse.courseId,
+        )
+        val gradingPeriodStreamItem = StreamItem(
+            type = "Message",
+            notificationCategory = "Grading Policies",
+            course_id = validCourse.courseId,
+        )
+        val streamItems = listOf(conversationStreamItem, messageStreamItem, discussionStreamItem, announcementStreamItem, notValidAnnouncementStreamItem, dueDateStreamItem, scoredGradeStreamItem, scoredScoreStreamItem, gradingPeriodStreamItem)
+
+        coEvery { streamApi.getUserStream(any()) } returns DataResult.Success(streamItems)
+        coEvery { getCoursesManager.getCoursesWithProgress(any(), any()) } returns DataResult.Success(listOf(validCourse))
+
+        val result = getRepository().getNotifications(forceRefresh = true)
+
+        assertEquals(5, result.size)
+        assertTrue(result.contains(announcementStreamItem))
+        assertTrue(result.contains(dueDateStreamItem))
+        assertTrue(result.contains(scoredGradeStreamItem))
+        assertTrue(result.contains(scoredScoreStreamItem))
+        assertTrue(result.contains(gradingPeriodStreamItem))
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `Test failed Stream API call`() = runTest {
+        coEvery { getCoursesManager.getCoursesWithProgress(any(), any()) } returns DataResult.Success(emptyList())
+        coEvery { streamApi.getUserStream(any()) } returns DataResult.Fail()
+        getRepository().getNotifications(true)
+    }
+
+    @Test
+    fun `Test successful Global Announcements depagination`() = runTest {
+        val announcement1 = AccountNotification(id = 1L, subject = "Announcement 1")
+        val announcement2 = AccountNotification(id = 2L, subject = "Announcement 2")
+
+        coEvery { accountNotificationApi.getAccountNotifications(any(), any(), any()) } returns
+            DataResult.Success(
+                listOf(announcement1),
+                LinkHeaders(nextUrl = "nextPageUrl")
+            )
+        coEvery { accountNotificationApi.getNextPageNotifications("nextPageUrl", any()) } returns
+                DataResult.Success(
+                    listOf(announcement2),
+                )
+
+        val result = getRepository().getGlobalAnnouncements(true)
+        assertEquals(2, result.size)
+        assertEquals(listOf(announcement1, announcement2), result)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `Test failed Global Announcements depagination`() = runTest {
+        coEvery { accountNotificationApi.getAccountNotifications(any(), any(), any()) } returns DataResult.Fail()
+        getRepository().getGlobalAnnouncements(true)
+    }
+
+    @Test
+    fun `Test successful getCourse by id`() = runTest {
+        val id = 1L
+        val course = Course(id = id, name = "Course 1")
+        coEvery { courseApi.getCourse(id, any()) } returns DataResult.Success(course)
+        val result = getRepository().getCourse(id)
+        assertEquals(course, result)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `Test failed getCourse by id`() = runTest {
+        coEvery { courseApi.getCourse(any(), any()) } returns DataResult.Fail()
+        getRepository().getCourse(1L)
+    }
+
+    private fun getRepository(): NotificationRepository {
+        return NotificationRepository(apiPrefs, streamApi, courseApi, accountNotificationApi, getCoursesManager)
+    }
 }
