834cc2f67df6fe33091a59ba1c33b03f7eff8de3
domonkosadam <domonkosadam01@gmail.com>
2025-10-16 16:18:07 +0200

    Fix max width

--- libs/horizon/src/main/java/com/instructure/horizon/features/dashboard/widget/skilloverview/card/DashboardSkillOverviewCardContent.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/features/dashboard/widget/skilloverview/card/DashboardSkillOverviewCardContent.kt
@@ -20,7 +20,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -64,6 +66,8 @@ fun DashboardSkillOverviewCardContent(
             }
         },
         modifier = modifier
+            .widthIn(max = 300.dp)
+            .padding(bottom = 8.dp),
     ) {
         if (state.completedSkillCount == 0) {
             Text(
