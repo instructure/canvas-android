8becb11abcba1bf9b54fcfee56fb1e168a6fc6b5
kristof.deak <kristof.deak@instructure.com>
2025-10-29 12:16:52 +0100

    Refactor Discussion Checkpoint in Syllabus test according to new master changes (on assignment details page there is a separate box for the checkpoints, so added some assertions to it). Fix scrolling issue.

--- apps/student/src/androidTest/java/com/instructure/student/ui/e2e/classic/DiscussionsE2ETest.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/e2e/classic/DiscussionsE2ETest.kt
@@ -267,8 +267,12 @@ class DiscussionsE2ETest: StudentComposeTest() {
         assignmentDetailsPage.assertDisplayToolbarTitle()
         assignmentDetailsPage.assertDisplayToolbarSubtitle(courseName)
 
+        Log.d(ASSERTION_TAG, "Assert that there is a separate view (box) for the checkpoints with their corresponding grades.")
+        assignmentDetailsPage.assertCheckpointGradesView("Reply to topic", "-/100")
+        assignmentDetailsPage.assertCheckpointGradesView("Additional replies (1)", "-/3")
+
         Log.d(ASSERTION_TAG, "Assert that the checkpoints are displayed properly on the Assignment Details Page.")
-        assignmentDetailsPage.assertDiscussionCheckpointDetailsOnDetailsPage("Reply to topic due","Nov 12, 2025 10:59 PM")
-        assignmentDetailsPage.assertDiscussionCheckpointDetailsOnDetailsPage("Additional replies (1) due","Nov 19, 2025 10:59 PM")
+        assignmentDetailsPage.assertDiscussionCheckpointDetailsOnDetailsPage("Reply to topic due","Nov 13, 2025 7:59 AM")
+        assignmentDetailsPage.assertDiscussionCheckpointDetailsOnDetailsPage("Additional replies (1) due","Nov 20, 2025 7:59 AM")
     }
 }
