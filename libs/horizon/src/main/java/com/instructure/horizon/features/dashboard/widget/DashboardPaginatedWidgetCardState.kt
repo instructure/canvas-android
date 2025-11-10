4915d188a23bf415c02e048ba4025827b4fe6a92
domonkosadam <53480952+domonkosadam@users.noreply.github.com>
2025-11-10 12:29:32 +0100

    [CLX-3127][Horizon] Learner dashboard landscape mode (#3364)
    refs: CLX-3127
    affects: Horizon
    release note: none

--- libs/horizon/src/main/java/com/instructure/horizon/features/dashboard/widget/DashboardPaginatedWidgetCardState.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/features/dashboard/widget/DashboardPaginatedWidgetCardState.kt
@@ -6,7 +6,14 @@ import java.util.Date
 data class DashboardPaginatedWidgetCardState(
     val items: List<DashboardPaginatedWidgetCardItemState> = emptyList(),
     val isLoading: Boolean = false,
-)
+) {
+    companion object {
+        val Loading = DashboardPaginatedWidgetCardState(
+            items = listOf(DashboardPaginatedWidgetCardItemState.Loading),
+            isLoading = true
+        )
+    }
+}
 
 data class DashboardPaginatedWidgetCardItemState(
     val chipState: DashboardPaginatedWidgetCardChipState? = null,
@@ -15,7 +22,20 @@ data class DashboardPaginatedWidgetCardItemState(
     val date: Date? = null,
     val title: String? = null,
     val route: DashboardPaginatedWidgetCardButtonRoute? = null
-)
+) {
+    companion object {
+        val Loading = DashboardPaginatedWidgetCardItemState(
+            chipState = DashboardPaginatedWidgetCardChipState(
+                label = "Announcement",
+                color = StatusChipColor.Sky
+            ),
+            title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Announcement title shown here.",
+            source = "Institution or Course Name Here",
+            date = Date(),
+            route = DashboardPaginatedWidgetCardButtonRoute.MainRoute("")
+        )
+    }
+}
 
 data class DashboardPaginatedWidgetCardChipState(
     val label: String,
