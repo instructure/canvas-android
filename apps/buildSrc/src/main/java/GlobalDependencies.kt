bf3fa1fff56f696508aa1c2dc05e2d64f4b8d71c
Tamas Kozmer <tamas.kozmer@instructure.com>
2025-12-12 14:41:06 +0100

    Android and Firebase dependencies.

--- apps/buildSrc/src/main/java/GlobalDependencies.kt
+++ apps/buildSrc/src/main/java/GlobalDependencies.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     /* SDK Versions */
-    const val COMPILE_SDK = 35
+    const val COMPILE_SDK = 36
     const val MIN_SDK = 28
     const val TARGET_SDK = 35
 
@@ -30,17 +30,17 @@ object Versions {
     const val MOBIUS = "1.2.1"
     const val HILT = "2.57.2"
     const val HILT_ANDROIDX = "1.3.0"
-    const val LIFECYCLE = "2.9.4"
+    const val LIFECYCLE = "2.10.0"
     const val FRAGMENT = "1.8.9"
-    const val WORK_MANAGER = "2.10.5"
+    const val WORK_MANAGER = "2.11.0"
     const val GLIDE_VERSION = "5.0.5"
     const val RETROFIT = "2.11.0"
     const val OKHTTP = "4.12.0"
     const val ROOM = "2.8.4"
     const val HAMCREST = "3.0"
-    const val NAVIGATION = "2.9.5"
+    const val NAVIGATION = "2.9.6"
     const val MEDIA3 = "1.8.0"
-    const val DATASTORE = "1.1.7"
+    const val DATASTORE = "1.2.0"
     const val LOTTIE = "6.6.6"
     const val ENCRYPTED_SHARED_PREFERENCES = "1.1.0"
     const val JAVA_JWT = "4.5.0"
@@ -61,31 +61,30 @@ object Libs {
     const val APOLLO_HTTP_CACHE = "com.apollographql.apollo:apollo-http-cache:${Versions.APOLLO}"
 
     /* Androidx libraries */
-    const val ANDROIDX_ANNOTATION = "androidx.annotation:annotation:1.9.0"
-    const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:1.7.0"
-    const val ANDROIDX_BROWSER = "androidx.browser:browser:1.8.0"
+    const val ANDROIDX_ANNOTATION = "androidx.annotation:annotation:1.9.1"
+    const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:1.7.1"
+    const val ANDROIDX_BROWSER = "androidx.browser:browser:1.9.0"
     const val ANDROIDX_CARDVIEW = "androidx.cardview:cardview:1.0.0"
-    const val ANDROIDX_CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.2.0"
-    const val ANDROIDX_EXIF = "androidx.exifinterface:exifinterface:1.3.7"
+    const val ANDROIDX_CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.2.1"
+    const val ANDROIDX_EXIF = "androidx.exifinterface:exifinterface:1.4.2"
     const val ANDROIDX_FRAGMENT = "androidx.fragment:fragment:${Versions.FRAGMENT}"
     const val ANDROIDX_FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT}"
     const val ANDROIDX_PALETTE = "androidx.palette:palette:1.0.0"
     const val ANDROIDX_PERCENT = "androidx.percentlayout:percentlayout:1.0.0"
     const val ANDROIDX_RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.4.0"
     const val ANDROIDX_VECTOR = "androidx.vectordrawable:vectordrawable:1.2.0"
-    const val ANDROIDX_SWIPE_REFRESH_LAYOUT = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
+    const val ANDROIDX_SWIPE_REFRESH_LAYOUT = "androidx.swiperefreshlayout:swiperefreshlayout:1.2.0"
     const val ANDROIDX_CORE_TESTING = "androidx.arch.core:core-testing:2.2.0"
     const val ANDROIDX_WORK_MANAGER = "androidx.work:work-runtime:${Versions.WORK_MANAGER}"
     const val ANDROIDX_WORK_MANAGER_KTX = "androidx.work:work-runtime-ktx:${Versions.WORK_MANAGER}"
     const val ANDROIDX_WORK_TEST = "androidx.work:work-testing:${Versions.WORK_MANAGER}"
-    const val ANDROIDX_WEBKIT = "androidx.webkit:webkit:1.12.0"
-    const val ANDROIDX_COMPOSE_ACTIVITY = "androidx.activity:activity-compose:1.10.0"
+    const val ANDROIDX_WEBKIT = "androidx.webkit:webkit:1.14.0"
+    const val ANDROIDX_COMPOSE_ACTIVITY = "androidx.activity:activity-compose:1.12.1"
     const val DATASTORE = "androidx.datastore:datastore-preferences:${Versions.DATASTORE}"
     const val ENCRYPTED_SHARED_PREFERENCES = "androidx.security:security-crypto:${Versions.ENCRYPTED_SHARED_PREFERENCES}"
-    const val JAVA_JWT = "com.auth0:java-jwt:${Versions.JAVA_JWT}"
 
     /* Firebase */
-    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:34.3.0"
+    const val FIREBASE_BOM = "com.google.firebase:firebase-bom:34.6.0"
     const val FIREBASE_CRASHLYTICS = "com.google.firebase:firebase-crashlytics"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging"
     const val FIREBASE_CONFIG = "com.google.firebase:firebase-config"
@@ -130,7 +129,7 @@ object Libs {
     const val VIEW_MODE_SAVED_STATE = "androidx.lifecycle:lifecycle-viewmodel-savedstate:${Versions.LIFECYCLE}"
     const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:${Versions.LIFECYCLE}"
     const val COMPOSE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.LIFECYCLE}"
-    const val COMPOSE_NAVIGATION = "androidx.navigation:navigation-compose:2.8.9"
+
     /* Media and content handling */
     const val NUTRIENT = "io.nutrient:nutrient:${Versions.NUTRIENT}"
     const val MEDIA3 = "androidx.media3:media3-exoplayer:${Versions.MEDIA3}"
@@ -169,9 +168,11 @@ object Libs {
     const val KEYBOARD_VISIBILITY_LISTENER = "net.yslibrary.keyboardvisibilityevent:keyboardvisibilityevent:2.2.1"
     const val APACHE_COMMONS_TEXT = "org.apache.commons:commons-text:1.12.0"
     const val CAMERA_VIEW = "com.otaliastudios:cameraview:2.7.2"
+    const val JAVA_JWT = "com.auth0:java-jwt:${Versions.JAVA_JWT}"
 
     const val PENDO = "sdk.pendo.io:pendoIO:3.7.+"
 
+    /* Database */
     const val ROOM = "androidx.room:room-runtime:${Versions.ROOM}"
     const val ROOM_COMPILER = "androidx.room:room-compiler:${Versions.ROOM}"
     const val ROOM_COROUTINES = "androidx.room:room-ktx:${Versions.ROOM}"
