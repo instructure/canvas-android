cc24052b06756716a6d5df3d4e964aff1eb695ff
kristof.deak <kristof.deak@instructure.com>
2026-01-12 15:46:51 +0100

    Workmanager issue repro. (Remove handleWorkmanagerTask usages).

--- apps/student/src/androidTest/java/com/instructure/student/ui/interaction/SubmissionDetailsInteractionTest.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/interaction/SubmissionDetailsInteractionTest.kt
@@ -41,7 +41,6 @@ import com.instructure.canvasapi2.models.Course
 import com.instructure.canvasapi2.models.RubricCriterion
 import com.instructure.canvasapi2.models.RubricCriterionRating
 import com.instructure.canvasapi2.models.SubmissionComment
-import com.instructure.espresso.handleWorkManagerTask
 import com.instructure.student.ui.pages.classic.WebViewTextCheck
 import com.instructure.student.ui.utils.StudentComposeTest
 import com.instructure.student.ui.utils.extensions.tokenLogin
@@ -79,13 +78,13 @@ class SubmissionDetailsInteractionTest : StudentComposeTest() {
         assignmentListPage.clickAssignment(assignment)
         assignmentDetailsPage.clickSubmit()
         urlSubmissionUploadPage.submitText("https://google.com")
-        handleWorkManagerTask("SubmissionWorker")
+     //   handleWorkManagerTask("SubmissionWorker")
 
         assignmentDetailsPage.assertAssignmentSubmitted()
         assignmentDetailsPage.goToSubmissionDetails()
         submissionDetailsPage.openComments()
         submissionDetailsPage.addAndSendComment("Hey!")
-        handleWorkManagerTask("SubmissionWorker")
+      //  handleWorkManagerTask("SubmissionWorker")
 
         submissionDetailsPage.assertCommentDisplayed("Hey!", data.users.values.first())
     }
@@ -104,14 +103,14 @@ class SubmissionDetailsInteractionTest : StudentComposeTest() {
         assignmentListPage.clickAssignment(assignment)
         assignmentDetailsPage.clickSubmit()
         urlSubmissionUploadPage.submitText("https://google.com")
-        handleWorkManagerTask("SubmissionWorker")
+    //    handleWorkManagerTask("SubmissionWorker")
 
         assignmentDetailsPage.assertAssignmentSubmitted()
         assignmentDetailsPage.assertNoAttemptSpinner()
 
         assignmentDetailsPage.clickSubmit()
         urlSubmissionUploadPage.submitText("https://google.com")
-        handleWorkManagerTask("SubmissionWorker")
+      //  handleWorkManagerTask("SubmissionWorker")
 
         assignmentDetailsPage.goToSubmissionDetails()
 
@@ -119,7 +118,7 @@ class SubmissionDetailsInteractionTest : StudentComposeTest() {
         submissionDetailsPage.assertSelectedAttempt("Attempt 1")
         submissionDetailsPage.openComments()
         submissionDetailsPage.addAndSendComment("Hey!")
-        handleWorkManagerTask("SubmissionWorker")
+      //  handleWorkManagerTask("SubmissionWorker")
 
         submissionDetailsPage.assertCommentDisplayed("Hey!", data.users.values.first())
 
