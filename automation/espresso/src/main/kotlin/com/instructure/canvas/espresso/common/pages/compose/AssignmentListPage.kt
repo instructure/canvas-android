11bfdefbf750a3b237bd5232ec59a9ef20aca989
Kristof Deak <92309696+kdeakinstructure@users.noreply.github.com>
2026-01-14 13:14:33 +0100

    [MBL-19655][Student] - Use DataSeeding in existing Discussion Checkpoint related E2E tests (#3475)
    * Use dataseeding in existing discussion checkpoint tests instead of burnt in course and discussions.
    Handle discussions with due dates and no due dates.
    Add date converting utility function.
    
    refs: MBL-19655
    affects: Student
    release note: -
    
    * Fix date difference issue in checkpoint dates.
    
    * PR changes. (Use imports instead of fully qualified names + remove hard coded due dates in assertion methods).

--- automation/espresso/src/main/kotlin/com/instructure/canvas/espresso/common/pages/compose/AssignmentListPage.kt
+++ automation/espresso/src/main/kotlin/com/instructure/canvas/espresso/common/pages/compose/AssignmentListPage.kt
@@ -114,8 +114,8 @@ class AssignmentListPage(private val composeTestRule: ComposeTestRule) {
         assertHasAssignmentCommon(assignment.name, assignment.dueAt, needsGradingLabel)
     }
 
-    fun assertHasAssignmentWithCheckpoints(assignmentName: String, dueAtString: String = "No due date", expectedGrade: String? = null) {
-        assertHasAssignmentCommon(assignmentName, dueAtString, expectedGrade, hasCheckPoints = true)
+    fun assertHasAssignmentWithCheckpoints(assignmentName: String, dueAtString: String = "No due date", dueAtStringSecondCheckpoint: String? = null, expectedGrade: String? = null) {
+        assertHasAssignmentCommon(assignmentName, dueAtString, dueAtStringSecondCheckpoint, expectedGrade, hasCheckPoints = true)
     }
 
     fun assertAssignmentNotDisplayed(assignmentName: String) {
@@ -162,15 +162,42 @@ class AssignmentListPage(private val composeTestRule: ComposeTestRule) {
         composeTestRule.onNode( hasTestTag("checkpointGradeText") and hasText(gradeAdditionalReplies), useUnmergedTree = true).assertIsDisplayed()
     }
 
-    private fun assertHasAssignmentCommon(assignmentName: String, assignmentDueAt: String?, expectedGradeLabel: String? = null, assignmentStatus: String? = null, hasCheckPoints : Boolean = false) {
+    private fun assertHasAssignmentCommon(assignmentName: String, assignmentDueAt: String?, secondCheckpointDueAt: String? = null, expectedGradeLabel: String? = null, assignmentStatus: String? = null, hasCheckPoints : Boolean = false) {
 
        // Check if the assignment is a discussion with checkpoints, if yes, we are expecting 2 due dates for the 2 checkpoints.
        if(hasCheckPoints) {
-            composeTestRule.onAllNodes(
-                hasText("No due date").and(
-                    hasParent(hasAnyDescendant(hasText(assignmentName)))
-                ),
-            true).assertCountEquals(2)
+           if (assignmentDueAt == null || assignmentDueAt == "No due date") {
+               composeTestRule.onAllNodes(
+                   hasText("No due date").and(
+                       hasParent(hasAnyDescendant(hasText(assignmentName)))
+                   ),
+                   true
+               ).assertCountEquals(2)
+           }
+           else {
+               if(secondCheckpointDueAt != null) {
+                   composeTestRule.onAllNodes(
+                       hasText(assignmentDueAt).and(
+                           hasParent(hasAnyDescendant(hasText(assignmentName)))
+                       ),
+                       true
+                   ).assertCountEquals(1)
+                   composeTestRule.onAllNodes(
+                       hasText(secondCheckpointDueAt).and(
+                           hasParent(hasAnyDescendant(hasText(assignmentName)))
+                       ),
+                       true
+                   ).assertCountEquals(1)
+               }
+               else {
+                     composeTestRule.onAllNodes(
+                          hasText(assignmentDueAt).and(
+                            hasParent(hasAnyDescendant(hasText(assignmentName)))
+                          ),
+                          true
+                     ).assertCountEquals(2)
+               }
+           }
         }
         else {
            // Check that either the assignment due date is present, or "No Due Date" is displayed
