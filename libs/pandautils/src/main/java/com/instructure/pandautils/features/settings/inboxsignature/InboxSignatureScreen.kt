122479b96de699ae07ca8ac4cc8ee4dc4ba5e280
kristof.deak <kristof.deak@instructure.com>
2026-02-11 16:18:48 +0100

    Fix Teacher QA V6 - Part 1 - 2. finding (Inbox Signature Settings page camera cutout issue).

--- libs/pandautils/src/main/java/com/instructure/pandautils/features/settings/inboxsignature/InboxSignatureScreen.kt
+++ libs/pandautils/src/main/java/com/instructure/pandautils/features/settings/inboxsignature/InboxSignatureScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.ime
@@ -27,6 +28,7 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
@@ -101,6 +103,7 @@ fun InboxSignatureScreen(uiState: InboxSignatureUiState, actionHandler: (InboxSi
 fun InboxSignatureContent(uiState: InboxSignatureUiState, actionHandler: (InboxSignatureAction) -> Unit, modifier: Modifier = Modifier) {
     Column(
         modifier
+            .windowInsetsPadding(WindowInsets.displayCutout)
             .fillMaxSize()
             .verticalScroll(rememberScrollState())) {
         Spacer(modifier = Modifier.size(16.dp))
