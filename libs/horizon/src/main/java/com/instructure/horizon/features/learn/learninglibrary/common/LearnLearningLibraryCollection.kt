fd6069b70d7bdba4a010ffa9f8411d760f8d46d0
domonkosadam <domonkosadam01@gmail.com>
2026-03-13 13:37:43 +0100

    Fix finding

--- libs/horizon/src/main/java/com/instructure/horizon/features/learn/learninglibrary/common/LearnLearningLibraryCollection.kt
+++ libs/horizon/src/main/java/com/instructure/horizon/features/learn/learninglibrary/common/LearnLearningLibraryCollection.kt
@@ -42,13 +42,12 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.onClick
-import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.instructure.canvasapi2.models.journey.learninglibrary.CollectionItemType
 import com.instructure.canvasapi2.utils.ContextKeeper
 import com.instructure.horizon.R
+import com.instructure.horizon.horizonui.expandable
 import com.instructure.horizon.horizonui.foundation.HorizonColors
 import com.instructure.horizon.horizonui.foundation.HorizonSpace
 import com.instructure.horizon.horizonui.foundation.HorizonTypography
@@ -169,10 +168,7 @@ private fun LearnLearningLibraryCollectionTitle(
     onExpandedChanged: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val expandedState = stringResource(R.string.a11y_expanded)
-    val collapsedState = stringResource(R.string.a11y_collapsed)
-    val expandAction = stringResource(R.string.a11y_expand)
-    val collapseAction = stringResource(R.string.a11y_collapse)
+    val context = LocalContext.current
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
@@ -183,22 +179,7 @@ private fun LearnLearningLibraryCollectionTitle(
             }
             .clearAndSetSemantics {
                 contentDescription = title
-                stateDescription = if (isExpanded) {
-                    expandedState
-                } else {
-                    collapsedState
-                }
-                onClick(
-                    label = if (isExpanded) {
-                        collapseAction
-                    } else {
-                        expandAction
-                    },
-                    {
-                        onExpandedChanged(!isExpanded)
-                        true
-                    }
-                )
+                expandable(context, isExpanded)
             }
     ) {
         Text(
